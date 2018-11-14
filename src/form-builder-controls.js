d2e6f50543b7ebe2d6cea8c81c4e86197f28b616
Mila Endo <milal.alim@gmail.com>
2018-11-14 15:24:10 -0800

    new field for color

--- src/form-builder-controls.js
+++ src/form-builder-controls.js
@@ -13,8 +13,7 @@ import {
     FormDatePicker
 } from "@processmaker/vue-form-elements/src/components";
 
-export default [
-    {
+export default [{
         builderComponent: FormText,
         builderBinding: 'FormText',
         rendererComponent: FormText,
@@ -29,8 +28,7 @@ export default [
                 fontSize: '1em',
                 fontWeight: 'normal'
             },
-            inspector: [
-                {
+            inspector: [{
                     type: "FormInput",
                     field: "label",
                     config: {
@@ -44,8 +42,7 @@ export default [
                     config: {
                         label: "Font Weight",
                         helper: "The weight of the text",
-                        options: [
-                            {
+                        options: [{
                                 value: 'normal',
                                 content: 'Normal'
                             },
@@ -56,16 +53,21 @@ export default [
                         ]
                     }
                 },
-
-
+                {
+                    type: "FormInput",
+                    field: "color",
+                    config: {
+                        label: "Text Color",
+                        helper: "Accepts all HTML colors and hex codes"
+                    }
+                },
                 {
                     type: "FormSelect",
                     field: "fontSize",
                     config: {
                         label: "Font Size",
                         helper: "The size of the text in em",
-                        options: [
-                            {
+                        options: [{
                                 value: '0.5em',
                                 content: '0.5'
                             },
@@ -107,8 +109,7 @@ export default [
                 helper: null,
                 type: 'text'
             },
-            inspector: [
-                {
+            inspector: [{
                     type: "FormInput",
                     field: "name",
                     config: {
@@ -125,8 +126,7 @@ export default [
                         label: "Field Type",
                         name: 'Field Type',
                         helper: "The type for this field",
-                        options: [
-                            {
+                        options: [{
                                 value: 'text',
                                 content: 'Text'
                             },
@@ -160,8 +160,7 @@ export default [
                     field: "placeholder",
                     config: {
                         label: "Placeholder",
-                        helper:
-                            "The placeholder is what is shown in the field when no value is provided yet"
+                        helper: "The placeholder is what is shown in the field when no value is provided yet"
                     }
                 },
                 {
@@ -169,8 +168,7 @@ export default [
                     field: "helper",
                     config: {
                         label: "Help Text",
-                        helper:
-                            "Help text is meant to provide additional guidance on the field's value"
+                        helper: "Help text is meant to provide additional guidance on the field's value"
                     }
                 }
             ]
@@ -190,8 +188,7 @@ export default [
                 label: "New Select",
                 placeholder: "",
                 validation: '',
-                options: [
-                    {
+                options: [{
                         value: 'new',
                         content: 'New Option'
                     }
@@ -199,8 +196,7 @@ export default [
                 ],
                 helper: null,
             },
-            inspector: [
-                {
+            inspector: [{
                     type: "FormInput",
                     field: "name",
                     config: {
@@ -230,8 +226,7 @@ export default [
                     field: "helper",
                     config: {
                         label: "Help Text",
-                        helper:
-                            "Help text is meant to provide additional guidance on the field's value"
+                        helper: "Help text is meant to provide additional guidance on the field's value"
                     }
                 },
                 {
@@ -257,16 +252,13 @@ export default [
             'editor-icon': require('./assets/icons/RadioButton.png'),
             config: {
                 label: "New Radio Button Group",
-                options: [
-                    {
-                        value: 'new',
-                        content: 'New Option'
-                    }
-                ],
+                options: [{
+                    value: 'new',
+                    content: 'New Option'
+                }],
                 helper: null,
             },
-            inspector: [
-                {
+            inspector: [{
                     type: "FormInput",
                     field: "label",
                     config: {
@@ -279,8 +271,7 @@ export default [
                     field: "helper",
                     config: {
                         label: "Help Text",
-                        helper:
-                            "Help text is meant to provide additional guidance on the field's value"
+                        helper: "Help text is meant to provide additional guidance on the field's value"
                     }
                 },
                 {
@@ -311,14 +302,12 @@ export default [
                 checked: false,
                 validation: '',
             },
-            inspector: [
-                {
+            inspector: [{
                     type: "FormInput",
                     field: "name",
                     config: {
                         label: "Field Name",
-                        helper:
-                            "The name of the group for the checkbox. All checkboxes which share the same name will work together."
+                        helper: "The name of the group for the checkbox. All checkboxes which share the same name will work together."
                     }
                 },
                 {
@@ -334,8 +323,7 @@ export default [
                     field: "helper",
                     config: {
                         label: "Help Text",
-                        helper:
-                            "Help text is meant to provide additional guidance on the field's value"
+                        helper: "Help text is meant to provide additional guidance on the field's value"
                     }
                 },
                 {
@@ -343,8 +331,7 @@ export default [
                     field: "checked",
                     config: {
                         label: "Initially Checked?",
-                        helper:
-                            "Should the checkbox be checked by default"
+                        helper: "Should the checkbox be checked by default"
                     }
                 },
             ]
@@ -366,8 +353,7 @@ export default [
                 helper: null,
                 rows: 2
             },
-            inspector: [
-                {
+            inspector: [{
                     type: "FormInput",
                     field: "name",
                     config: {
@@ -407,8 +393,7 @@ export default [
                     field: "placeholder",
                     config: {
                         label: "Placeholder",
-                        helper:
-                            "The placeholder is what is shown in the field when no value is provided yet"
+                        helper: "The placeholder is what is shown in the field when no value is provided yet"
                     }
                 },
                 {
@@ -416,8 +401,7 @@ export default [
                     field: "helper",
                     config: {
                         label: "Help Text",
-                        helper:
-                            "Help text is meant to provide additional guidance on the field's value"
+                        helper: "Help text is meant to provide additional guidance on the field's value"
                     }
                 }
             ]
@@ -438,8 +422,7 @@ export default [
                 type: 'date',
                 name: ''
             },
-            inspector: [
-                {
+            inspector: [{
                     type: "FormInput",
                     field: "name",
                     config: {
@@ -462,8 +445,7 @@ export default [
                     field: "placeholder",
                     config: {
                         label: "Placeholder",
-                        helper:
-                            "The placeholder is what is shown in the field when no value is provided yet"
+                        helper: "The placeholder is what is shown in the field when no value is provided yet"
                     }
                 }
             ]
@@ -484,8 +466,7 @@ export default [
                 variant: 'primary',
                 event: 'submit'
             },
-            inspector: [
-                {
+            inspector: [{
                     type: "FormInput",
                     field: "label",
                     config: {
@@ -499,8 +480,7 @@ export default [
                     config: {
                         label: "Variant",
                         helper: "The variant determines the appearance of the button",
-                        options: [
-                            {
+                        options: [{
                                 value: 'primary',
                                 content: 'Primary'
                             },
@@ -562,8 +542,7 @@ export default [
                 eventData: 0
 
             },
-            inspector: [
-                {
+            inspector: [{
                     type: "FormInput",
                     field: "label",
                     config: {
@@ -577,8 +556,7 @@ export default [
                     config: {
                         label: "Variant",
                         helper: "The variant determines the appearance of the button",
-                        options: [
-                            {
+                        options: [{
                                 value: 'primary',
                                 content: 'Primary'
                             },
@@ -647,10 +625,8 @@ export default [
                 [],
                 []
             ],
-            config: {
-            },
-            inspector: [
-                {
+            config: {},
+            inspector: [{
                     type: "FormText",
                     config: {
                         label: "MultiColumn",
@@ -677,8 +653,7 @@ export default [
                 fields: [],
                 form: ''
             },
-            inspector: [
-                {
+            inspector: [{
                     type: "FormInput",
                     field: "name",
                     config: {
@@ -701,8 +676,7 @@ export default [
                     field: "editable",
                     config: {
                         label: "Editable?",
-                        helper:
-                            "Should records be editable/removable and can new records be added"
+                        helper: "Should records be editable/removable and can new records be added"
                     }
                 },
 
