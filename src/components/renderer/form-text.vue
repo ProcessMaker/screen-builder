b8de20051b2f9b1198ddb6443331e3b7f91834d4
Taylor Dondich <taylor@dondich.com>
2018-11-21 12:05:16 -0800

    Fix syntax error in formtext and bump version number for release.

--- src/components/renderer/form-text.vue
+++ src/components/renderer/form-text.vue
@@ -1,37 +1,36 @@
 <template>
-<div class="form-group">
-<div :style="styles" v-html="rendered"></div>
-</div>
+    <div class="form-group">
+        <div :style="styles" v-html="rendered"></div>
+    </div>
 </template>
 
 <script>
 import Mustache from "mustache";
 
 export default {
-    props: [
-        'label',
-        'fontSize',
-        'fontWeight',
-        'textAlign',
-        'validationData',
-        'color'
-    ],
-    computed: {
-        styles() {
-            return {
-                fontSize: this.fontSize,
-                fontWeight: this.fontWeight,
-                textAlign: this.textAlign,
-                color: this.color
-            }
-        },
-        rendered() {
-            try {
-                return Mustache.render(this.label, this.validationData)
-            } catch(e) {
-                return this.label;
-            }
-        }
+  props: [
+    "label",
+    "fontSize",
+    "fontWeight",
+    "textAlign",
+    "validationData",
+    "color"
+  ],
+  computed: {
+    styles() {
+      return {
+        fontSize: this.fontSize,
+        fontWeight: this.fontWeight,
+        textAlign: this.textAlign,
+        color: this.color
+      };
+    },
+    rendered() {
+      try {
+        return Mustache.render(this.label, this.validationData);
+      } catch (e) {
+        return this.label;
+      }
     }
   }
 };
