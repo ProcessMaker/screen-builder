b808d3ca18c6fe2efc5eb1e3c5e108b08c355ea4
Mila Endo <milal.alim@gmail.com>
2018-12-03 10:58:45 -0800

    Revert "button now has name and value"
    This reverts commit f8b5936d17d1044e91bb43a07ec668ac300f1e31.

--- src/components/renderer/form-button.vue
+++ src/components/renderer/form-button.vue
@@ -1,30 +1,38 @@
 <template>
-  <div class="form-group">
-    <button @click="click" :class="classList" :name="name" :value="value">{{label}}</button>
-  </div>
+<div class="form-group">
+    <button @click="click" :class="classList">{{label}}</button>
+</div>
 </template>
 
 <script>
 export default {
-  props: ["variant", "label", "event", "eventData", "name", "value"],
-  computed: {
-    classList() {
-      let variant = this.variant || "primary";
-      return {
-        btn: true,
-        ["btn-" + variant]: true
-      };
+    props: [
+        'variant',
+        'label',
+        'event',
+        'eventData'
+    ],
+    computed: {
+        classList() {
+            let variant = this.variant || 'primary';
+            return {
+                btn: true,
+                ['btn-' + variant]: true
+            }
+        }
+    },
+    methods: {
+        click() {
+            this.$emit(this.event, this.eventData)
+        }
     }
-  },
-  methods: {
-    click() {
-      this.$emit(this.event, this.eventData);
-    }
-  }
-};
+
+    
+}
 </script>
 
 <style lang="scss" scoped>
+
 </style>
 
 
