5e556172a7ed7f9657ade40ed306baf08844044d
Nolan Ehrstrom <github@nolanpro.com>
2018-12-12 17:49:20 -0800

    Add a static record display type

---
+++ src/components/renderer/form-record-list-static.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <h4>{{label}}</h4>
+        <table style="width:100%; text-align:left; padding:0.3em;">
+            <thead style="border-bottom:1px solid black; background-color:lightgrey; font-weight:bold;">
+                <tr>
+                    <th v-for="field in fields" style="padding:0.3em; margin:0">
+                        {{ field.content }}
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="val in value" style="border-bottom: 1px solid lightgrey;">
+                    <td v-for="field in fields" style="padding:0.3em; margin:0">
+                        {{ val[field.value] }}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+
+<script>
+// import bModal from "bootstrap-vue/es/components/modal/modal";
+// import VueFormRenderer from "../vue-form-renderer";
+
+export default {
+  name: "FormRecordListStatic",
+  props: ["label", "fields", "value", "editable", "_config", "form"],
+  components: {
+  },
+  mounted() {
+  },
+  data() {
+    return {
+    };
+  },
+};
+</script>
