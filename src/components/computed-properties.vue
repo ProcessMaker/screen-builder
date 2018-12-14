5c52a6c651063d6e2d76532b4f34dc123123221c
Marco Antonio Nina Mena <marco.antonio.nina@processmaker.com>
2018-12-14 12:14:28 -0400

    Add fortawesome

--- src/components/computed-properties.vue
+++ src/components/computed-properties.vue
@@ -19,15 +19,14 @@
             <b-table :items="current" :fields="fields" responsive striped bordered small hover fixed>
                 <template slot="actions" slot-scope="row">
                     <!-- we use @click.stop here to prevent emitting of a 'row-clicked' event  -->
-                    <b-btn size="sm" variant="action" @click.stop="row.toggleDetails" class="mr-2">
-                        {{ row.detailsShowing ? 'Hide' : 'Show'}} Details
+                    <b-btn size="lg" variant="action" @click.stop="row.toggleDetails" class="p-0 mr-2 border-0 bg-transparent" title="Details">
+                        <i class="fa fa-list-alt fa-1x"></i>
                     </b-btn>
-                    <b-btn size="sm" variant="action" class="mr-2" @click.stop="editProperty(row.item)">
-                        Edit
+                    <b-btn size="lg" variant="action" title="edit" class="p-0 mr-2 border-0 bg-transparent" @click.stop="editProperty(row.item)">
+                        <i class="fa fa-edit fa-1x"></i>
                     </b-btn>
-                    <b-btn size="sm" variant="action" class="mr-2"
-                           @click.stop="deleteProperty(row.item)">
-                        Delete
+                    <b-btn size="lg" variant="action" title="Delete" class="p-0 mr-2 border-0 bg-transparent" @click.stop="deleteProperty(row.item)">
+                        <i class="fa fa-trash fa-1x"></i>
                     </b-btn>
                 </template>
                 <template slot="row-details" slot-scope="row">
