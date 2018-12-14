67013a0046657aaa2a7f418385ea0f626c1e4a2c
Marco Antonio Nina Mena <marco.antonio.nina@processmaker.com>
2018-12-14 12:14:28 -0400

    Add fortawesome

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-Vue.config.productionTip = false
+import Fortawesome from '@fortawesome/fontawesome-free';
+Vue.config.productionTip = false;
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 new Vue({
   render: h => h(App)
