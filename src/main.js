abd58b924e682d388ac12aecdcd6f35a6bea80ba
David Callizaya <davidcallizaya@gmail.com>
2018-12-19 09:22:17 -0400

    Remove unused import.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Fortawesome from '@fortawesome/fontawesome-free';
 Vue.config.productionTip = false;
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
