23e6cd3f4c7d594673acadef1d4a72a8f72032c2
Nolan Ehrstrom <github@nolanpro.com>
2019-02-04 14:36:50 -0800

    Add the basic custom css modal

---
+++ src/components/custom-css.vue
@@ -0,0 +1,38 @@
+<template>
+  <b-modal
+    ref="modal"
+    size="lg"
+    id="custom-css"
+    centered
+    hide-footer
+    title="Custom CSS"
+  >
+  <textarea v-bind:value="value" v-on:input="$emit('input', $event.target.value)" />
+  <b-btn @click="close">Close</b-btn>
+  </b-modal>
+</template>
+
+<script>
+export default {
+  props: ["value"],
+  data() {
+    return {}
+  },
+  methods: {
+    show() {
+      this.$refs.modal.show();
+    },
+    close() {
+      this.$refs.modal.hide();
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+textarea {
+  width: 100%;
+  height: 300px;
+  font-family: monospace;
+}
+</style>
