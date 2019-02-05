c7d64e2d54ba96cad2376d721c01c18fb6896bfb
David Callizaya <davidcallizaya@gmail.com>
2019-02-05 11:18:09 -0400

    Config formula field with monaco control.

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+const MonocoEditorPlugin = require('monaco-editor-webpack-plugin');
+
+// vue.config.js
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new MonocoEditorPlugin({
+        languages: ['javascript', 'typescript']
+      })
+    ]
+  }
+}
