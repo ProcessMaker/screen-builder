190d455d5c9a63917a1c1b9bebbc0738a6735db7
David Callizaya <davidcallizaya@gmail.com>
2019-02-11 10:01:59 -0400

    Add mixins to render colors

---
+++ src/mixins/HasColorProperty.js
@@ -0,0 +1,12 @@
+
+export default {
+  methods: {
+    elementCssClass(element) {
+      const css = [];
+      element.config.bgcolor ? css.push(element.config.bgcolor) : null;
+      element.config.color ? css.push(element.config.color) : null;
+      return css.join(' ');
+    },
+  }
+};
+
