89a47863c1aaf546bdbc65fb62fb36cea0dc7462
Robert Komaromi <robertkomaromi@gmail.com>
2019-03-11 14:53:58 -0400

    Make bootstrap an external/peer dependency

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,9 @@ module.exports = {
       new MonocoEditorPlugin({
         languages: ['javascript', 'typescript', 'css']
       })
-    ]
+    ],
+    externals: {
+      subtract: ['bootstrap']
+    }
   }
 }
