dde73c02609e9e36ad8319c27cb91a1c96f2a85f
Tuan N. Truong <t.truong@vehikl.com>
2019-03-19 16:15:14 -0400

    Move validations to footer

--- src/App.vue
+++ src/App.vue
@@ -1,195 +1,190 @@
 <template>
-    <div id="app" class="h-100 mb-3">
-        <nav class="navbar  navbar-expand-lg  navbar navbar-dark bg-dark">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" @click="mode = 'editor'" href="#">Editor</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" @click="mode = 'preview'" href="#">Preview</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" @click="openComputedProperties" href="#">Computed Properties</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" @click="openCustomCSS" href="#">Custom CSS</a>
-                </li>
-            </ul>
-        </nav>
-        <computed-properties v-model="computed" ref="computedProperties"></computed-properties>
-        <custom-CSS v-model="customCSS" ref="customCSS" :cssErrors="cssErrors"/>
-        <vue-form-builder ref="builder" @change="updateConfig" v-show="displayBuilder"/>
+  <div id="app" class="h-100 mb-3">
+    <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" @click="mode = 'editor'" href="#">Editor</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" @click="mode = 'preview'" href="#">Preview</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" @click="openComputedProperties" href="#">Computed Properties</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" @click="openCustomCSS" href="#">Custom CSS</a>
+        </li>
+      </ul>
+    </nav>
+    <computed-properties v-model="computed" ref="computedProperties"></computed-properties>
+    <custom-CSS v-model="customCSS" ref="customCSS" :cssErrors="cssErrors"/>
+    <vue-form-builder ref="builder" @change="updateConfig" v-show="displayBuilder"/>
 
-        <div id="preview" v-show="displayPreview" class="d-flex h-100">
-
-            <div id="data-input" class="w-25 border overflow-auto">
-                <div class="card-header">
-                    Data Input
-                </div>
-                <div class="card-body mb-5">
-                    <div class="alert"
-                         :class="{'alert-success': previewInputValid, 'alert-danger': !previewInputValid}">
-                        <span v-if="previewInputValid">Valid JSON Data Object</span>
-                        <span v-else>Invalid JSON Data Object</span>
-                    </div>
-                    <form-text-area rows="18" v-model="previewInput"></form-text-area>
-                </div>
-            </div>
-
-            <div id="renderer-container" class="w-50 p-4 pt-5 overflow-auto mb-5">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-sm">
-                            <vue-form-renderer ref="renderer"
-                                               v-model="previewData"
-                                               @submit="previewSubmit"
-                                               :config="config"
-                                               :computed="computed"
-                                               :custom-css="customCSS"
-                                               v-on:css-errors="cssErrors = $event"/>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div id="data-preview" class="w-25 border overflow-auto mb-5">
-                <div class="card-header">
-                    Data Preview
-                </div>
-                <vue-json-pretty :data="previewData" class="card-body"></vue-json-pretty>
-            </div>
+    <div id="preview" v-show="displayPreview" class="d-flex h-100">
+      <div id="data-input" class="w-25 border overflow-auto">
+        <div class="card-header">Data Input</div>
+        <div class="card-body mb-5">
+          <div
+            class="alert"
+            :class="{'alert-success': previewInputValid, 'alert-danger': !previewInputValid}"
+          >
+            <span v-if="previewInputValid">Valid JSON Data Object</span>
+            <span v-else>Invalid JSON Data Object</span>
+          </div>
+          <form-text-area rows="18" v-model="previewInput"></form-text-area>
         </div>
+      </div>
+
+      <div id="renderer-container" class="w-50 p-4 pt-5 overflow-auto mb-5">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm">
+              <vue-form-renderer
+                ref="renderer"
+                v-model="previewData"
+                @submit="previewSubmit"
+                :config="config"
+                :computed="computed"
+                :custom-css="customCSS"
+                v-on:css-errors="cssErrors = $event"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="data-preview" class="w-25 border overflow-auto mb-5">
+        <div class="card-header">Data Preview</div>
+        <vue-json-pretty :data="previewData" class="card-body"></vue-json-pretty>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-  import ComputedProperties from "./components/computed-properties.vue";
-  import CustomCSS from "./components/custom-css.vue";
-  import VueFormBuilder from "./components/vue-form-builder.vue";
-  import VueFormRenderer from "./components/vue-form-renderer.vue";
-  import VueJsonPretty from 'vue-json-pretty';
-  // Bring in our initial set of controls
+import ComputedProperties from './components/computed-properties.vue'
+import CustomCSS from './components/custom-css.vue'
+import VueFormBuilder from './components/vue-form-builder.vue'
+import VueFormRenderer from './components/vue-form-renderer.vue'
+import VueJsonPretty from 'vue-json-pretty'
+// Bring in our initial set of controls
 
-  import controlConfig from "./form-builder-controls";
-  import globalProperties from "./global-properties";
+import controlConfig from './form-builder-controls'
+import globalProperties from './global-properties'
 
+import { FormTextArea } from '@processmaker/vue-form-elements/src/components'
 
-  import {
-    FormTextArea,
-  } from "@processmaker/vue-form-elements/src/components";
-
-
-  export default {
-    name: "app",
+export default {
+    name: 'app',
     data() {
-      return {
-        mode: "editor",
-        // Computed properties
-        computed: [],
-        config: [
-          {
-            name: "Default",
+        return {
+            mode: 'editor',
+            // Computed properties
             computed: [],
-            items: []
-          }
-        ],
-        previewData: {},
-        previewInput: '{}',
-        customCSS: "",
-        cssErrors: '',
-      };
+            config: [
+                {
+                    name: 'Default',
+                    computed: [],
+                    items: []
+                }
+            ],
+            previewData: {},
+            previewInput: '{}',
+            customCSS: '',
+            cssErrors: ''
+        }
     },
     components: {
-      ComputedProperties,
-      CustomCSS,
-      VueFormBuilder,
-      VueFormRenderer,
-      VueJsonPretty,
-      FormTextArea
+        ComputedProperties,
+        CustomCSS,
+        VueFormBuilder,
+        VueFormRenderer,
+        VueJsonPretty,
+        FormTextArea
     },
     watch: {
-      mode(mode) {
-        if (mode === 'preview') {
-          this.previewData = this.previewInput ? JSON.parse(this.previewInput) : null;
+        mode(mode) {
+            if (mode === 'preview') {
+                this.previewData = this.previewInput ? JSON.parse(this.previewInput) : null
+            }
+        },
+        config() {
+            // Reset the preview data with clean object to start
+            this.previewData = {}
+        },
+        previewInput() {
+            if (this.previewInputValid) {
+                // Copy data over
+                this.previewData = JSON.parse(this.previewInput)
+            } else {
+                this.previewData = {}
+            }
         }
-      },
-      config() {
-        // Reset the preview data with clean object to start
-        this.previewData = {}
-      },
-      previewInput() {
-        if (this.previewInputValid) {
-          // Copy data over
-          this.previewData = JSON.parse(this.previewInput)
-        } else {
-          this.previewData = {}
-        }
-      }
     },
     computed: {
-      previewInputValid() {
-        try {
-          JSON.parse(this.previewInput)
-          return true
-        } catch (err) {
-          return false
+        previewInputValid() {
+            try {
+                JSON.parse(this.previewInput)
+                return true
+            } catch (err) {
+                return false
+            }
+        },
+        displayBuilder() {
+            return this.mode === 'editor'
+        },
+        displayPreview() {
+            return this.mode === 'preview'
         }
-      },
-      displayBuilder() {
-        return this.mode === 'editor';
-      },
-      displayPreview() {
-        return this.mode === 'preview';
-      }
     },
     mounted() {
-      // Iterate through our initial config set, calling this.addControl
-      for (var i = 0; i < controlConfig.length; i++) {
-        //Add new properties global to inspector
-        Array.prototype.push.apply(controlConfig[i].control.inspector, globalProperties[0].inspector);
-        this.addControl(
-          controlConfig[i].control,
-          controlConfig[i].rendererComponent,
-          controlConfig[i].rendererBinding,
-          controlConfig[i].builderComponent,
-          controlConfig[i].builderBinding
-        )
-      }
+        // Iterate through our initial config set, calling this.addControl
+        for (var i = 0; i < controlConfig.length; i++) {
+            //Add new properties global to inspector
+            Array.prototype.push.apply(controlConfig[i].control.inspector, globalProperties[0].inspector)
+            this.addControl(
+                controlConfig[i].control,
+                controlConfig[i].rendererComponent,
+                controlConfig[i].rendererBinding,
+                controlConfig[i].builderComponent,
+                controlConfig[i].builderBinding
+            )
+        }
     },
     methods: {
-      openComputedProperties() {
-        this.$refs.computedProperties.show();
-      },
-      openCustomCSS() {
-        this.$refs.customCSS.show();
-      },
-      updateConfig(newConfig) {
-        this.config = newConfig
-      },
-      updatePreview(data) {
-        this.previewData = data
-      },
-      previewSubmit() {
-        alert("Preview Form was Submitted")
-      },
-      addControl(control, rendererComponent, rendererBinding, builderComponent, builderBinding) {
-        // Add it to the renderer
-        this.$refs.renderer.$options.components[rendererBinding] = rendererComponent;
-        // Add it to the form builder
-        this.$refs.builder.addControl(control, builderComponent, builderBinding)
-      }
+        openComputedProperties() {
+            this.$refs.computedProperties.show()
+        },
+        openCustomCSS() {
+            this.$refs.customCSS.show()
+        },
+        updateConfig(newConfig) {
+            this.config = newConfig
+        },
+        updatePreview(data) {
+            this.previewData = data
+        },
+        previewSubmit() {
+            alert('Preview Form was Submitted')
+        },
+        addControl(control, rendererComponent, rendererBinding, builderComponent, builderBinding) {
+            // Add it to the renderer
+            this.$refs.renderer.$options.components[rendererBinding] = rendererComponent
+            // Add it to the form builder
+            this.$refs.builder.addControl(control, builderComponent, builderBinding)
+        }
     }
-  };
+}
 </script>
 
 <style lang="scss">
-    @import "~bootstrap/dist/css/bootstrap";
+@import '~bootstrap/dist/css/bootstrap';
 
-    html,
-    body {
-        height: 100%;
-        min-height: 100%;
-        max-height: 100%;
-        overflow: hidden;
-    }
+html,
+body {
+    height: 100%;
+    min-height: 100%;
+    max-height: 100%;
+    overflow: hidden;
+}
 </style>
