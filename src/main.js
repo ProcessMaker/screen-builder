478530f22174b14bff16116cdf0e34a0abe6aa65
Nolan Ehrstrom <github@nolanpro.com>
2019-04-02 15:30:01 -0700

    Allow standalone to pass through english string translations

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 Vue.config.productionTip = false;
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import i18next from 'i18next';
+import VueI18Next from '@panter/vue-i18next';
+
+// Allow strings to be wrapped in $t(...) for translating
+// outside this package. This standalone app just returns
+// the English string
+Vue.use(VueI18Next)
+i18next.init({lng: 'en'})
+Vue.mixin({ i18n: new VueI18Next(i18next) })
 
 new Vue({
   render: h => h(App)
