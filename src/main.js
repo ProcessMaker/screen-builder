958f2db36aa3b27843b16695b0ca90bc61227fae
Robert Komaromi <robertkomaromi@gmail.com>
2019-04-17 15:21:56 -0400

    Move form elements CSS import

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ Vue.config.productionTip = false;
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import i18next from 'i18next';
 import VueI18Next from '@panter/vue-i18next';
-
+import '@processmaker/vue-form-elements/dist/vue-form-elements.css';
 // Allow strings to be wrapped in $t(...) for translating
 // outside this package. This standalone app just returns
 // the English string
