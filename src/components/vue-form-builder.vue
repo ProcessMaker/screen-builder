74449a90457facd7d602e360b5a701c37e4f6c3d
Ryan Cooley <ryan.cooley@processmaker.com>
2019-05-17 10:01:44 -0700

    Revert "Removing “drag element here” card"
    This reverts commit 66c96478547a5108faf99b39299b5acbee8d5450.

--- src/components/vue-form-builder.vue
+++ src/components/vue-form-builder.vue
@@ -103,6 +103,11 @@
                         </button>
                     </div>
                 </div>
+                <div class="card">
+                    <div class="card-body text-center">
+                        {{ $t('Drag an element here') }}
+                    </div>
+                </div>
             </draggable>
         </div>
         <div class="form-builder__inspector col-3 shadow-sm pl-0 pr-0 mr-3 card">
