4b0a6f50347b0cc5b264d9e76f14bba96774caff
Tuan N. Truong <t.truong@vehikl.com>
2019-05-22 09:44:23 -0400

    Refactor height for preview inspector

--- src/App.vue
+++ src/App.vue
@@ -56,35 +56,32 @@
                 Inspector
               </b-card-header>
 
-              <b-card-body class="p-0 h-100">
-                <div class="h-auto">
+              <b-card-body class="p-0 overflow-auto">
                   <b-button v-b-toggle.dataInput variant="outline"
-                    class="text-left card-header d-flex align-items-center w-100"
+                    class="text-left card-header d-flex align-items-center w-100 shadow-none"
                     @click="showDataInput = !showDataInput">
                     <i class="fas fa-file-import mr-2"></i>
                       {{ $t('Data Input') }}
                     <i class="fas fa-angle-down ml-auto" :class="{ 'fas fa-angle-right' : !showDataInput }"></i>
                   </b-button>
 
-                  <b-collapse id="dataInput" visible class="overflow-auto">
-                    <form-text-area class="data-height dataInput" v-model="previewInput" rows="10"></form-text-area>
+                  <b-collapse id="dataInput" visible class="">
+                    <form-text-area class="dataInput mb-0" v-model="previewInput" rows="10"></form-text-area>
                   </b-collapse>
-                </div>
 
-                <div class="h-auto">
+
                   <b-button v-b-toggle.dataPreview variant="outline"
-                    class="text-left card-header d-flex align-items-center w-100 border-top"
+                    class="text-left card-header d-flex align-items-center w-100 border-top shadow-none"
+                    data-toggle="collapse"
                     @click="showDataPreview = !showDataPreview">
                     <i class="fas fa-file-code mr-2"></i>
                       {{ $t('Data Preview') }}
                     <i class="fas fa-angle-down ml-auto" :class="{ 'fas fa-angle-right' : !showDataPreview }"></i>
                   </b-button>
 
-                  <b-collapse id="dataPreview" visible class="mt-2 overflow-auto">
-                    <vue-json-pretty  :data="previewData" class="p-2 h-50"></vue-json-pretty>
+                  <b-collapse id="dataPreview" visible class="mt-2">
+                    <vue-json-pretty  :data="previewData" class="p-2"></vue-json-pretty>
                   </b-collapse>
-                </div>
-
               </b-card-body>
             </b-card>
           </b-col>
