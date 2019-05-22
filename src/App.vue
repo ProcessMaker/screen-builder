2302d04dbf8a12fab90dedda4bfb948188182b36
Tuan N. Truong <t.truong@vehikl.com>
2019-05-22 13:28:53 -0400

    Indenting

--- src/App.vue
+++ src/App.vue
@@ -57,21 +57,21 @@
               </b-card-header>
 
               <b-card-body class="p-0 overflow-auto">
-                  <div class="h-50">
-                    <b-button v-b-toggle.dataInput variant="outline"
-                      class="text-left card-header d-flex align-items-center w-100 shadow-none"
-                      @click="showDataInput = !showDataInput">
-                      <i class="fas fa-file-import mr-2"></i>
-                        {{ $t('Data Input') }}
-                      <i class="fas fa-angle-down ml-auto" :class="{ 'fas fa-angle-right' : !showDataInput }"></i>
-                    </b-button>
+                <div class="h-50">
+                  <b-button v-b-toggle.dataInput variant="outline"
+                    class="text-left card-header d-flex align-items-center w-100 shadow-none"
+                    @click="showDataInput = !showDataInput">
+                    <i class="fas fa-file-import mr-2"></i>
+                      {{ $t('Data Input') }}
+                    <i class="fas fa-angle-down ml-auto" :class="{ 'fas fa-angle-right' : !showDataInput }"></i>
+                  </b-button>
 
                   <b-collapse id="dataInput" visible class="">
                     <form-text-area class="dataInput mb-0" v-model="previewInput" rows="12"></form-text-area>
                   </b-collapse>
-                  </div>
+                </div>
 
-                  <div class="h-50">
+                <div class="h-50">
                   <b-button v-b-toggle.dataPreview variant="outline"
                     class="text-left card-header d-flex align-items-center w-100 shadow-none"
                     data-toggle="collapse"
@@ -84,7 +84,7 @@
                   <b-collapse id="dataPreview" visible class="mt-2">
                     <vue-json-pretty  :data="previewData" class="p-2"></vue-json-pretty>
                   </b-collapse>
-                  </div>
+                </div>
               </b-card-body>
             </b-card>
           </b-col>
