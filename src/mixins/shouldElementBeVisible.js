9b3e91adeb7c1bf3966dd30f95e04f043ee53431
Robert Komaromi <robertkomaromi@gmail.com>
2019-06-10 12:52:51 -0400

    Fix element visibility rendering

---
+++ src/mixins/shouldElementBeVisible.js
@@ -0,0 +1,19 @@
+import { Parser } from 'expr-eval';
+
+export default {
+  methods: {
+    shouldElementBeVisible(element) {
+      const { conditionalHide } = element.config;
+
+      if (!conditionalHide) {
+        return true;
+      }
+
+      try {
+        return !!Parser.evaluate(conditionalHide, this.transientData);
+      } catch (error) {
+        return false;
+      }
+    },
+  }
+}
