3712156ce57e757d7b088bdd86a5a25abbad0d17
Tuan N. Truong <t.truong@vehikl.com>
2019-06-24 10:56:33 -0400

    Add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,23 @@ module.exports = {
     'plugin:vue/essential',
     'eslint:recommended'
   ],
-  rules: {},
+  rules: {
+    semi: ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+    indent: ['error', 2, { SwitchCase: 1 }],
+    quotes: ['error', 'single'],
+    'no-debugger': 'off',
+    'object-shorthand': 'error',
+    'space-before-function-paren': ['error', 'never'],
+    'keyword-spacing': 'error',
+    'vue/html-indent': ['error', 2, { alignAttributesVertically: false }],
+    'vue/html-closing-bracket-newline': ['error', {
+      singleline: 'never',
+      multiline: 'always',
+    }],
+    'vue/html-self-closing': 'error',
+    'vue/mustache-interpolation-spacing': 'error',
+  },
   parserOptions: {
     parser: 'babel-eslint'
   }
