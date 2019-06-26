ca7fc82ab7ee74c50ccf6f814691509c54ff4a13
Robert Komaromi <robertkomaromi@gmail.com>
2019-06-26 15:07:09 -0400

    WIP use single component for data source stuffs

--- src/components/inspector/data-source.vue
+++ src/components/inspector/data-source.vue
@@ -2,40 +2,46 @@
   <div>
     <label for="data-sources">{{ $t('Data Source') }}</label>
     <b-form-select id="data-sources" v-model="dataSource" :options="dataSources"/>
-    <small class="form-text text-muted">Data source to populate select</small>
+    <small class="form-text text-muted mb-3">Data source to populate select</small>
 
     <div v-if="dataSource === 'Provide Data'">
       <label for="json-data">{{ $t('JSON Data') }}</label>
-      <b-form-textarea id="json-data" rows="8" v-model="jsonData"/>
+      <b-form-textarea class="mb-3" id="json-data" rows="8" v-model="jsonData"/>
     </div>
 
     <div v-if="dataSource === 'Data Object'">
       <label for="data-name">{{ $t('Data Name') }}</label>
       <b-form-input id="data-name" v-model="dataName"/>
-      <small class="form-text text-muted">Data source to populate select</small>
+      <small class="form-text text-muted mb-3">Data source to populate select</small>
     </div>
 
-    <div>
-      <label for="key">{{ $t('Key') }}</label>
-      <b-form-input id="key" v-model="key"/>
-      <small class="form-text text-muted">Field to save to the data object</small>
+    <label for="key">{{ $t('Key') }}</label>
+    <b-form-input id="key" v-model="key"/>
+    <small class="form-text text-muted mb-3">Field to save to the data object</small>
 
-      <label for="value">{{ $t('Value') }}</label>
-      <b-form-input id="value" v-model="value"/>
-      <small class="form-text text-muted">Field to show in the select box</small>
-    </div>
+    <label for="value">{{ $t('Value') }}</label>
+    <b-form-input id="value" v-model="value"/>
+    <small class="form-text text-muted mb-3">Field to show in the select box</small>
 
-    <div v-if="dataSource === 'Data Object'">
-      <label for="pmql-query">{{ $t('PMQL Query (optional)') }}</label>
-      <b-form-textarea id="json-data" rows="4" v-model="pmqlQuery"/>
-      <small class="form-text text-muted">Advanced data search</small>
-    </div>
+    <label for="pmql-query">{{ $t('PMQL Query (optional)') }}</label>
+    <b-form-textarea id="json-data" rows="4" v-model="pmqlQuery"/>
+    <small class="form-text text-muted">Advanced data search</small>
   </div>
 </template>
 
 <script>
+// export const dataSources = [
+//   { value: 'provideData', content: 'Provide Data' },
+//   { value: 'dataObject', content: 'Data Object' },
+// ];
+
+// export const dataSourceValues = dataSources.reduce((values, source) => {
+//   values[source.value] = source.value;
+//   return values;
+// }, {});
+
 export default {
-  props: ['options', 'data'],
+  props: ['options'],
   model: {
     prop: 'options',
     event: 'change',
@@ -57,14 +63,32 @@ export default {
     };
   },
   watch: {
-    parsedOptions(parsedOptions) {
-      this.$emit('change', parsedOptions);
+    // parsedOptions(parsedOptions) {
+    //   this.$emit('change', parsedOptions);
+    // },
+    dataSource() {
+      this.jsonData = '';
+      this.dataName = '';
     },
     options(options) {
-      this.jsonData = JSON.stringify(options);
+      console.log('Input options changed!');
+
+      if (Array.isArray(options)) {
+        this.jsonData = JSON.stringify(options);
+      } else {
+        this.jsonData = options.json;
+        this.dataName = options.name;
+        this.key = options.key;
+        this.value = options.value;
+        this.pmqlQuery = options.pmqlQuery;
+      }
     },
-    dataName(dataName) {
-      this.jsonData = JSON.stringify(this.data[dataName]);
+    // dataName(dataName) {
+    //   this.jsonData = JSON.stringify(this.data[dataName]);
+    // },
+    dataObjectOptions(dataObjectOptions) {
+      console.log('New options emitted!');
+      this.$emit('change', dataObjectOptions);
     },
   },
   computed: {
@@ -74,8 +98,17 @@ export default {
           return this.jsonData;
 
         case 'Data Object':
-          return this.data[this.dataName];
+          return this.dataObjectOptions;
       }
+    },
+    dataObjectOptions() {
+      return {
+        json: this.jsonData,
+        name: this.dataName,
+        key: this.key,
+        value: this.value,
+        pmqlQuery: this.pmqlQuery,
+      };
     },
     parsedOptions() {
       try {
