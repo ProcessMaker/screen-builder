cb77d043a237f0bc90b2eb08c9b78fc65064ffb5
Robert Komaromi <robertkomaromi@gmail.com>
2019-07-18 12:06:18 -0400

    Force use of data source

--- src/components/inspector/index.js
+++ src/components/inspector/index.js
@@ -4,4 +4,3 @@ export { default as FormMultiselect } from './form-multiselect';
 export { default as ImageUpload } from './image-upload.vue';
 export { default as OptionsList } from './options-list';
 export { default as PageSelect } from './page-select';
-export { default as DataSource } from './data-source.vue';
