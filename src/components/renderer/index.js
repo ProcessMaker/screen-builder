924c0cf882943a2f67523e141d98962100549d5b
Robert Komaromi <robertkomaromi@gmail.com>
2019-08-06 15:47:41 -0400

    Include static form record list in exports

--- src/components/renderer/index.js
+++ src/components/renderer/index.js
@@ -1,4 +1,5 @@
 export { default as FormButton } from './form-button';
 export { default as FormImage } from './form-image';
 export { default as FormRecordList } from './form-record-list';
+export { default as FormRecordListStatic } from './form-record-list-static.vue';
 export { default as FormText } from './form-text';
