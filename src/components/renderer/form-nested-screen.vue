5f4a22ec91332c76332cb4bd6993407f7bc2db10
David Callizaya <davidcallizaya@gmail.com>
2020-01-20 09:51:55 -0400

    Add nested screen control

---
+++ src/components/renderer/form-nested-screen.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="form-group">
+    hello world
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ['id'],
+  computed: {
+  },
+  methods: {
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
