68d6c1ae8774af4cc0cfee9d45e305f7fd36a533
David Callizaya <davidcallizaya@gmail.com>
2020-01-23 16:43:07 -0400

    Add nested screen tests

---
+++ tests/e2e/specs/NestedScreen.js
@@ -0,0 +1,32 @@
+describe('Test Nested screen', () => {
+  it('With a variable defined', () => {
+    cy.visit('/');
+    cy.get('#controls > div:contains("Nested Screen")').drag('#screen-container div:contains("Drag an element here")', 'bottom');
+    cy.get('#screen-container div.card').click();
+    cy.get('#Variable input[name="DataVariable"]').type('person');
+    cy.get('#Variable div.multiselect').click();
+    cy.get('#Variable span:contains("Sub screen example"):first').click();
+    cy.get('#app button:contains("Preview"):first').click();
+    cy.get('#person input[name="firstname"]').type('Alan');
+    cy.get('#person input[name="lastname"]').type('Turing');
+    cy.get('#showDataPreview').should((a) => {
+      const person = JSON.parse(a.text().split('{...}').join(''));
+      expect(person).to.eql({ person: { firstname: 'Alan', lastname: 'Turing' }});
+    });
+  });
+
+  it('Without a variable defined', () => {
+    cy.visit('/');
+    cy.get('#controls > div:contains("Nested Screen")').drag('#screen-container div:contains("Drag an element here")', 'bottom');
+    cy.get('#screen-container div.card').click();
+    cy.get('#Variable div.multiselect').click();
+    cy.get('#Variable span:contains("Sub screen example"):first').click();
+    cy.get('#app button:contains("Preview"):first').click();
+    cy.get('#preview input[name="firstname"]').type('Alan');
+    cy.get('#preview input[name="lastname"]').type('Turing');
+    cy.get('#showDataPreview').should((a) => {
+      const person = JSON.parse(a.text().split('{...}').join(''));
+      expect(person).to.eql({ firstname: 'Alan', lastname: 'Turing' });
+    });
+  });
+});
