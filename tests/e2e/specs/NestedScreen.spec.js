0b87ff1382cf0ce18fbfca563025f8ba62705191
David Callizaya <davidcallizaya@gmail.com>
2020-01-27 12:46:42 -0400

    Improve test with better cy selectors

--- tests/e2e/specs/NestedScreen.spec.js
+++ tests/e2e/specs/NestedScreen.spec.js
@@ -1,15 +1,15 @@
 describe('Nested screen', () => {
   it('With a variable defined', () => {
     cy.visit('/');
-    cy.get('#controls > div:contains("Nested Screen")').drag('#screen-container div:contains("Drag an element here")', 'bottom');
-    cy.get('#screen-container div.card').click();
-    cy.get('#Variable input[name="DataVariable"]').type('person');
-    cy.get('#Variable div.multiselect').click();
-    cy.get('#Variable span:contains("Sub screen example"):first').click();
-    cy.get('#app button:contains("Preview"):first').click();
-    cy.get('#person input[name="firstname"]').type('Alan');
-    cy.get('#person input[name="lastname"]').type('Turing');
-    cy.get('#showDataPreview').should((a) => {
+    cy.get('[data-cy=controls-FormNestedScreen]').drag('[data-cy=screen-drop-zone]', 'bottom');
+    cy.get('[data-cy=screen-element-container]').click();
+    cy.get('[data-cy=inspector-DataVariable]').type('person');
+    cy.get('[data-cy=inspector-SelectScreen] div.multiselect').click();
+    cy.get('[data-cy=inspector-SelectScreen] span:contains("Sub screen example"):first').click();
+    cy.get('[data-cy=mode-preview]').click();
+    cy.get('[data-cy=preview] input[name="firstname"]').type('Alan');
+    cy.get('[data-cy=preview] input[name="lastname"]').type('Turing');
+    cy.get('[data-cy=preview-data-content]').should((a) => {
       const person = JSON.parse(a.text().split('{...}').join(''));
       expect(person).to.eql({ person: { firstname: 'Alan', lastname: 'Turing' }});
     });
@@ -17,14 +17,14 @@ describe('Nested screen', () => {
 
   it('Without a variable defined', () => {
     cy.visit('/');
-    cy.get('#controls > div:contains("Nested Screen")').drag('#screen-container div:contains("Drag an element here")', 'bottom');
-    cy.get('#screen-container div.card').click();
-    cy.get('#Variable div.multiselect').click();
-    cy.get('#Variable span:contains("Sub screen example"):first').click();
-    cy.get('#app button:contains("Preview"):first').click();
-    cy.get('#preview input[name="firstname"]').type('Alan');
-    cy.get('#preview input[name="lastname"]').type('Turing');
-    cy.get('#showDataPreview').should((a) => {
+    cy.get('[data-cy=controls-FormNestedScreen]').drag('[data-cy=screen-drop-zone]', 'bottom');
+    cy.get('[data-cy=screen-element-container]').click();
+    cy.get('[data-cy=inspector-SelectScreen] div.multiselect').click();
+    cy.get('[data-cy=inspector-SelectScreen] span:contains("Sub screen example"):first').click();
+    cy.get('[data-cy=mode-preview]').click();
+    cy.get('[data-cy=preview] input[name="firstname"]').type('Alan');
+    cy.get('[data-cy=preview] input[name="lastname"]').type('Turing');
+    cy.get('[data-cy=preview-data-content]').should((a) => {
       const person = JSON.parse(a.text().split('{...}').join(''));
       expect(person).to.eql({ firstname: 'Alan', lastname: 'Turing' });
     });
