a1d23565931d415be4cb92724ea7517eea64a5e8
Robert Komaromi <robertkomaromi@gmail.com>
2020-02-27 14:13:25 -0500

    Remove form causing page reload

--- src/components/renderer/form-loop.vue
+++ src/components/renderer/form-loop.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="form-group">
-    <div v-for="loopIndex in times" :key="loopIndex">
-      <form>
-        <vue-form-renderer
-          @submit="submit"
-          :data="getMatrixValue(loopIndex)"
-          @update="setMatrixValue(loopIndex, $event)"
-          :config="rendererConfig"
-          :computed="null"
-          :custom-css="null"
-          :watchers="null"
-        />
-      </form>
-    </div>
+    <vue-form-renderer
+      v-for="loopIndex in times"
+      :key="loopIndex"
+      :data="getMatrixValue(loopIndex)"
+      :config="rendererConfig"
+      :computed="null"
+      :custom-css="null"
+      :watchers="null"
+      @submit="submit"
+      @update="setMatrixValue(loopIndex, $event)"
+    />
   </div>
 </template>
 
