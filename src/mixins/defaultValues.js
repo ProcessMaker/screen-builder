aa87bbb5c42663699f393d9b6b7d7adaee0edf99
Nolan Ehrstrom <github@nolanpro.com>
2020-04-06 08:36:55 -0700

    Move default value calculator to a mixin

--- src/mixins/defaultValues.js
+++ src/mixins/defaultValues.js
@@ -1,64 +1,143 @@
 import Mustache from 'mustache';
 
 export default {
-  data() {
-    return {
-      defaultValueApplied: false,
-      defaultsInitialized: false,
+  props: {
+    showDebug: {
+      type: Boolean,
+      default: false, 
     }
   },
+  data() {
+    return {
+      defaultsInitialized: false,
+      defaultsFormData: {},
+      activeDefaultValues: [],
+      lastSetTransientData: null,
+    }
+  },
+  watch: {
+    transientData(val) {
+      this.debug("---> transientData", JSON.stringify(val));
+      if (this.defaultsInitialized) {
+        this.update(val);
+      }
+    },
+    config: {
+      handler() {
+        this.debug("---> config");
+        this.initializeDefaultValues();
+        this.update(this.transientData);
+      },
+      deep: true,
+      immediate: true,
+    },
+    mode() {
+      this.initializeDefaultValues();
+      this.update(this.transientData);
+    },
+  },
   methods: {
+    debug() {
+      if (this.showDebug) {
+        console.log(this.isLoop, ...arguments);
+      }
+    },
+    update(data) {
+      this.debug("update()", JSON.stringify(data));
+
+      if (this.isEqual(data, this.lastSetTransientData)) {
+        return;
+      }
+
+      this.defaultsFormData = _.cloneDeep(data);
+      this.updateDefaultValues();
+      // Run this again so previous set defaults get later updates
+      this.updateDefaultValues();
+      if (this.isEqual(this.transientData, this.defaultsFormData)) {
+        return
+      }
+      this.lastSetTransientData = _.cloneDeep(this.defaultsFormData);
+      this.transientData = _.cloneDeep(this.defaultsFormData);
+    },
     initializeDefaultValues() {
-      this.defaultsInitialized = false;
-      this.activeDefaultValues = {};
-      setTimeout(() => {
-        if (typeof this.config !== undefined) {
-          this.itemsWithDefaultValues(this.config)
-            .forEach(item => {
-              if (item.config.defaultValue) {
-                const path = this.getValidPath(item.config.name);
-                this.applyDefaultValues(path, item);
-              }
-          });
-        }
-        this.defaultsInitialized = true;
-      }, 100);
+      this.count = 0;
+      this.lastSetTransientData = {};
+      this.activeDefaultValues = [];
+      this.itemsWithDefaultValues(this.config);
+      this.defaultsInitialized = true;
     },
     itemsWithDefaultValues(items) {
       if (!Array.isArray(items)) {
-        return [];
+        return;
       }
 
-      const results = [];
       items.forEach(item => {
-        // Loop will run this inside each iteration, so skip if it's a loop
-        if (item.component === 'FormLoop') {
+        if (item.component === 'FormLoop' || item.component === 'FormNestedScreen') {
           return;
         }
 
         if (Array.isArray(item)) {
-          this.itemsWithDefaultValues(item)
-            .forEach(result => results.push(result));
+          this.itemsWithDefaultValues(item);
 
         } else if ('items' in item) {
-          this.itemsWithDefaultValues(item.items)
-            .forEach(result => results.push(result));
+          this.itemsWithDefaultValues(item.items);
 
         } else {
           if (item.config && item.config.defaultValue) {
             if (typeof item.config.defaultValue === 'object') {
               if (item.config.defaultValue.value) {
-                results.push(item);
+                this.addToActiveDefaultValues(item);
               }
             } else {
-              results.push(item);
+              this.addToActiveDefaultValues(item);
             }
           }
         }
       });
-      return results;
     },
-    applyDefaultValues(path, item) {
+    addToActiveDefaultValues(item) {
+      this.activeDefaultValues.push({
+        path: this.getValidPath(item.config.name),
+        item: item,
+        setValue: null,
+        inactive: false,
+      });
+    },
+
+    updateDefaultValues() {
+      if (!this.defaultsInitialized) {
+        return;
+      }
+
+      this.activeDefaultValues.forEach(({path, item, setValue, inactive}, index) => {
+        if (inactive) {
+          return;
+        }
+
+        const current = _.get(this.defaultsFormData, path, null);
+        
+        // Check if any fields were modified from their original default values
+        // If so, we no longer want to apply any defaults.
+        if (current === null || setValue === null || this.isEqual(current, setValue)) {
+          // No changes, so we can apply the default values
+          this.applyDefaultValue(path, item);
+          this.activeDefaultValues[index].setValue = this.defaultsFormData[path];
+
+        } else {
+          // There are changes, so lets remove it from our list of active elements
+          this.activeDefaultValues[index].inactive = true;
+        }
+      });
+    },
+
+    isEqual(a, b) {
+      if (typeof a === 'object') {
+        return _.isEqual(a, b);
+      }
+      return String(a) === String(b);
+    },
+
+    applyDefaultValue(path, item) {
       if (typeof item.config.defaultValue === 'string') {
         this.setBasicDefaultValue(path, item.config.defaultValue);
       } else {
@@ -72,52 +151,19 @@ export default {
           this.setBasicDefaultValue(path, value);
         }
       }
-
-      this.defaultValueApplied = true;
-      this.$nextTick(() => this.defaultValueApplied = false);
-
-      const setValue = _.clone(this.model[path]);
-      this.activeDefaultValues[path] = { item, setValue };
     },
-    updateDefaultValues() {
-      if (!this.defaultsInitialized) {
-        return;
-      }
 
-      // Prevent endless parsing of dynamic defaults, like `new Date()`
-      if (this.defaultValueApplied) {
-        return;
-      }
-
-      for (const path in this.activeDefaultValues) {
-        const current = this.model[path];
-        const cached = this.activeDefaultValues[path].setValue;
-
-        // Check if any fields were modified from their original default values
-        // If so, we no longer want to apply any defaults.
-        if (_.isEqual(current, cached)) {
-          // No changes, so we can apply the default values
-          this.applyDefaultValues(path, this.activeDefaultValues[path].item);
-        } else {
-          // There are changes, so lets remove it from our list of active elements
-          // console.log(this.isLoop, "D2", path);
-          this.$delete(this.activeDefaultValues, path);
-        }
-
-      };
-    },
     setBasicDefaultValue(path, value) {
-      const result = Mustache.render(value, this.transientData);
-      if (!_.isEqual(this.model[path], result)) {
-        this.model[path] = result;
-      }
+      let result = value;
+      try {
+        result = Mustache.render(value, this.defaultsFormData);
+      } catch(e) { }
+      this.defaultsFormData[path] = result;
     },
     setJsDefaultValue(path, value) {
       const func = new Function(value);
-      const result = func.bind(_.clone(this.transientData))();
-      if (!_.isEqual(this.model[path], result)) {
-        this.model[path] = result;
-      }
+      const result = func.bind(_.clone(this.defaultsFormData))();
+      this.defaultsFormData[path] = result;
     },
 
   }
