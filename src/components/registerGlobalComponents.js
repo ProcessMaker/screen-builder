97a49eb6ed63c4f9d5de67ede35076f007950981
David Callizaya <davidcallizaya@gmail.com>
2020-06-18 11:15:42 -0400

    Complete Loop Add/Remove

--- src/components/registerGlobalComponents.js
+++ src/components/registerGlobalComponents.js
@@ -6,6 +6,7 @@ import FormMultiColumn from '../components/renderer/form-multi-column';
 import NewFormMultiColumn from '../components/renderer/new-form-multi-column';
 import FormNestedScreen from '../components/renderer/form-nested-screen';
 import ScreenRenderer from '../components/screen-renderer';
+import AddLoopRow from '../components/renderer/add-loop-row';
 
 // Container elements must be registered globally because they
 // could rely on eachother.
@@ -16,3 +17,4 @@ Vue.component('FormMultiColumn', FormMultiColumn);
 Vue.component('NewFormMultiColumn', NewFormMultiColumn);
 Vue.component('FormNestedScreen', FormNestedScreen);
 Vue.component('ScreenRenderer', ScreenRenderer);
+Vue.component('AddLoopRow', AddLoopRow);
