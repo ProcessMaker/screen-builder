61a5945c5e52cc5d881d741172e4351c06cce8c4
David Callizaya <davidcallizaya@gmail.com>
2020-06-18 15:25:47 -0400

    Test validation rule and placeholder

--- tests/e2e/specs/FormInput.js
+++ tests/e2e/specs/FormInput.js
@@ -37,13 +37,17 @@ describe('Form Input', () => {
     cy.visit('/');
     cy.get('[data-cy=controls-FormInput]').drag('[data-cy=screen-drop-zone]', 'bottom');
     cy.get('[data-cy=screen-element-container]').click();
-    cy.get('[data-cy=inspector-validation] button').click();
-    cy.setMultiselect('[data-cy=inspector-validation] .multiselect', 'Email');
-    cy.get('[data-cy=inspector-validation] button:contains("Save")').click();
+    cy.get('[data-cy=inspector-readonly]').click();
     cy.get('[data-cy=mode-preview]').click();
-    cy.get('[data-cy=preview-content] [name=form_input_1]').type('123');
-    cy.get('[data-cy=preview-content]').should('contain.html', 'format is invalid');
-    cy.get('[data-cy=preview-content] [name=form_input_1]').clear().type('admin@processmaker.com');
-    cy.get('[data-cy=preview-content]').should('not.contain.html', 'format is invalid');
+    cy.get('[data-cy=preview-content] [name=form_input_1]').should('have.attr', 'readonly');
+  });
+  it('Placeholder', () => {
+    cy.visit('/');
+    cy.get('[data-cy=controls-FormInput]').drag('[data-cy=screen-drop-zone]', 'bottom');
+    cy.get('[data-cy=screen-element-container]').click();
+    cy.get('[data-cy=accordion-Configuration]').click();
+    cy.get('[data-cy=inspector-placeholder]').clear().type('enter text here');
+    cy.get('[data-cy=mode-preview]').click();
+    cy.get('[data-cy=preview-content] [name=form_input_1]').should('have.attr', 'placeholder', 'enter text here');
   });
 });
