db2be605c8540a21a744e54a61f84c20886112ed
David Callizaya <davidcallizaya@gmail.com>
2020-06-18 13:08:29 -0400

    Fix Image Control

--- src/mixins/extensions/LoadFieldComponents.js
+++ src/mixins/extensions/LoadFieldComponents.js
@@ -1,20 +1,18 @@
 export default {
   methods: {
-    loadFieldProperties({ properties, element }) {
+    loadFieldProperties({ properties, element, componentName }) {
       properties.class = this.elementCssClass(element);
       properties[':validation-data'] = 'vdata';
-      this.registerVariable(element.config.name, element.config);
-      if (element.config.name) {
+      if (element.config.name && componentName !== 'FormImage' ) {
+        this.registerVariable(element.config.name, element.config);
         properties['v-model'] = `${element.config.name}`;
+      } else if (componentName === 'FormImage') {
+        this.registerVariable(element.config.variableName, element.config);
       }
       properties[':ancestor-screens'] = '$parent && $parent.ancestorScreens';
       properties.name = element.config.name !== undefined ? element.config.name : null;
       properties.disabled = element.config.interactive || element.config.disabled;
       properties[':form-config'] = '$parent && $parent.definition.config';
-      // events
-      //properties.input="dataChanged";
-      //properties.submit="submit";
-      //properties.pageNavigate = '$parent.pageNavigate';
     },
   },
   mounted() {
