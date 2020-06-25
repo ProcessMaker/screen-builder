1beb1f713a358f2b47db448aee33a185ef9c2303
David Callizaya <davidcallizaya@gmail.com>
2020-06-25 14:38:37 -0400

    Complete computed properties tests

--- tests/e2e/specs/ComputedFields.spec.js
+++ tests/e2e/specs/ComputedFields.spec.js
@@ -1,5 +1,58 @@
 describe('Computed fields', () => {
 
+  it('CRUD of computed fields', () => {
+    cy.visit('/');
+    // Create a calculated property
+    cy.get('[data-cy="topbar-calcs"]').click();
+    cy.get('[data-cy="calcs-add-property"]').click();
+    cy.get('[data-cy="calcs-property-name"]').clear().type('form_input_1');
+    cy.get('[data-cy="calcs-property-description"]').clear().type('form_input_1 = form_input_2 ^ 2');
+    cy.get('[data-cy="calcs-switch-formula"]').click();
+    cy.get('[data-cy="calcs-property-formula"]').clear().type('pow(form_input_2, 2)');
+    cy.get('[data-cy="calcs-button-save"]').click();
+    cy.get('[data-cy="calcs-table"]').should('contain.text', 'form_input_1 = form_input_2 ^ 2');
+    cy.get('[data-cy="calcs-modal"] .close').click();
+
+    // Edit the created calculated property
+    cy.get('[data-cy="topbar-calcs"]').click();
+    cy.get('[data-cy="calcs-table-edit"]:first').click();
+    cy.get('[data-cy="calcs-property-name"]').clear().type('form_input_2');
+    cy.get('[data-cy="calcs-property-description"]').clear().type('form_input_2 = form_input_1 * 100');
+    cy.get('[data-cy="calcs-switch-formula"]').click();
+    cy.get('[data-cy="calcs-property-formula"]').clear().type('form_input_1 * 100');
+    cy.get('[data-cy="calcs-button-save"]').click();
+    cy.get('[data-cy="calcs-table"]').should('contain.text', 'form_input_2 = form_input_1 * 100');
+    cy.get('[data-cy="calcs-modal"] .close').click();
+
+    // Delete the created calculated property
+    cy.get('[data-cy="topbar-calcs"]').click();
+    cy.get('[data-cy="calcs-table-remove"]:first').click();
+    cy.get('[data-cy="calcs-table"]').should('not.contain.text', 'form_input_2 = form_input_1 * 100');
+    cy.get('[data-cy="calcs-modal"] .close').click();
+  });
+  it('Create duplicated properties', () => {
+    cy.visit('/');
+    // Create a calculated property
+    cy.get('[data-cy="topbar-calcs"]').click();
+    cy.get('[data-cy="calcs-add-property"]').click();
+    cy.get('[data-cy="calcs-property-name"]').clear().type('form_input_1');
+    cy.get('[data-cy="calcs-property-description"]').clear().type('form_input_1 = form_input_2 ^ 2');
+    cy.get('[data-cy="calcs-switch-formula"]').click();
+    cy.get('[data-cy="calcs-property-formula"]').clear().type('pow(form_input_2, 2)');
+    cy.get('[data-cy="calcs-button-save"]').click();
+    cy.get('[data-cy="calcs-table"]').should('contain.text', 'form_input_1 = form_input_2 ^ 2');
+    cy.get('[data-cy="calcs-modal"] .close').click();
+
+    // Create a duplicated calculated property
+    cy.get('[data-cy="topbar-calcs"]').click();
+    cy.get('[data-cy="calcs-add-property"]').click();
+    cy.get('[data-cy="calcs-property-name"]').clear().type('form_input_1');
+    cy.get('[data-cy="calcs-property-description"]').clear().type('form_input_1 = form_input_2 ^ 2');
+    cy.get('[data-cy="calcs-switch-formula"]').click();
+    cy.get('[data-cy="calcs-property-formula"]').clear().type('pow(form_input_2, 2)');
+    cy.get('[data-cy="calcs-button-save"]').click();
+    cy.get('[data-cy="calcs-modal"]').should('contain.text', 'Property already exists');
+  });
   it('Create a javascript computed field', () => {
     cy.visit('/');
     // Add an input field
