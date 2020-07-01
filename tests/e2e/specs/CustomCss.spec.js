8fa4c2ada547117f1582dfb5bf2d3a069e54b3b9
sanjacornelius <sanja.cornelius@processmaker.com>
2020-07-01 08:37:50 -0700

    Update Custom CSS Tests

--- tests/e2e/specs/CustomCss.spec.js
+++ tests/e2e/specs/CustomCss.spec.js
@@ -17,15 +17,36 @@ describe('Custom CSS', () => {
         cy.get('[data-cy=inspector-customCssSelector]').type('new_input_css');
         cy.get('[data-cy=topbar-css]').click();
         cy.get('[data-cy=monaco-editor]').should('be.visible');
-        cy.get('#custom-css___BV_modal_header_ > .close').click();
-        cy.get('[data-cy=monaco-editor]').should('not.be.visible');
-        cy.get('[data-cy=topbar-css]').click();
-        cy.get('[data-cy=monaco-editor]').should('be.visible');
         cy.get('[data-cy=cancel-button]').click();
         cy.get('[data-cy=monaco-editor]').should('not.be.visible');
     });
 
-    it('Updates the elements styling', () => {
+    it('Closes Modal', () => {
+        cy.visit('/');
+        cy.get('[data-cy=controls-FormInput]').drag('[data-cy=screen-drop-zone]', 'bottom');
+        cy.get('[data-cy=screen-element-container]').click();
+        cy.get('[data-cy=accordion-Advanced]').click();
+        cy.get('[data-cy=inspector-customCssSelector]').type('new_input_css');
+        cy.get('[data-cy=topbar-css]').click();
+        cy.get('[data-cy=monaco-editor]').should('be.visible');
+        cy.get('#custom-css___BV_modal_header_ > .close').click();
+        cy.get('[data-cy=monaco-editor]').should('not.be.visible');
+    });
+
+    it('Saves Custom CSS', () => {
+        cy.visit('/');
+        cy.get('[data-cy=controls-FormInput]').drag('[data-cy=screen-drop-zone]', 'bottom');
+        cy.get('[data-cy=screen-element-container]').click();
+        cy.get('[data-cy=accordion-Advanced]').click();
+        cy.get('[data-cy=inspector-customCssSelector]').type('new_input_css');
+        cy.get('[data-cy=topbar-css]').click();
+        cy.get('[data-cy=monaco-editor]').type("div[selector='new_input_css'] {background-color:red;padding:10px;}", {parseSpecialCharSequences: false} );
+        cy.get('[data-cy=save-button]').click();
+        cy.get('[data-cy=topbar-css]').click();
+        cy.get('[data-cy=monaco-editor]').should('not.be.empty');
+    });
+
+    it('Adds styling to element', () => {
       cy.visit('/');
       cy.get('[data-cy=controls-FormInput]').drag('[data-cy=screen-drop-zone]', 'bottom');
       cy.get('[data-cy=screen-element-container]').click();
