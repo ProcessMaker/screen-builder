5573312dad21bea833b83563b7a0de1bcc5ce605
David Callizaya <davidcallizaya@gmail.com>
2020-07-06 14:32:38 -0400

    Test checkgroup select list

--- tests/e2e/specs/FormSelectList.js
+++ tests/e2e/specs/FormSelectList.js
@@ -74,4 +74,31 @@ describe('Form Select List', () => {
       form_select_list_1: ['two', 'one'],
     });
   });
+
+  it('Checkbox group', () => {
+    cy.get('[data-cy=accordion-DataSource]').click();
+    cy.get('[data-cy=inspector-data-sources]').select('Provide Values');
+    cy.get('[data-cy=inspector-add-option]').click();
+    cy.get('[data-cy=inspector-option-value]').type('one');
+    cy.get('[data-cy=inspector-option-content]').type('one');
+    cy.get('[data-cy=inspector-option-save]').click();
+    cy.get('[data-cy=inspector-add-option]').click();
+    cy.get('[data-cy=inspector-option-value]').type('two');
+    cy.get('[data-cy=inspector-option-content]').type('two');
+    cy.get('[data-cy=inspector-option-save]').click();
+    cy.get('[data-cy=inspector-add-option]').click();
+    cy.get('[data-cy=inspector-option-value]').type('three');
+    cy.get('[data-cy=inspector-option-content]').type('three');
+    cy.get('[data-cy=inspector-option-cancel]').click();
+    cy.get('[data-cy=inspector-render-as]').select('Radio/Checkbox Group');
+    cy.get('[data-cy=inspector-allow-multi-select]').click();
+
+    cy.get('[data-cy=mode-preview]').click();
+    cy.get('[data-cy=preview-content] [data-cy="screen-field-form_select_list_1"]').eq(1).click();
+    cy.get('[data-cy=preview-content] [data-cy="screen-field-form_select_list_1"]').eq(0).click();
+
+    cy.assertPreviewData({
+      form_select_list_1: ['two', 'one'],
+    });
+  });
 });
