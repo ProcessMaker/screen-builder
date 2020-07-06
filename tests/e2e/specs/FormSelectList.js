2acf606f8c9eaa29f659c3ce6fa6b5d7370b88de
David Callizaya <davidcallizaya@gmail.com>
2020-07-06 14:13:21 -0400

    Add tests for FormSelectList

---
+++ tests/e2e/specs/FormSelectList.js
@@ -0,0 +1,45 @@
+describe('Form Select List', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    // Add loop control
+    cy.get('[data-cy=controls-FormSelectList]').drag('[data-cy=screen-drop-zone]', 'bottom'); 
+    cy.get('[data-cy=screen-element-container]').click();
+  });
+  
+  it('Default properties', () => {
+    cy.get('[data-cy=mode-preview]').click();
+    cy.get('[data-cy=preview-content] [data-cy="screen-field-form_select_list_1"]').click();
+    cy.get('[data-cy=preview-content]').should('contain.text', 'No Data Available');
+    cy.assertPreviewData({
+      form_select_list_1: null,
+    });
+  });
+
+  it('Set data source values', () => {
+    cy.get('[data-cy=accordion-DataSource]').click();
+    cy.get('[data-cy=inspector-data-sources]').select('Provide Values');
+    cy.get('[data-cy=inspector-add-option]').click();
+    cy.get('[data-cy=inspector-option-value]').type('one');
+    cy.get('[data-cy=inspector-option-content]').type('one');
+    cy.get('[data-cy=inspector-option-save]').click();
+    cy.get('[data-cy=inspector-add-option]').click();
+    cy.get('[data-cy=inspector-option-value]').type('two');
+    cy.get('[data-cy=inspector-option-content]').type('two');
+    cy.get('[data-cy=inspector-option-save]').click();
+    cy.get('[data-cy=inspector-add-option]').click();
+    cy.get('[data-cy=inspector-option-value]').type('three');
+    cy.get('[data-cy=inspector-option-content]').type('three');
+    cy.get('[data-cy=inspector-option-cancel]').click();
+
+    cy.get('[data-cy=mode-preview]').click();
+    cy.get('[data-cy=preview-content] [data-cy="screen-field-form_select_list_1"]').click();
+    cy.get('[data-cy=preview-content] span:contains(one)').should('be.visible');
+    cy.get('[data-cy=preview-content] span:contains(two)').should('be.visible');
+    cy.get('[data-cy=preview-content] span:contains(three)').should('not.exist');
+
+    cy.get('[data-cy=preview-content] span:contains(two):first').click();
+    cy.assertPreviewData({
+      form_select_list_1: 'two',
+    });
+  });
+});
