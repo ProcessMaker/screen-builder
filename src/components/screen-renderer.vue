8463aa1907a6200b79f04ff2e8529b31c85272d9
David Callizaya <davidcallizaya@gmail.com>
2020-07-24 12:49:33 -0400

    Add Vuelidate implementation

--- src/components/screen-renderer.vue
+++ src/components/screen-renderer.vue
@@ -47,5 +47,28 @@ export default {
 </script>
 
 <style>
-
+.form-group--error {
+  animation-name: shakeError;
+  animation-fill-mode: forwards;
+  animation-duration: .6s;
+  animation-timing-function: ease-in-out;
+}
+@keyframes shakeError {
+  0% {
+    transform: translateX(0); }
+  15% {
+    transform: translateX(0.375rem); }
+  30% {
+    transform: translateX(-0.375rem); }
+  45% {
+    transform: translateX(0.375rem); }
+  60% {
+    transform: translateX(-0.375rem); }
+  75% {
+    transform: translateX(0.375rem); }
+  90% {
+    transform: translateX(-0.375rem); }
+  100% {
+    transform: translateX(0); }
+}
 </style>
