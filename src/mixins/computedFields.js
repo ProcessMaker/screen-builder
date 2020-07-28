7df03f007a2ae0afb0a37cd46e8539f9c5a2edd9
David Callizaya <davidcallizaya@gmail.com>
2020-07-28 11:45:41 -0400

    Validate invalid expressions

--- src/mixins/computedFields.js
+++ src/mixins/computedFields.js
@@ -3,10 +3,14 @@ import { Parser } from 'expr-eval';
 export default {
   methods: {
     evaluateExpression(expression, type) {
-      if (type === 'expression') {
-        return Parser.evaluate(expression, Object.assign({}, this, this.vdata));
-      } else {
-        return new Function(expression).bind(Object.assign({}, this, this.vdata))();
+      try {
+        if (type === 'expression') {
+          return Parser.evaluate(expression, Object.assign({}, this, this.vdata));
+        } else {
+          return new Function(expression).bind(Object.assign({}, this, this.vdata))();
+        }
+      } catch (e) {
+        e;
       }
     },
   },
