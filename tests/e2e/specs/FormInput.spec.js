701c37a0f541ed1299acdd0bb92f55aa70cfcc34
David Callizaya <davidcallizaya@gmail.com>
2020-07-28 11:50:10 -0400

    Fix date input test

--- tests/e2e/specs/FormInput.spec.js
+++ tests/e2e/specs/FormInput.spec.js
@@ -92,7 +92,7 @@ describe('Form Input', () => {
     cy.get('[data-cy=screen-element-container]').click();
     cy.setMultiselect('[data-cy=inspector-dataFormat]', 'Datetime');
     cy.get('[data-cy=mode-preview]').click();
-    cy.get('[data-cy=preview-content] [name=form_input_1]').type('01312020 230 PM');
+    cy.get('[data-cy=preview-content] [name=form_input_1]').clear().type('01312020 230 PM');
     cy.get('[name=form_input_1]:visible').should('have.value', '01/31/2020 2:30 PM');
     cy.assertPreviewData({
       form_input_1: '2020-01-31T14:30:00.000Z',
@@ -104,7 +104,7 @@ describe('Form Input', () => {
     cy.get('[data-cy=screen-element-container]').click();
     cy.setMultiselect('[data-cy=inspector-dataFormat]', 'Datetime');
     cy.get('[data-cy=mode-preview]').click();
-    cy.get('[data-cy=preview-content] [name=form_input_1]').type('01312020');
+    cy.get('[data-cy=preview-content] [name=form_input_1]').clear().type('01312020');
     cy.get('[name=form_input_1]:visible').should('have.value', '01/31/2020');
     cy.assertPreviewData({
       form_input_1: '01/31/2020',
