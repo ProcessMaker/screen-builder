13307c016ef8ad866f883dd8bdda07f9c9aa5408
David Callizaya <davidcallizaya@gmail.com>
2020-08-28 12:49:39 -0400

    Add default value dynamic tests

--- tests/e2e/specs/DefaultValues.spec.js
+++ tests/e2e/specs/DefaultValues.spec.js
@@ -37,4 +37,39 @@ describe('Default values', () => {
       form_checkbox_1: true,
     });
   });
+  it('Dynamic Basic default value', () => {
+    cy.visit('/');
+    cy.get('[data-cy=controls-FormInput]').drag('[data-cy=screen-drop-zone]', 'bottom');
+    cy.get('[data-cy=screen-element-container]').click();
+    cy.get('[data-cy=accordion-Advanced]').click();
+    cy.get('[data-cy=inspector-defaultValue-basicValue]').clear().type('initial value');
+    cy.get('[data-cy=mode-preview]').click();
+    cy.get('[data-cy=preview-content] [name=form_input_1]').should('have.value', 'initial value');
+    cy.assertPreviewData({
+      form_input_1: 'initial value',
+    });
+    cy.get('[data-cy=preview-content] [name=form_input_1]').type('New value');
+    cy.assertPreviewData({
+      form_input_1: 'New value',
+    });
+  });
+  it('Dynamic Javascript default value', () => {
+    cy.visit('/');
+    cy.setPreviewDataInput({name: 'world'});
+    cy.get('[data-cy=controls-FormInput]').drag('[data-cy=screen-drop-zone]', 'bottom');
+    cy.get('[data-cy=screen-element-container]').click();
+    cy.get('[data-cy=accordion-Advanced]').click();
+    cy.get('[data-cy=inspector-defaultValue-js]').click();
+    cy.setVueComponentValue('[data-cy=inspector-defaultValue-jsValue]', 'return `hello ${this.name}`;');
+    cy.get('[data-cy=mode-preview]').click();
+    cy.get('[data-cy=preview-content] [name=form_input_1]').should('have.value', 'hello world');
+    cy.assertPreviewData({
+      name: 'world',
+      form_input_1: 'hello world',
+    });
+    cy.get('[data-cy=preview-content] [name=form_input_1]').type('New value');
+    cy.assertPreviewData({
+      form_input_1: 'New value',
+    });
+  });
 });
