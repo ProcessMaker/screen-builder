853b30f7c24f63328869d405c06ad509bf367f70
David Callizaya <davidcallizaya@gmail.com>
2020-09-03 12:07:40 -0400

    Add file upload

--- tests/e2e/specs/ComplexScreen.spec.js
+++ tests/e2e/specs/ComplexScreen.spec.js
@@ -1,9 +1,8 @@
 describe('Complex screen', () => {
   beforeEach(() => {
     cy.visit('/');
-    cy.mockComponent('FileUpload').then(() => {
-      cy.loadFromJson('complex_screen.json', 2);
-    });
+    cy.loadFromJson('complex_screen.json', 2);
+    cy.loadFromJson('complex_screen.json', 2);
   });
   
   it('Load screen', () => {
