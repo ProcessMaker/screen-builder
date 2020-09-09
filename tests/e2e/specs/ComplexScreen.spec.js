051c4879c54dd74ebe0762ddd5d9a46ec3a81f7b
David Callizaya <davidcallizaya@gmail.com>
2020-09-08 22:10:49 -0400

    Complete complex tests

--- tests/e2e/specs/ComplexScreen.spec.js
+++ tests/e2e/specs/ComplexScreen.spec.js
@@ -12,64 +12,70 @@ describe('Complex screen', () => {
     cy.get('[data-cy=preview-content] [name=form_input_1]').type('12345678');
     cy.get('[data-cy=preview-content] [name=form_input_1]').should('have.value', '12345678');
 
-    cy.get('[data-cy=preview-content] [name=form_input_5]').type('55555');
-    cy.get('[data-cy=preview-content] [name=form_input_5]').should('have.value', '55555');
-
+    cy.get('[data-cy=preview-content] [name=form_text_area_1]').type('Hello!');
     cy.get('[data-cy=preview-content] [name=form_text_area_1]').should('have.value', 'Hello!');
 
-    cy.get('[data-cy=preview-content] [name=form_input_2]').type('testing');
-    cy.get('[data-cy=preview-content] [name=form_input_2]').should('have.value', 'testing');
+    cy.setMultiselect('[data-cy=preview-content] [data-cy="screen-field-form_select_list_1"]', 'a');
 
-    //cy.get('[data-cy=preview-content] [name=form_input_6]').type('text');
-    //cy.get('[data-cy=preview-content] [name=form_input_6]').should('have.value', 'text');
+    cy.get('[data-cy=preview-content] [name=form_checkbox_1]').click();
 
-    //cy.get('[data-cy=preview-content] [name=form_text_area_2]').type('some large text');
-    //cy.get('[data-cy=preview-content] [name=form_text_area_2]').should('have.value', 'some large text');
+    cy.get('[data-cy=preview-content] [data-cy="screen-field-form_date_picker_1"] input').click();
+    cy.get('[data-cy=preview-content] [data-cy="screen-field-form_date_picker_1"] .day.today').click();
 
-    cy.get('[data-cy=preview-content] [data-cy="screen-field-form_select_list_1"]').click();
-    cy.get('[data-cy=preview-content] span:contains(a)').should('be.visible');
+    cy.get('[data-cy=preview-content] [data-cy="screen-field-form_date_picker_2"] input').click();
+    cy.get('[data-cy=preview-content] [data-cy="screen-field-form_date_picker_2"] .day.today').click();
+    cy.get('[data-cy=preview-content] [data-cy="screen-field-form_date_picker_2"] [data-action="close"]').click();
+
+    const today = new Date();
+    today.setUTCHours(0);
+    today.setUTCMinutes(0);
+    today.setUTCSeconds(0);
+    today.setUTCMilliseconds(0);
+
+    const now = new Date();
+    now.setUTCSeconds(0);
+    now.setUTCMilliseconds(0);
+
+    cy.get('[data-cy=preview-content] [name=form_input_2]').eq(0).clear().type('input in loop 1');
+    cy.get('[data-cy=preview-content] [name=form_input_2]').eq(1).clear().type('input in loop 2');
+    cy.get('[data-cy=preview-content] [name=form_input_2]').eq(2).clear().type('input in loop 3');
 
     cy.assertPreviewData({
       'form_input_1': '12345678',
-      'form_input_5': null,
       'form_text_area_1': 'Hello!',
-      'form_select_list_1': null,
-      'form_input_2': null,
-      'form_input_6': null,
-      'form_text_area_2': null,
-      'form_select_list_2': null,
-      'form_input_3': null,
-      'form_input_7': null,
-      'form_text_area_3': '<h4 style="color:red"><i>HELLO!</i></h4>',
-      'form_select_list_3': null,
-      'form_input_4': null,
-      'form_input_8': null,
-      'form_select_list_4': null,
-      'form_checkbox_1': null,
-      'form_checkbox_5': null,
-      'form_date_picker_1': null,
-      'form_checkbox_2': true,
-      'form_checkbox_6': true,
-      'form_date_picker_2': null,
-      'form_checkbox_3': null,
-      'form_checkbox_7': null,
-      'form_date_picker_3': null,
-      'form_checkbox_4': true,
-      'form_checkbox_8': true,
-      'form_date_picker_4': null,
-      'form_record_list_1': null,
+      'form_select_list_1': 'a',
+      'form_checkbox_1': true,
+      'form_date_picker_1': today.toISOString(),
+      'form_date_picker_2': now.toISOString(),
       'loop': [
         {
-          'form_input_9': null,
+          'form_input_2': 'input in loop 1',
         },
         {
-          'form_input_9': null,
+          'form_input_2': 'input in loop 2',
+        },
+        {
+          'form_input_2': 'input in loop 3',
         },
       ],
-      'form_checkbox_9': null,
-      'form_checkbox_10': null,
-      'email': null,
-      'url': null,
+      'form_record_list_1': null,
+      'page1': null,
+      'form_input_5': null,
+      'form_select_list_4': null,
+      'form_date_picker_7': null,
+      'form_record_list_3': null,
+      'page2': null,
+      'form_text_area_4': null,
+      'form_checkbox_4': null,
+      'form_date_picker_8': null,
+      'form_input_4': null,
+      'form_date_picker_5': null,
+      'form_text_area_3': null,
+      'form_date_picker_6': null,
+      'form_select_list_3': null,
+      'form_checkbox_3': null,
+      'first_name': null,
+      'last_name': null,
     });
   });
 });
