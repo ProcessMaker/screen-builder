f54e813947fb37b423427910b233ffb749c2d071
David Callizaya <davidcallizaya@gmail.com>
2020-09-22 16:04:20 -0400

    Test task component

--- tests/e2e/specs/Task.spec.js
+++ tests/e2e/specs/Task.spec.js
@@ -1,6 +1,41 @@
 describe('Task component', () => {
-
   it('In a webentry', () => {
-    cy.visit('/?scenario=TaskAssigned');
+    cy.server();
+
+    cy.visit('/?scenario=WebEntry', {
+      onBeforeLoad(win) {
+        // Call some code to initialize the fake server part using MockSocket
+        cy.stub(win, 'WebSocket').callsFake((url) => ({
+          url,
+          onclose: null,
+          onopen: null,
+          close(){},
+          send(){},
+        }));
+        win.Echo = {
+          
+        };
+      },
+    });
+    cy.wait(2000);
+  });
+
+  it('In a Process Task', () => {
+    cy.server();
+
+    cy.visit('/?scenario=TaskAssigned', {
+      onBeforeLoad(win) {
+        // Call some code to initialize the fake server part using MockSocket
+        cy.stub(win, 'WebSocket').callsFake((url) => ({
+          url,
+          onclose: null,
+          onopen: null,
+          close(){},
+          send(){},
+        }));
+      },
+    });
+
+    cy.wait(2000);
   });
 });
