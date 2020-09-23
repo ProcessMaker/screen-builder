1ecadf7178ac3d69dd37749c5ed3ebe616df9131
Nolan Ehrstrom <github@nolanpro.com>
2020-09-23 13:03:07 -0700

    Update the data provider plugin

--- src/DataProvider.js
+++ src/DataProvider.js
@@ -1,11 +1,67 @@
 /* istanbul ignore file */
+import axios from 'axios';
+import _ from 'lodash';
 
 export default {
-  apiInstance: null,
   install(Vue) {
     Vue.prototype.$dataProvider = this;
   },
+  apiInstance() {
+    // Use the real apiClient
+    if (_.has(window, 'ProcessMaker.apiClient') && !window.ProcessMaker.isStub) {
+      return window.ProcessMaker.apiClient;
+    }
+
+    // Allow standalone app to use a real api if token and baseURL are present
+    if (this.token()) {
+      axios.defaults.baseURL = this.baseURL();
+      console.log("Set axios base url to", this.baseURL());
+      axios.defaults.headers.common = {
+        'Authorization': 'Bearer ' + this.token(),
+      };
+      return axios;
+    }
+
+    // If the apiClient is a stub, use it for running tests
+    if (_.has(window, 'ProcessMaker.apiClient') && window.ProcessMaker.isStub) {
+      return window.ProcessMaker.apiClient;
+    }
+
+    // For anything else, use a dummy api client
+    return {
+      get() {
+        return Promise.resolve({
+          data: {
+            data: [],
+          },
+        });
+      },
+    };
+  },
   get(...args) {
-    return window.ProcessMaker.apiClient.get(...args);
+    return this.apiInstance().get(...args);
+  },
+  token() {
+    return localStorage.getItem('token');
+  },
+  baseURL() {
+    return localStorage.getItem('baseURL');
+  },
+  
+  // Methods below are used in the components
+  
+  getTask(params) {
+    const endpoint = _.get(window, 'PM4ConfigOverrides.getTaskEndpoint', '/tasks');
+    return this.get(endpoint + params);
+  },
+
+  getTasks(params) {
+    const endpoint = _.get(window, 'PM4ConfigOverrides.getTasksEndpoint', '/tasks');
+    return this.get(endpoint + params);
+  },
+  
+  getScreen(id) {
+    const endpoint = _.get(window, 'PM4ConfigOverrides.getTasksEndpoint', '/tasks');
+    return this.get(endpoint + params);
   },
 };
