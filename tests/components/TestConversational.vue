48903693519fc23fb3a9032d4d855a11556c65b5
Nolan Ehrstrom <github@nolanpro.com>
2020-10-20 10:17:40 -0700

    Update conversational test component

---
+++ tests/components/TestConversational.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <h3>Conversational Form</h3>
+    <div id="cf-context"></div>
+    
+    <h3>Screen</h3>
+    <task
+      ref="task"
+      v-model="formData"
+      :initial-screen-id="screenId"
+      @submit="submit"
+    />
+  </div>
+</template>
+
+<script>
+
+import Vue from 'vue';
+import ConversationalForm from 'package-conversational-forms/resources/js/components/ConversationalForm';
+Vue.component('ConversationalForm', ConversationalForm);
+
+export default {
+  data() {
+    return {
+      screenId: 1,
+      formData: { name: null },
+    };
+  },
+  methods: {
+    submit() {
+
+    },
+  },
+};
+</script>
+
+<style scoped>
+#cf-context {
+  border: 1px solid;
+  width: 50%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 50px;
+  height: 600px;
+}
+</style>
