f6bb26126d08d72451efba4a94b952ca7ea5bfa9
David Callizaya <davidcallizaya@gmail.com>
2021-01-29 19:33:34 -0400

    Add NOT IN list validation rules

--- tests/e2e/specs/ValidationRules.js
+++ tests/e2e/specs/ValidationRules.js
@@ -9,16 +9,27 @@ describe('Record list', () => {
   it('Invalid default values', () => {
     cy.loadFromJson('validation_rules.json', 0);
     cy.get('[data-cy=mode-preview]').click();
+
+    // ACCEPTED CHECKBOX
     cy.get('[data-cy=preview-content] [data-cy="screen-field-form_checkbox_1"]').parent().should('contain.text', 'Field must be accepted');
-    cy.get('[data-cy=preview-content] [data-cy="screen-field-form_input_1"]').parent().should('contain.text', 'Field must be accepted');
-    cy.get('[data-cy=preview-content] [data-cy="screen-field-form_input_2"]').parent().should('contain.text', 'Invalid value');
-
     cy.get('[data-cy=preview-content] [name=form_checkbox_1]').click();
-    cy.get('[data-cy=preview-content] [name=form_input_1]').type('on');
-    cy.get('[data-cy=preview-content] [name=form_input_2]').type('2');
-
     cy.get('[data-cy=preview-content] [data-cy="screen-field-form_checkbox_1"]').parent().should('not.contain.text', 'Field must be accepted');
+
+    // ACCEPTED INPUT
+    cy.get('[data-cy=preview-content] [data-cy="screen-field-form_input_1"]').parent().should('contain.text', 'Field must be accepted');
+    cy.get('[data-cy=preview-content] [name=form_input_1]').type('on');
     cy.get('[data-cy=preview-content] [data-cy="screen-field-form_input_1"]').parent().should('not.contain.text', 'Field must be accepted');
+
+    // IN LIST
+    cy.get('[data-cy=preview-content] [data-cy="screen-field-form_input_2"]').parent().should('contain.text', 'Invalid value');
+    cy.get('[data-cy=preview-content] [name=form_input_2]').type('2');
     cy.get('[data-cy=preview-content] [data-cy="screen-field-form_input_2"]').parent().should('not.contain.text', 'Invalid value');
+
+    // NOT IN LIST
+    cy.get('[data-cy=preview-content] [name=form_input_3]').type('2');
+    cy.get('[data-cy=preview-content] [data-cy="screen-field-form_input_3"]').parent().should('contain.text', 'Invalid value');
+    cy.get('[data-cy=preview-content] [name=form_input_3]').type('4');
+    cy.get('[data-cy=preview-content] [data-cy="screen-field-form_input_3"]').parent().should('not.contain.text', 'Invalid value');
+
   });
 });
