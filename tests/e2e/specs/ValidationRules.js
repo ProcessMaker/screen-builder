76705d914a11e4a7b8777d28a980f20e04f4f268
David Callizaya <davidcallizaya@gmail.com>
2021-01-29 19:27:09 -0400

    Add IN validation rule

--- tests/e2e/specs/ValidationRules.js
+++ tests/e2e/specs/ValidationRules.js
@@ -11,11 +11,14 @@ describe('Record list', () => {
     cy.get('[data-cy=mode-preview]').click();
     cy.get('[data-cy=preview-content] [data-cy="screen-field-form_checkbox_1"]').parent().should('contain.text', 'Field must be accepted');
     cy.get('[data-cy=preview-content] [data-cy="screen-field-form_input_1"]').parent().should('contain.text', 'Field must be accepted');
+    cy.get('[data-cy=preview-content] [data-cy="screen-field-form_input_2"]').parent().should('contain.text', 'Invalid value');
 
     cy.get('[data-cy=preview-content] [name=form_checkbox_1]').click();
     cy.get('[data-cy=preview-content] [name=form_input_1]').type('on');
+    cy.get('[data-cy=preview-content] [name=form_input_2]').type('2');
 
     cy.get('[data-cy=preview-content] [data-cy="screen-field-form_checkbox_1"]').parent().should('not.contain.text', 'Field must be accepted');
     cy.get('[data-cy=preview-content] [data-cy="screen-field-form_input_1"]').parent().should('not.contain.text', 'Field must be accepted');
+    cy.get('[data-cy=preview-content] [data-cy="screen-field-form_input_2"]').parent().should('not.contain.text', 'Invalid value');
   });
 });
