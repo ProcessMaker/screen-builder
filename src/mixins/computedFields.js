9c633fd515950e76348d8db3172ef96c250bdaf0
Marco A. Nina Mena <marco.antonio.nina@processmaker.com>
2021-02-11 09:13:39 -0400

    Add a proxy in calculated properties for variables.

--- src/mixins/computedFields.js
+++ src/mixins/computedFields.js
@@ -5,10 +5,27 @@ export default {
     evaluateExpression(expression, type) {
       let value = null;
       try {
+        const self = this;
+
+        //monitor if variable belongs to data (defined variables) or vdata (external variables)
+        //in this way the event is not executed again when the variable is update
+        const data = new Proxy(Object.assign({}, this), {
+          get(data, name) {
+            if (data[name] === undefined) {
+              return self.vdata[name]
+            } else {
+              return data[name];
+            }
+          },
+          set(data, name, value) {
+            throw "You are not allowed to set properties from inside an expression";
+          }
+        });
+
         if (type === 'expression') {
-          value = Parser.evaluate(expression, Object.assign({}, this, this.vdata));
+          value = Parser.evaluate(expression, data);
         } else {
-          value = new Function(expression).bind(Object.assign({}, this, this.vdata))();
+          value = new Function(expression).bind(data)();
         }
 
         if (value instanceof Date) {
