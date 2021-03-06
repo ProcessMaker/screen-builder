1881655f42b838f6614add79fb1a546eda1695ff
sanjacornelius <sanja.cornelius@processmaker.com>
2021-03-05 17:24:49 -0800

    Set fixed width for table columns

--- src/components/renderer/form-record-list.vue
+++ src/components/renderer/form-record-list.vue
@@ -281,6 +281,7 @@ export default {
           key: option[key || 'value'],
           sortable: true,
           label: option[value || 'content'],
+          tdClass: 'table-column',
         };
       };
 
@@ -439,3 +440,9 @@ export default {
   },
 };
 </script>
+
+<style>
+  .table td.table-column {
+    max-width: 300px;
+  }
+</style>
