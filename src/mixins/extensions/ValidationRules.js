435153728f66d8e6e00a56d213197968621d8562
Marco A. Nina Mena <marcoantonina@gmail.com>
2021-03-08 20:54:54 -0400

    Add validation for loops

--- src/mixins/extensions/ValidationRules.js
+++ src/mixins/extensions/ValidationRules.js
@@ -43,28 +43,19 @@ export default {
         if (this.validVariableName(element.config.name)) {
           set(screen.validations, element.config.name, {});
           const validationRule = get(screen.validations, element.config.name);
-          if (element.config.validation instanceof Array) {
-            element.config.validation.forEach((validation) => {
-              const rule = this.camelCase(validation.value.split(':')[0]);
-              if (!rule) {
-                return;
-              }
-              let validationFn = validators[rule];
-              if (!validationFn) {
-                // eslint-disable-next-line no-console
-                console.error(`Undefined validation rule "${rule}"`);
-                return;
-              }
-              if (validation.configs instanceof Array) {
-                const params = [];
-                validation.configs.forEach(cnf => {
-                  params.push(cnf.value);
-                });
-                validationFn = validationFn(...params);
-              }
-              validationRule[rule] = validationFn;
+          this.addRules(element.config.validation, validationRule);
+
+          if (element.component === 'FormLoop') {
+            //Collections validation
+            validationRule['$each'] = [];
+
+            element.items.forEach((item) => {
+              set(validationRule['$each'], item.config.name, {});
+              const validationLoopRule = get(validationRule['$each'], item.config.name);
+              this.addRules(item.config.validation, validationLoopRule);
             });
           }
+
           // Remove the validation from inside the control
           delete properties[':validation'];
           delete properties['validation'];
