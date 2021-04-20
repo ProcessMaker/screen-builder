e78c46d6c4af38c5914218ed6c4eca97612c8c5f
Nolan Ehrstrom <github@nolanpro.com>
2021-04-20 16:21:56 -0700

    Run watcher scripts in sync mode

--- src/mixins/watchers.js
+++ src/mixins/watchers.js
@@ -6,8 +6,6 @@ const broadcastEvent = '.Illuminate\\\\Notifications\\\\Events\\\\BroadcastNotif
 export default {
   data() {
     return {
-      listeners: [],
-      echoListeners: [],
     };
   },
   methods: {
@@ -21,8 +19,6 @@ export default {
       return new Promise((complete, exception) => {
         const input = Mustache.render(watcher.input_data, this.vdata);
         const config = Mustache.render(watcher.script_configuration, this.vdata);
-        this.listenWatcher(watcher).then((response) => complete(response))
-          .catch(error => exception(error));
         
         let scriptId = watcher.script_id;
         if (watcher.script_key) {
@@ -32,6 +28,11 @@ export default {
           watcher: watcher.uid,
           data: input,
           config,
+          sync: true,
+        }, { timeout: 0 }).then(response => {
+          complete(response.data.output);
+        }).catch(err => {
+          exception(err);
         });
       }).then((response) => {
         if (watcher.output_variable) {
@@ -67,59 +68,8 @@ export default {
         }
       });
     },
-    listenWatcher(watcher) {
-      return new Promise((resolve, exception) => {
-        this.listeners.push({ watcher, resolve, exception });
-      });
-    },
-    /**
-     * Clean the registered echo listeners
-     */
-    cleanEchoListeners() {
-      this.echoListeners.forEach(element => {
-        window.Echo.private(
-          element.channel
-        ).stopListening(element.event);
-      });
-      this.echoListeners.splice(0);
-    },
-    loadWatcherResponse(watcherUid, response) {
-      window.ProcessMaker.apiClient.get(`/scripts/execution/${response.key}`).then((result) => {
-        const response = result.data;
-        this.listeners.forEach(({watcher, resolve, exception}) => {
-          if (watcher.uid === watcherUid) {
-            if (response.exception) {
-              exception(response.message);
-            } else {
-              resolve(response.output);
-            }
-          }
-        });
-      }).catch((error) => {
-        this.$parent.$refs.watchersSynchronous.error(error);
-      });
-    },
   },
   mounted() {
     this.queueWatcher = debounce(this.queueWatcherSync, window.ProcessMaker.watchersDebounce || 1000);
-    if (window.ProcessMaker && window.ProcessMaker.user) {
-      const channel = `ProcessMaker.Models.User.${window.ProcessMaker.user.id}`;
-      const event = 'ProcessMaker\\Notifications\\ScriptResponseNotification';
-      this.echoListeners.push({
-        event,
-        channel,
-        broadcastEvent,
-      });
-      window.Echo.private(channel).notification(
-        (data) => {
-          if (data.type === event) {
-            this.loadWatcherResponse(data.watcher, data.response);
-          }
-        },
-      );
-    }
-  },
-  destroyed() {
-    this.cleanEchoListeners();
   },
 };
