062ba00852d8d73014bc480fbb8852e4ca94b89f
David Callizaya <davidcallizaya@gmail.com>
2021-08-05 20:43:21 -0400

    Test FormTextArea with calcProp

--- tests/e2e/specs/ComputedFieldsReadOnly.spec.js
+++ tests/e2e/specs/ComputedFieldsReadOnly.spec.js
@@ -109,4 +109,29 @@ describe('Computed fields', () => {
     });
 
   });
+
+  it('The user should not be able to change a FormTextArea assigned to a computed property', () => {
+    cy.visit('/');
+    // Add an input field
+    cy.get('[data-cy=controls-FormTextArea]').drag('[data-cy=screen-drop-zone]', 'bottom'); 
+
+    cy.get('[data-cy=screen-element-container]').eq(0).click();
+
+    cy.get('[data-cy="topbar-calcs"]').click();
+    cy.get('[data-cy="calcs-add-property"]').click();
+    cy.get('[data-cy="calcs-property-name"]').clear().type('form_text_area_1');
+    cy.get('[data-cy="calcs-property-description"]').clear().type('form_text_area_1 is always 1');
+    cy.get('[data-cy="calcs-switch-javascript"]').click();
+    cy.setVueComponentValue('[data-cy="calcs-property-javascript"]', 'return "1";');
+    cy.get('[data-cy="calcs-button-save"]').click();
+    cy.get('[data-cy="calcs-modal"] .close').click();
+    cy.get('[data-cy=mode-preview]').click();
+
+    // Assertion: Check the form_text_area_1 is read only
+    cy.get('[data-cy=preview-content] [name=form_text_area_1]').should('have.attr', 'readonly');
+    // Assertion: Check the form_text_area_1 is always 1
+    cy.assertPreviewData({
+      form_text_area_1: '1',
+    });
+  });
 });
