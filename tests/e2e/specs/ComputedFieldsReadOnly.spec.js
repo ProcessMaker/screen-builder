f3222551274e3590bdd1d039a7b18c486f133e06
David Callizaya <davidcallizaya@gmail.com>
2021-08-05 20:17:25 -0400

    Update test FormInput of calc prop

--- tests/e2e/specs/ComputedFieldsReadOnly.spec.js
+++ tests/e2e/specs/ComputedFieldsReadOnly.spec.js
@@ -1,6 +1,6 @@
 describe('Computed fields', () => {
 
-  it('The user should not be able to change a control assigned to a computed property', () => {
+  it('The user should not be able to change a FormInput assigned to a computed property', () => {
     cy.visit('/');
     // Add an input field
     cy.get('[data-cy=controls-FormInput]').drag('[data-cy=screen-drop-zone]', 'bottom'); 
@@ -12,16 +12,101 @@ describe('Computed fields', () => {
     cy.get('[data-cy="calcs-property-name"]').clear().type('form_input_1');
     cy.get('[data-cy="calcs-property-description"]').clear().type('form_input_1 is always 1');
     cy.get('[data-cy="calcs-switch-javascript"]').click();
-    cy.setVueComponentValue('[data-cy="calcs-property-javascript"]', 'return "one";');
+    cy.setVueComponentValue('[data-cy="calcs-property-javascript"]', 'return "1";');
     cy.get('[data-cy="calcs-button-save"]').click();
     cy.get('[data-cy="calcs-modal"] .close').click();
     cy.get('[data-cy=mode-preview]').click();
 
-    // Assertion: Check the form_input_1 is always 1
+    // Assertion: Check the form_input_1 is read only
     cy.get('[data-cy=preview-content] [name=form_input_1]').should('have.attr', 'readonly');
-
+    // Assertion: Check the form_input_1 is always 1
     cy.assertPreviewData({
-      form_input_1: 'one',
+      form_input_1: '1',
     });
+
+    // Check Type Integer
+    cy.get('[data-cy=mode-editor]').click();
+    cy.get('[data-cy=screen-element-container]').click();
+    cy.setMultiselect('[data-cy=inspector-dataFormat]', 'Integer');
+    cy.get('[data-cy=mode-preview]').click();
+    // Assertion: Check the form_input_1 is read only
+    cy.get('[data-cy=preview-content] [name=form_input_1]').should('have.attr', 'readonly');
+    // Assertion: Check the form_input_1 is always 1
+    cy.assertPreviewData({
+      form_input_1: '1',
+    });
+
+    // Check Type Currency
+    cy.get('[data-cy=mode-editor]').click();
+    cy.get('[data-cy=screen-element-container]').click();
+    cy.setMultiselect('[data-cy=inspector-dataFormat]', 'Currency');
+    cy.get('[data-cy=mode-preview]').click();
+    // Assertion: Check the form_input_1 is read only
+    cy.get('[data-cy=preview-content] [name=form_input_1]').should('have.attr', 'readonly');
+    // Assertion: Check the form_input_1 is always 1
+    cy.assertPreviewData({
+      form_input_1: '1',
+    });
+
+    // Check Type Percentage
+    cy.get('[data-cy=mode-editor]').click();
+    cy.get('[data-cy=screen-element-container]').click();
+    cy.setMultiselect('[data-cy=inspector-dataFormat]', 'Percentage');
+    cy.get('[data-cy=mode-preview]').click();
+    // Assertion: Check the form_input_1 is read only
+    cy.get('[data-cy=preview-content] [name=form_input_1]').should('have.attr', 'readonly');
+    // Assertion: Check the form_input_1 is always 1
+    cy.assertPreviewData({
+      form_input_1: '1',
+    });
+
+    // Check Type Decimal
+    cy.get('[data-cy=mode-editor]').click();
+    cy.get('[data-cy=screen-element-container]').click();
+    cy.setMultiselect('[data-cy=inspector-dataFormat]', 'Decimal');
+    cy.get('[data-cy=mode-preview]').click();
+    // Assertion: Check the form_input_1 is read only
+    cy.get('[data-cy=preview-content] [name=form_input_1]').should('have.attr', 'readonly');
+    // Assertion: Check the form_input_1 is always 1
+    cy.assertPreviewData({
+      form_input_1: '1',
+    });
+
+    // Check Type Datetime
+    cy.get('[data-cy=mode-editor]').click();
+    cy.get('[data-cy=screen-element-container]').click();
+    cy.setMultiselect('[data-cy=inspector-dataFormat]', 'Datetime');
+    cy.get('[data-cy=mode-preview]').click();
+    // Assertion: Check the form_input_1 is read only
+    cy.get('[data-cy=preview-content] [name=form_input_1]').should('have.attr', 'readonly');
+    // Assertion: Check the form_input_1 is always 1
+    cy.assertPreviewData({
+      form_input_1: '1',
+    });
+
+    // Check Type Date
+    cy.get('[data-cy=mode-editor]').click();
+    cy.get('[data-cy=screen-element-container]').click();
+    cy.setMultiselect('[data-cy=inspector-dataFormat]', 'Date');
+    cy.get('[data-cy=mode-preview]').click();
+    // Assertion: Check the form_input_1 is read only
+    cy.get('[data-cy=preview-content] [name=form_input_1]').should('have.attr', 'readonly');
+    // Assertion: Check the form_input_1 is always 1
+    cy.assertPreviewData({
+      form_input_1: '1',
+    });
+
+    // Check Type Password
+    cy.get('[data-cy=mode-editor]').click();
+    cy.get('[data-cy=screen-element-container]').click();
+    cy.setMultiselect('[data-cy=inspector-dataFormat]', 'Password');
+    cy.get('[data-cy=mode-preview]').click();
+    // Assertion: Check the form_input_1 is read only
+    cy.get('[data-cy=preview-content] [name=form_input_1]').should('have.attr', 'readonly');
+    // Assertion: Check the form_input_1 is always 1
+    cy.assertPreviewData({
+      form_input_1: '1',
+    });
+
   });
 });
