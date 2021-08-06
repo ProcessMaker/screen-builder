a49b299c2c2412aa54e75f4279c13f55a0f62b33
David Callizaya <davidcallizaya@gmail.com>
2021-08-05 21:08:14 -0400

    Test FormSelectList with calcProp

--- tests/e2e/specs/ComputedFieldsReadOnly.spec.js
+++ tests/e2e/specs/ComputedFieldsReadOnly.spec.js
@@ -159,4 +159,46 @@ describe('Computed fields', () => {
       form_date_picker_1: '1',
     });
   });
+
+  it('The user should not be able to change a FormSelectList assigned to a computed property', () => {
+    cy.visit('/');
+    // Add an input field
+    cy.get('[data-cy=controls-FormSelectList]').drag('[data-cy=screen-drop-zone]', 'bottom'); 
+    cy.get('[data-cy=screen-element-container]').click();
+    cy.get('[data-cy=accordion-DataSource]').click();
+    cy.get('[data-cy=inspector-data-sources]').select('Provide Values');
+    cy.get('[data-cy=inspector-add-option]').click();
+    cy.get('[data-cy=inspector-option-value]').type('1');
+    cy.get('[data-cy=inspector-option-content]').type('one');
+    cy.get('[data-cy=inspector-option-save]').click();
+    cy.get('[data-cy=inspector-add-option]').click();
+    cy.get('[data-cy=inspector-option-value]').type('2');
+    cy.get('[data-cy=inspector-option-content]').type('two');
+    cy.get('[data-cy=inspector-option-save]').click();
+    cy.get('[data-cy=inspector-add-option]').click();
+    cy.get('[data-cy=inspector-option-value]').type('3');
+    cy.get('[data-cy=inspector-option-content]').type('three');
+    cy.get('[data-cy=inspector-option-cancel]').click();
+    cy.get('[data-cy=inspector-add-option]').click();
+    cy.get('[data-cy=inspector-option-value]').type('4');
+    cy.get('[data-cy=inspector-option-content]').type('four');
+    cy.get('[data-cy=inspector-option-save]').click();
+
+    cy.get('[data-cy="topbar-calcs"]').click();
+    cy.get('[data-cy="calcs-add-property"]').click();
+    cy.get('[data-cy="calcs-property-name"]').clear().type('form_select_list_1');
+    cy.get('[data-cy="calcs-property-description"]').clear().type('form_select_list_1 is always 1');
+    cy.get('[data-cy="calcs-switch-javascript"]').click();
+    cy.setVueComponentValue('[data-cy="calcs-property-javascript"]', 'return "1";');
+    cy.get('[data-cy="calcs-button-save"]').click();
+    cy.get('[data-cy="calcs-modal"] .close').click();
+    cy.get('[data-cy=mode-preview]').click();
+
+    // Assertion: Check the form_select_list_1 is read only
+    cy.get('[data-cy=preview-content] [data-cy="screen-field-form_select_list_1"] input').should('have.attr', 'disabled');
+    // Assertion: Check the form_select_list_1 is always 1
+    cy.assertPreviewData({
+      form_select_list_1: '1',
+    });
+  });
 });
