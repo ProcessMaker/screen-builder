58dc4f41f6e1955092490d5c3ba7d2f3f39c01cf
Eleazar Resendez <eleazar.resendez@processmaker.com>
2021-10-07 16:34:59 -0500

    Recalc validation rules

--- src/mixins/VisibilityRule.js
+++ src/mixins/VisibilityRule.js
@@ -1,18 +1,25 @@
 import { Parser } from 'expr-eval';
 
 export default {
+  mounted() {
+    this.$root.$on('refresh-validation-rules', () => {
+      this.loadValidationRules();
+    });
+  },
   methods: {
     visibilityRuleIsVisible(rule, fieldName) {
       try {
         const data = Object.assign({ _parent: this._parent }, this.vdata);
         const isVisible = !!Parser.evaluate(rule, Object.assign({}, this, data));
-        
+
         // Update the array of hidden fields
-        const fileExists = this.hiddenFields__.indexOf(fieldName) !== -1;
-        if (isVisible && fileExists) {
+        const fieldExists = this.hiddenFields__.indexOf(fieldName) !== -1;
+        if (isVisible && fieldExists) {
           this.hiddenFields__ = this.hiddenFields__.filter((f) => f !== fieldName);
-        } else if (!isVisible && !fileExists) {
+          this.$root.$emit('refresh-validation-rules');
+        } else if (!isVisible && !fieldExists) {
           this.hiddenFields__.push(fieldName);
+          this.$root.$emit('refresh-validation-rules');
         }
 
         return isVisible;
