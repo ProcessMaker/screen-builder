80c80a86819919dff62ea4a92a2772e80eed1d33
sanjacornelius <sanja.cornelius@processmaker.com>
2021-10-07 13:12:49 -0700

    Check if fieldName exists

--- src/mixins/VisibilityRule.js
+++ src/mixins/VisibilityRule.js
@@ -7,12 +7,14 @@ export default {
         const data = Object.assign({ _parent: this._parent }, this.vdata);
         const isVisible = !!Parser.evaluate(rule, Object.assign({}, this, data));
         
-        // Update the array of hidden fields
-        const fileExists = this.hiddenFields__.indexOf(fieldName) !== -1;
-        if (isVisible && fileExists) {
-          this.hiddenFields__ = this.hiddenFields__.filter((f) => f !== fieldName);
-        } else if (!isVisible && !fileExists) {
-          this.hiddenFields__.push(fieldName);
+        if (fieldName) {
+          // Update the array of hidden fields
+          const fileExists = this.hiddenFields__.indexOf(fieldName) !== -1;
+          if (isVisible && fileExists) {
+            this.hiddenFields__ = this.hiddenFields__.filter((f) => f !== fieldName);
+          } else if (!isVisible && !fileExists) {
+            this.hiddenFields__.push(fieldName);
+          }
         }
 
         return isVisible;
