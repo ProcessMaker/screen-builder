9956af33a4b06fc09c19abcfda516048ba653dcc
David Callizaya <davidcallizaya@gmail.com>
2021-10-07 12:10:33 -0400

    Test required IF with checkbox field

--- tests/e2e/specs/ValidationRules.spec.js
+++ tests/e2e/specs/ValidationRules.spec.js
@@ -311,9 +311,45 @@ describe('Validation Rules', () => {
     // Fill out the required missing field; ensure the form *can* be submitted
     cy.get('[data-cy=preview-content] [name="form_input"]')
       .type('text');
-    
+
     cy.get('[data-cy=preview-content] [name="submit_button"]')
       .click()
       .then(() => expect(alert).to.equal('Preview Form was Submitted'));
+  });
+
+
+  it('Required IF with boolean values', () => {
+    cy.loadFromJson('required_if_with_checkbox.json', 0);
+    cy.get('[data-cy=mode-preview]').click();
+    
+    // Check box 1
+    cy.get('[data-cy=preview-content] [data-cy="screen-field-form_checkbox_1"]')
+      .click();
+    
+    // Name should be required
+    cy.get('[data-cy=preview-content] [data-cy="screen-field-submit"]')
+      .should('contain.html', 'alert alert-danger');
+    
+    // Uncheck box 1
+    cy.get('[data-cy=preview-content] [data-cy="screen-field-form_checkbox_1"]')
+      .click();
+    
+    // Name should not be required
+    cy.get('[data-cy=preview-content] [data-cy="screen-field-submit"]')
+      .should('not.contain.html', 'alert alert-danger');
+    
+    // Check box 1
+    cy.get('[data-cy=preview-content] [data-cy="screen-field-form_checkbox_1"]')
+      .click();
+    
+    // Fill name
+    cy.get('[data-cy=preview-content] [data-cy="screen-field-form_input_2"]')
+      .clear()
+      .type('test');
+    
+    // Name should not be required
+    cy.get('[data-cy=preview-content] [data-cy="screen-field-submit"]')
+      .should('not.contain.html', 'alert alert-danger');
+    
   });
 });
