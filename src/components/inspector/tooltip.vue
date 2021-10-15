3d16a737ae3e37a18cb2040d22ba38135a4f9b0d
sanjacornelius <sanja.cornelius@processmaker.com>
2021-10-15 08:31:53 -0700

    Use Lodash Get  method

--- src/components/inspector/tooltip.vue
+++ src/components/inspector/tooltip.vue
@@ -131,7 +131,7 @@ export default {
   },
   computed: {
     buttonType() {
-      return this.$attrs.selectedControl.config.event;
+      return _.get(this.$attrs, 'selectedControl.config.event');
     },
   },
   watch: {
