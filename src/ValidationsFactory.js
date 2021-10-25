85be59f6c06b52a9d1df7d1f8a6f3774b6e3bb14
sanjacornelius <sanja.cornelius@processmaker.com>
2021-10-25 08:33:44 -0700

    Check if pagesValidated exists

--- src/ValidationsFactory.js
+++ src/ValidationsFactory.js
@@ -107,8 +107,7 @@ class FormMultiColumnValidations extends Validations {
  */
 class PageNavigateValidations extends Validations {
   async addValidations(validations) {
-    const page = parseInt(this.element.config.eventData);
-    if (!isNaN(page) && !this.screen.pagesValidated.includes(page)) {
+    if (this.screen.pagesValidated && !this.screen.pagesValidated.includes(parseInt(this.element.config.eventData))) {
       this.screen.pagesValidated.push(parseInt(this.element.config.eventData));
       if (this.screen.config[this.element.config.eventData] && this.screen.config[this.element.config.eventData].items) {
         await ValidationsFactory(this.screen.config[this.element.config.eventData].items, { screen: this.screen, data: this.data }).addValidations(validations);
