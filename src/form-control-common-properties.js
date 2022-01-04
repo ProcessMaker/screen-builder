6f898f0fe7cf4bba38e9bb02a79394b8c8cde855
Nolan Ehrstrom <github@nolanpro.com>
2022-01-04 10:32:29 -0800

    Do not allow array index in variable name

--- src/form-control-common-properties.js
+++ src/form-control-common-properties.js
@@ -95,7 +95,8 @@ export const keyNameProperty = {
   config: {
     label: 'Variable Name',
     name: 'Variable Name',
-    validation: 'regex:/^(?:[A-Za-z])(?:[0-9A-Z_a-z])*(?:\\.[0-9A-Z_a-z]+)*$/|required|not_in:' + javascriptReservedKeywords,
+    // Update tests/e2e/specs/Builder.spec.js when changing this
+    validation: 'regex:/^([a-zA-Z]([a-zA-Z0-9_]?)+\\.?)+(?<!\\.)$/|required|not_in:' + javascriptReservedKeywords,
     helper: 'A variable name is a symbolic name to reference information.',
   },
 };
--- tests/e2e/specs/Builder.spec.js
+++ tests/e2e/specs/Builder.spec.js
@@ -36,4 +36,40 @@ describe('Screen Builder', () => {
       form_input_1: '',
     });
   });
+
+  it('Validates variable names', () => {
+    cy.get('[data-cy=screen-element-container]').eq(0).click();
+    
+    // Valid variable names
+    [
+      'A',
+      'aaa',
+      'aaa.bbb',
+      'aaa.BBB.ccc',
+      'aaa1',
+      'aaa_abc.abc_1',
+      'aaa1.bbb1',
+    ].forEach(name => {
+      cy.get('[data-cy=inspector-name]').clear().type(name);
+      cy.get('[data-cy=inspector-name]').should('not.have.class', 'is-invalid');
+    });
+    
+    // Invalid variable names
+    [
+      '1',
+      '1.aaa',
+      'aaa.0',
+      'aaa.',
+      'aaa.bbb.',
+      'aaa._',
+      'aaa._.ccc',
+      'aaa..ccc',
+      'aaa.123.ccc',
+      'aaa.1.ccc',
+    ].forEach(name => {
+      cy.get('[data-cy=inspector-name]').clear().type(name);
+      cy.get('[data-cy=inspector-name]').should('have.class', 'is-invalid');
+    });
+
+  });
 });
