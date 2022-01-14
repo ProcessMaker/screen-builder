79ca4c29f7f4fd59aeca9a21a31786356b69c00a
Marco A. Nina Mena <marcoantonina@gmail.com>
2022-01-14 14:43:29 -0500

    Fix test

--- tests/e2e/specs/NestedCalcProperties.spec.js
+++ tests/e2e/specs/NestedCalcProperties.spec.js
@@ -6,19 +6,15 @@ describe('nested calculated properties', () => {
 
   // @link: https://processmaker.atlassian.net/browse/FOUR-4873
   it('Verify nested calculated properties', () => {
-    cy.loadFromJson('nested_calc_properties.json', 0);
+    cy.loadFromJson('nested_calc_properties.json', 1);
     cy.get('[data-cy=mode-preview]').click();
-
-    // Wait until you load the screen
-    cy.wait(500);
 
     cy.get('[data-cy=preview] input[name="varA"]').type(10);
     cy.get('[data-cy=preview] input[name="varB"]').type(5);
-    cy.get('[data-cy="screen-field-operation"]').selectOption('Addition');
-
+    cy.get('[data-cy=preview-content] [data-cy="screen-field-operation"]').selectOption('Addition');
+    
     // Wait until you load the screen
     cy.wait(500);
-    
 
     // Check the data of the screen
     cy.assertPreviewData({
