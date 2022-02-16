1469d58688071b39c7b996125581077a655bf5a8
Alex Runyan <alex@runyan.co>
2022-02-16 08:45:02 -0500

    Some refactoring for clarity and computedProperties mixin refactored to resolve bug

--- src/mixins/computedFields.js
+++ src/mixins/computedFields.js
@@ -1,26 +1,18 @@
-import _ from 'lodash';
 import { Parser } from 'expr-eval';
 
 export default {
   methods: {
     evaluateExpression(expression, type) {
+      const self = this;
       let value = null;
 
       try {
-        const self = this;
-        const merged = {};
-
-        _.merge(merged, self.vdata, self._data);
-
-        //monitor if variable belongs to data (defined variables) or vdata (external variables)
-        //in this way the event is not executed again when the variable is update
-        const data = new Proxy(merged, {
+        // Monitor if variable belongs to data (defined variables) or
+        // vdata (external variables)in this way the event is not
+        // executed again when the variable is update
+        const data = new Proxy(Object.assign({}, self, self.vdata), {
           get(data, name) {
-            if (undefined !== data[name]) {
-              return data[name];
-            }
-
-            return self.vdata[name];
+            return undefined === data[name] ? self.vdata[name] : data[name];
           },
           set() {
             throw 'You are not allowed to set properties from inside an expression';
