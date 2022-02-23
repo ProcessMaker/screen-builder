bb92f402cc971a10f01a6824177deb7f9df53e0e
danloa <danloa@gmail.com>
2022-02-23 09:44:10 -0400

    Do not copy computed fields to 'this' object

--- src/mixins/computedFields.js
+++ src/mixins/computedFields.js
@@ -6,15 +6,26 @@ export default {
     evaluateExpression(expression, type) {
       let value = null;
 
-      const merged = {};
-      _.merge(merged, this.vdata, this._data);
+      const self = this;
 
       try {
+        // 'this' will be used in the Proxy, to have the correct data will add to it all the vdata
+        // excluding the computed properties (to avoid circular references)
+        for (const attr in this.vdata) {
+          if (!_.has(this._computedWatchers, attr)) {
+            this[attr] = this.vdata[attr];
+          }
+        }
+
         //monitor if variable belongs to data (defined variables) or vdata (external variables)
         //in this way the event is not executed again when the variable is update
-        const data = new Proxy(merged, {
+        const data = new Proxy(Object.assign({}, this), {
           get(data, name) {
-            return data[name];
+            if (data[name] === undefined) {
+              return self.vdata[name];
+            } else {
+              return data[name];
+            }
           },
           set() {
             throw 'You are not allowed to set properties from inside an expression';
