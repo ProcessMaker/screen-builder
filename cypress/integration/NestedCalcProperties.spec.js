920d6b6f9d79c5e3bf8fc688d05ad772e8c378d1
Jose Chirivella <jose.chirivella@processmaker.com>
2022-07-28 15:58:33 -0400

    FOUR-6510 Swapping the object property location to match the deeply equal object to compare

--- cypress/integration/NestedCalcProperties.spec.js
+++ cypress/integration/NestedCalcProperties.spec.js
@@ -12,17 +12,17 @@ describe('nested calculated properties', () => {
     cy.get('[data-cy=preview] input[name="varA"]').type(10);
     cy.get('[data-cy=preview] input[name="varB"]').type(5);
     cy.get('[data-cy=preview-content] [data-cy="screen-field-operation"]').selectOption('Addition');
-    
+
     // Wait until you load the screen
     cy.wait(500);
 
     // Check the data of the screen
     cy.assertPreviewData({
-      'equal': '=',
-      'operation': 1,
-      'varA': 10,
-      'varB': 5,
       'varR': 15,
+      'varA': 10,
+      'operation': 1,
+      'varB': 5,
+      'equal': '=',
     });
   });
 });
