d3aedcad9e7d1361fa1de2ccd202e6e42a490bbf
Teisha McRae <mcraeteisha@gmail.com>
2022-08-09 14:38:38 -0400

    Install ESLint with configs for Prettier and Airbnb

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,71 +1,35 @@
 module.exports = {
-  root: true,
-
   env: {
+    browser: true,
+    es2020: true,
     node: true,
   },
 
   extends: [
-    'plugin:vue/essential',
-    'eslint:recommended',
+    'plugin:vue/recommended',
+    'airbnb-base',
+    'plugin:prettier/recommended'
   ],
 
-  rules: {
-    semi: [
-      'error',
-      'always',
-    ],
-    'comma-dangle': [
-      'error',
-      'always-multiline',
-    ],
-    indent: [
-      'error',
-      2,
-      {
-        SwitchCase: 1,
-      },
-    ],
-    quotes: [
-      'error',
-      'single',
-    ],
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'object-shorthand': 'error',
-    'space-before-function-paren': [
-      'error',
-      'never',
-    ],
-    'keyword-spacing': 'error',
-    'vue/html-indent': [
-      'error',
-      2,
-      {
-        alignAttributesVertically: false,
-      },
-    ],
-    'vue/html-closing-bracket-newline': [
-      'error',
-      {
-        singleline: 'never',
-        multiline: 'always',
-      },
-    ],
-    'vue/html-self-closing': 'error',
-    'vue/mustache-interpolation-spacing': 'error',
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
-
   parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
     parser: '@babel/eslint-parser',
   },
 
+  plugins: ['vue', 'prettier',],
+
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+
+  root: true,
+
   overrides: [
     {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
       env: {
         jest: true,
       },
