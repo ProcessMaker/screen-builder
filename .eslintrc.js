4583d685cf789fa331bff987fb5570ebd3e4a683
Jose Chirivella <jose.chirivella@processmaker.com>
2022-08-18 17:17:02 -0400

    FOUR-6064 Modified the eslint rules to adhere to our new rules * Eslintignore file to actually work and added the correct folders * Prettier rules to not add trailingCommas

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,37 +2,52 @@ module.exports = {
   env: {
     browser: true,
     es2020: true,
-    node: true,
+    node: true
   },
 
   extends: [
-    'plugin:vue/recommended',
-    'airbnb-base',
-    'plugin:prettier/recommended'
+    "plugin:vue/recommended",
+    "airbnb-base",
+    "plugin:prettier/recommended"
   ],
 
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    parser: '@babel/eslint-parser',
+    ecmaVersion: 6,
+    sourceType: "module",
+    parser: "@babel/eslint-parser"
   },
 
-  plugins: ['vue', 'prettier',],
+  plugins: ["vue", "prettier"],
 
   rules: {
-    'prettier/prettier': 'error',
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "prettier/prettier": ["error", { trailingComma: "none" }],
+    "no-unexpected-multiline": "error",
+    "no-param-reassign": 1,
+    eqeqeq: "error",
+    "max-len": ["error", { code: 140, ignoreUrls: true }],
+    "comma-dangle": ["error", "never"],
+    quotes: ["error", "double", { allowTemplateLiterals: true }],
+    "import/no-extraneous-dependencies": "warn",
+    "consistent-return": "warn",
+    "no-plusplus": 0,
+    "no-underscore-dangle": 0,
+    "no-restricted-syntax": "warn",
+    "no-continue": "warn",
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
   },
 
   root: true,
 
   overrides: [
     {
-      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+      ],
       env: {
-        jest: true,
-      },
-    },
-  ],
+        jest: true
+      }
+    }
+  ]
 };
