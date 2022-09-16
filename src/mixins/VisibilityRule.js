7daa5dc8a2e17420669015dd9753d47a86e8330f
David Callizaya <davidcallizaya@gmail.com>
2022-09-16 09:31:37 -0400

    Improve refresh validation event triggering

--- src/mixins/VisibilityRule.js
+++ src/mixins/VisibilityRule.js
@@ -1,36 +1,11 @@
 import { Parser } from 'expr-eval';
-import { debounce } from 'lodash';
 
 export default {
-  mounted() {
-    this.refreshValidationRulesByName = debounce(this.refreshValidationRulesByName, 500);
-
-    this.$root.$on('refresh-validation-rules', () => {
-      this.loadValidationRules();
-    });
-  },
   methods: {
-    refreshValidationRulesByName(fieldName, isVisible) {
-      if (fieldName) {
-        // Update the array of hidden fields
-        const fieldExists = this.hiddenFields__.indexOf(fieldName) !== -1;
-        if (isVisible && fieldExists) {
-          this.hiddenFields__ = this.hiddenFields__.filter((f) => f !== fieldName);
-        } else if (!isVisible && !fieldExists) {
-          this.hiddenFields__.push(fieldName);
-        }
-      }
-      this.$root.$emit('refresh-validation-rules');
-    },
-
-    visibilityRuleIsVisible(rule, fieldName) {
+    visibilityRuleIsVisible(rule) {
       try {
         const data = Object.assign({ _parent: this._parent }, this.vdata);
         const isVisible = !!Parser.evaluate(rule, Object.assign({}, data));
-
-        window.setTimeout(() => {
-          this.refreshValidationRulesByName(fieldName, isVisible);
-        }, 250);
         return isVisible;
       } catch (e) {
         return false;
