c2d3903f5c5a574e4377303e5ca8c8ad2ff230b9
Jose Chirivella <jose.chirivella@processmaker.com>
2022-10-12 16:25:37 -0500

    FOUR-6840 Indenting and sorting the vue.config.js file

--- vue.config.js
+++ vue.config.js
@@ -1,42 +1,45 @@
-const path = require('path');
+const path = require("path");
+const MonacoEditorPlugin = require("monaco-editor-webpack-plugin");
 
 const plugins = [];
-if (process.env.NODE_ENV !== 'production') {
-  const MonocoEditorPlugin = require('monaco-editor-webpack-plugin');
-  plugins.push(new MonocoEditorPlugin({
-    languages: ['javascript', 'typescript', 'css', 'json'],
-  }));
+if (process.env.NODE_ENV !== "production") {
+  plugins.push(
+    new MonacoEditorPlugin({
+      languages: ["javascript", "typescript", "css", "json"]
+    })
+  );
 }
 
 module.exports = {
   runtimeCompiler: true,
-  transpileDependencies: ['vuetable-2'],
+  transpileDependencies: ["vuetable-2"],
   configureWebpack: {
     plugins,
     resolve: {
-      modules: [
-        path.resolve(__dirname, 'node_modules'),
-        'node_modules',
-      ],
+      modules: [path.resolve(__dirname, "node_modules"), "node_modules"],
       symlinks: false,
       alias: {
-        '@processmaker/vue-form-elements$': process.env.NODE_ENV === 'development'
-          ? '@processmaker/vue-form-elements/src/components'
-          : '@processmaker/vue-form-elements',
-      },
+        "@processmaker/vue-form-elements$":
+          process.env.NODE_ENV === "development"
+            ? "@processmaker/vue-form-elements/src/components"
+            : "@processmaker/vue-form-elements"
+      }
     },
-    externals: process.env.NODE_ENV === 'production' ? [
-      'vue-monaco',
-      'monaco-editor',
-      'vue-deepset',
-      /^@fortawesome\/.+$/,
-      'vue',
-      'vuex',
-      /^bootstrap\/.+$/,
-      /^@processmaker\/.+$/,
-      'i18next',
-      '@panter/vue-i18next',
-      'validatorjs',
-    ] : [],
-  },
+    externals:
+      process.env.NODE_ENV === "production"
+        ? [
+            "vue-monaco",
+            "monaco-editor",
+            "vue-deepset",
+            /^@fortawesome\/.+$/,
+            "vue",
+            "vuex",
+            /^bootstrap\/.+$/,
+            /^@processmaker\/.+$/,
+            "i18next",
+            "@panter/vue-i18next",
+            "validatorjs"
+          ]
+        : []
+  }
 };
