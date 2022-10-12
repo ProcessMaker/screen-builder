ce78c23722db8c0fb8437264e4b4a3f36967223f
David Callizaya <davidcallizaya@gmail.com>
2022-10-12 13:49:26 -0400

    Setup cypress-audit

--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -1,6 +1,15 @@
+const { lighthouse, prepareAudit } = require('cypress-audit');
+
 module.exports = (on, config) => {
   require('@cypress/code-coverage/task')(on, config);
   on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'));
+  // cypress-audit: Setup the lighthouse plugin
+  on("before:browser:launch", (_browser, launchOptions) => {
+    prepareAudit({ args: launchOptions });
+  });
+  on("task", {
+    lighthouse: lighthouse()
+  });
   return Object.assign({}, config, {
     fixturesFolder: 'tests/e2e/fixtures',
     integrationFolder: 'tests/e2e/specs',
