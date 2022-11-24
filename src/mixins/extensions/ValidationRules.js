3beb73f3d4566d078e875915732ccaaf0563606b
Agustin Busso <agustin.busso@processmaker.com>
2022-11-24 09:54:47 -0300

    Check if valid variable name and show error

--- src/mixins/extensions/ValidationRules.js
+++ src/mixins/extensions/ValidationRules.js
@@ -1,5 +1,6 @@
 import { validationMixin } from 'vuelidate';
 import VueVuelidateJsonschema from 'vue-vuelidate-jsonschema';
+import { javascriptReservedKeywords } from '../../form-control-common-properties';
 
 export default {
   mounted() {
@@ -10,8 +11,14 @@ export default {
           delete properties[':validation'];
           delete properties['validation'];
           // Add validation class and error message
-          properties[':class'] = `{ 'form-group--error': ${this.checkVariableExists('$v.vdata.' + element.config.name)} && $v.vdata.${element.config.name}.$invalid || ${this.checkVariableExists('$v.schema.' + element.config.name)} && $v.schema.${element.config.name}.$invalid }`;
-          properties[':error'] = `${this.checkVariableExists('$v.vdata.' + element.config.name)} && validationMessage($v.vdata.${element.config.name}) || ${this.checkVariableExists('$v.schema.' + element.config.name)} && validationMessage($v.schema.${element.config.name})`;
+          if (!this.isValidVariableName(element.config.name)) {
+            if (this.$store.getters["globalErrorsModule/getMode"] === "preview") {
+              console.error("Invalid variable name");
+            }
+          } else {
+            properties[':class'] = `{ 'form-group--error': ${this.checkVariableExists('$v.vdata.' + element.config.name)} && $v.vdata.${element.config.name}.$invalid || ${this.checkVariableExists('$v.schema.' + element.config.name)} && $v.schema.${element.config.name}.$invalid }`;
+            properties[':error'] = `${this.checkVariableExists('$v.vdata.' + element.config.name)} && validationMessage($v.vdata.${element.config.name}) || ${this.checkVariableExists('$v.schema.' + element.config.name)} && validationMessage($v.schema.${element.config.name})`;
+          }
         }
       },
       onbuild({ screen }) {
@@ -21,6 +28,21 @@ export default {
     });
   },
   methods: {
+    isValidVariableName(name) {
+      const validVariableNameExp = RegExp('^[A-Za-z][0-9a-zA-Z_$]*$');
+      const nameParts = name.split(".");
+      let valid = true;
+      nameParts.forEach((item) => {
+        if (!validVariableNameExp.test(item)) {
+          valid = false;
+        }
+        if (javascriptReservedKeywords.split(",").includes(item)) {
+          valid = false;
+        }
+      });
+
+      return valid;
+    },
     checkVariableExists(name) {
       const check =name.split('.').reduce((check, el) => {
         check.variable = check.variable ? `${check.variable}.${el}` : el;
