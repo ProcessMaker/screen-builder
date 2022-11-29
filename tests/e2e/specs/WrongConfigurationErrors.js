0c2f590c9dadda6f8aeb78a4789f2d7ca53e537a
Agustin Busso <agustin.busso@processmaker.com>
2022-11-29 16:22:05 -0300

    Add test case

--- tests/e2e/specs/WrongConfigurationErrors.js
+++ tests/e2e/specs/WrongConfigurationErrors.js
@@ -1,5 +1,5 @@
 describe("Wrong Configuration Errors", () => {
-  it("Screen rendering problem warning", () => {
+  it("Screen with rendering problem in a component should show a warning", () => {
     cy.visit("/", {
       onBeforeLoad(win) {
         cy.stub(win.console, "warn").as("consoleWarn");
@@ -19,24 +19,34 @@ describe("Wrong Configuration Errors", () => {
       "There was a problem rendering the screen"
     );
   });
-//   it("Screen rendering valid screen", () => {
-//     cy.visit("/", {
-//       onBeforeLoad(win) {
-//         cy.stub(win.console, "warn").as("consoleWarn");
-//         cy.stub(win.console, "error").as("consoleError");
-//       }
-//     });
-//     cy.get("[data-cy=controls-FormInput]").drag(
-//       "[data-cy=screen-drop-zone]",
-//       "bottom"
-//     );
-//     cy.get("[data-cy=screen-element-container]").click();
-//     cy.get("[data-cy=inspector-name]").clear().type("s.1");
-//     cy.get("[data-cy=mode-preview]").click();
-//     cy.get("[data-cy=mode-editor]").click();
-//     cy.get("@consoleWarn").shouldNot(
-//       "be.calledWith",
-//       "There was a problem rendering the screen"
-//     );
-//   });
+
+  it("Screen with rendering problem in a component should not show a warning after the problematic component is deleted", () => {
+    cy.visit("/", {
+      onBeforeLoad(win) {
+        cy.stub(win.console, "warn").as("consoleWarn");
+        cy.stub(win.console, "error").as("consoleError");
+      }
+    });
+    cy.get("[data-cy=controls-FormInput]").drag(
+      "[data-cy=screen-drop-zone]",
+      "bottom"
+    );
+    cy.get("[data-cy=screen-element-container]").click();
+    cy.get("[data-cy=inspector-name]").clear().type("s.1");
+    cy.get("[data-cy=mode-preview]").click();
+    cy.get("[data-cy=mode-editor]").click();
+    cy.get("@consoleWarn").should(
+      "be.calledWith",
+      "There was a problem rendering the screen"
+    );
+    cy.get("[data-cy=screen-element-container] .ml-auto > .btn-danger").click();
+    cy.get("[data-cy=controls-FormInput]").drag(
+      "[data-cy=screen-drop-zone]",
+      "bottom"
+    );
+    cy.get("[data-cy=mode-preview]").click();
+    cy.get("[data-cy=mode-editor]").click();
+    cy.get("@consoleError").should("not.to.be.called");
+    cy.get("@consoleWarn").should("not.to.be.calledTwice");
+  });
 });
