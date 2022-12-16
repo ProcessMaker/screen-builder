8eda959b867c2ab1d9747dd2418d67ab153b61e6
Agustin Busso <agustin.busso@processmaker.com>
2022-12-16 13:01:36 -0300

    Add reset function

--- src/mixins/ScreenBase.js
+++ src/mixins/ScreenBase.js
@@ -150,6 +150,11 @@ export default {
       }
       this.$emit('submit', this.vdata);
     },
+    resetValue(variableName) {
+      console.log("Reset value");
+      console.log(variableName);
+      this.setValue(variableName, null);
+    },
     getValidationData() {
       return this.vdata;
     },
