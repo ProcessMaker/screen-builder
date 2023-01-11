5342b53904b83f42ed35964174c9a743a5cdb213
David Callizaya <davidcallizaya@gmail.com>
2023-01-11 18:46:05 -0400

    Test validation problem with nested screen

---
+++ tests/e2e/specs/TestValidationWithNestedSideEffects.spec.js
@@ -0,0 +1,19 @@
+describe("test validation with nested side effects", () => {
+  beforeEach(() => {
+    cy.server();
+    cy.visit("/");
+  });
+
+  it("Verify test validation with nested side effects", () => {
+    cy.loadFromJson("Test Validation with Nested side effects.json", 1);
+    // click on preview
+    cy.get("[data-cy=mode-preview]").click();
+    // click on New Submit button
+    cy.get("[data-cy=preview-content] button[aria-label='Submit']").click();
+    // In editor: ensure standard required field displays error while readonly required field does not
+    cy.get("[data-cy=preview-content] [name='form_input_2']")
+      .parent()
+      .find(".invalid-feedback")
+      .should("be.visible");
+  });
+});
