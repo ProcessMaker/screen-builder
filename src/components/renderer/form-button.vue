5b71d185bf86783caea45a5cc7b9ce755a29c496
Rodrigo Quelca <rodrigo@cd ..processmaker.com>
2023-01-19 15:20:39 -0400

    FOUR-7000: Improve Submit button component

--- src/components/renderer/form-button.vue
+++ src/components/renderer/form-button.vue
@@ -1,49 +1,100 @@
 <template>
-  <div class="form-group"  style="overflow-x: hidden">
-    <button v-b-tooltip="options" @click="click" :class="classList" :name="name" :aria-label="$attrs['aria-label']" :tabindex="$attrs['tabindex']">
+  <div class="form-group" style="overflow-x: hidden">
+    <button
+      v-b-tooltip="tooltipOptions"
+      :class="classList"
+      :name="name"
+      :aria-label="ariaLabel"
+      :tabindex="tabIndex"
+      @click="click"
+    >
       {{ label }}
     </button>
   </div>
 </template>
 
 <script>
-import Mustache from 'mustache';
-import { mapActions, mapState } from "vuex";
-import { getValidPath } from '@/mixins';
-
+import Mustache from "mustache";
+import { getValidPath } from "@/mixins";
 
 export default {
   mixins: [getValidPath],
-  props: ['variant', 'label', 'event', 'eventData', 'name', 'fieldValue', 'value', 'tooltip', 'transientData'],
+  props: {
+    variant: {
+      type: String,
+      default: "primary"
+    },
+    label: {
+      type: String,
+      default: "New Submit"
+    },
+    event: {
+      type: String,
+      default: "submit"
+    },
+    eventData: {
+      type: Object,
+      default: null
+    },
+    name: {
+      type: String,
+      default: null
+    },
+    fieldValue: {
+      type: String,
+      default: "1"
+    },
+    value: {
+      type: String,
+      default: "1"
+    },
+    tooltip: {
+      type: Object,
+      default: null
+    },
+    transientData: {
+      type: Object,
+      default: null
+    }
+  },
+  data() {
+    return {
+      ariaLabel: this.$attrs["aria-label"],
+      tabIndex: this.$attrs.tabindex
+    };
+  },
   computed: {
-    ...mapState('globalErrorsModule', ['valid']),
     classList() {
-      let variant = this.variant || 'primary';
+      const variant = this.variant || "primary";
       return {
         btn: true,
-        ['btn-' + variant]: true,
-        disabled: this.event === 'submit' && !this.valid
+        [`btn-${variant}`]: true,
+        disabled: this.event === "submit" && !this.valid
       };
     },
-    options() {
-      if (!this.tooltip || this.event === 'submit') {
+    tooltipOptions() {
+      if (!this.tooltip || this.event === "submit") {
         return {};
       }
-
-      let content = '';
+      let content = "";
       try {
-        content = Mustache.render(this.tooltip.content || '', this.transientData);
-      } catch (error) { error; }
+        content = Mustache.render(
+          this.tooltip.content || "",
+          this.transientData
+        );
+      } catch (error) {
+        console.error(error);
+      }
 
       return {
         title: content,
         html: true,
-        placement: this.tooltip.position || '',
-        trigger: 'hover',
-        variant: this.tooltip.variant || '',
-        boundary: 'window',
+        placement: this.tooltip.position || "",
+        trigger: "hover",
+        variant: this.tooltip.variant || "",
+        boundary: "window"
       };
-    },
+    }
   },
   methods: {
     setValue(parent, name, value) {
@@ -56,26 +107,26 @@ export default {
       }
     },
     async click() {
-      if (this.event === 'script') {
-        const trueValue = this.fieldValue || '1';
-        const value = (this.value == trueValue) ? null : trueValue;
-        this.$emit('input', value);
+      if (this.event === "script") {
+        const trueValue = this.fieldValue || "1";
+        const value = this.value === trueValue ? null : trueValue;
+        this.$emit("input", value);
       }
-      if (this.event !== 'pageNavigate' && this.name) {
+      if (this.event !== "pageNavigate" && this.name) {
         this.setValue(this.$parent, this.name, this.fieldValue);
       }
-      if (this.event === 'submit') {
-        this.$emit('input', this.fieldValue);
-        this.$nextTick(()=>{
-          this.$emit('submit', this.eventData);
+      if (this.event === "submit") {
+        this.$emit("input", this.fieldValue);
+        this.$nextTick(() => {
+          this.$emit("submit", this.eventData);
         });
         return;
       }
       this.$emit(this.event, this.eventData);
-      if (this.event === 'pageNavigate') {
-        this.$emit('page-navigate', this.eventData);
+      if (this.event === "pageNavigate") {
+        this.$emit("page-navigate", this.eventData);
       }
-    },
-  },
+    }
+  }
 };
 </script>
