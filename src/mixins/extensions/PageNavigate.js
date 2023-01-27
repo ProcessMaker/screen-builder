bc6b5fb01d910ff50f246d1b0502c1f4ac4b346e
Rodrigo Quelca <rodrigo@cd ..processmaker.com>
2023-01-27 14:25:17 -0400

    FOUR-6892: Improve page navigation component

--- src/mixins/extensions/PageNavigate.js
+++ src/mixins/extensions/PageNavigate.js
@@ -3,24 +3,27 @@ export default {
     /* istanbul ignore next */
     pageNavigate() {},
     pageNavigationProperties({ properties }) {
-      properties['@page-navigate'] = 'pageNavigate';
-      properties[':validate'] = '$v';
+      properties["@page-navigate"] = "pageNavigate";
+      properties[":validate"] = "$v";
     },
     pageNavigationBuild(screen) {
-      this.addData(screen, 'currentPage__', 'this._initialPage');
-      screen.methods.pageNavigate = function(page) {
+      this.addData(screen, "currentPage__", "this._initialPage");
+      screen.methods.pageNavigate = function (page) {
         // Skip navigate button if page is not defined
         if (!this.$parent.definition.config[page]) {
           return;
         }
         this.currentPage__ = page;
       };
-    },
+    }
   },
   mounted() {
     this.extensions.push({
       onloadproperties(params) {
-        if (params.element.component === 'FormButton' && params.element.config.event === 'pageNavigate') {
+        if (
+          params.element.component === "FormButton" &&
+          params.element.config.event === "pageNavigate"
+        ) {
           this.pageNavigationProperties(params);
         }
       },
