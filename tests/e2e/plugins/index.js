40b2013a57d282f062272ea031342859ac3cc664
Jose Chirivella <jose.chirivella@processmaker.com>
2023-01-31 18:08:23 -0600

    FOUR-6840 Commenting out code-coverage for cypress * Added .babelrc * Added coverage folder to the things not to watch for changes

--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -1,8 +1,8 @@
 const { lighthouse, prepareAudit } = require('cypress-audit');
 
 module.exports = (on, config) => {
-  require('@cypress/code-coverage/task')(on, config);
-  on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'));
+  // require('@cypress/code-coverage/task')(on, config);
+  // on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'));
   // cypress-audit: Setup the lighthouse plugin
   on("before:browser:launch", (_browser, launchOptions) => {
     prepareAudit({ args: Array.isArray(launchOptions) ? launchOptions : [] });
