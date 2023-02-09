bf414160ef0d3a452cb743a0b23c4cddd878fc63
Jose Chirivella <jose.chirivella@processmaker.com>
2023-02-09 11:24:00 -0600

    FOUR-6510 Moved the lighthouse config to the new cypress folder * Removed the old folder for cypress

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,7 +1,14 @@
+const { lighthouse, prepareAudit } = require('cypress-audit');
+
 module.exports = (on, config) => {
   require('@cypress/code-coverage/task')(on, config);
   // include any other plugin code...
-
+  on("before:browser:launch", (_browser, launchOptions) => {
+    prepareAudit({ args: Array.isArray(launchOptions) ? launchOptions : [] });
+  });
+  on("task", {
+    lighthouse: lighthouse()
+  });
   // It's IMPORTANT to return the config object
   // with any changed environment variables
   return config;
