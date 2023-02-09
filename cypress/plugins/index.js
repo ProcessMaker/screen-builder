c3d4b706ac2fb0887f00930e05b807b45d7c01c6
Jose Chirivella <jose.chirivella@processmaker.com>
2023-02-09 15:44:12 -0600

    FOUR-6510 Added a utils file to check for fileSize (jpegs at this moment) and check if file exists in Downloads folder * Added screenshot and downloads folder to gitignore * Added a cypress task if we execute the command "findFiles"

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,6 @@
 const { lighthouse, prepareAudit } = require('@cypress-audit/lighthouse');
 const { pa11y } = require("@cypress-audit/pa11y");
+const globby = require("globby");
 
 module.exports = (on, config) => {
   require('@cypress/code-coverage/task')(on, config);
@@ -9,7 +10,27 @@ module.exports = (on, config) => {
   });
   on("task", {
     lighthouse: lighthouse(),
-    pa11y: pa11y()
+    pa11y: pa11y(),
+    // a task to find one file matching the given mask
+    // returns just the first matching file
+    async findFiles(mask) {
+      if (!mask) {
+        throw new Error("Missing a file mask to search");
+      }
+
+      console.log("searching for files %s", mask);
+      const list = await globby(mask);
+
+      if (!list.length) {
+        console.log("found no files");
+
+        return null;
+      }
+
+      console.log("found %d files, first one %s", list.length, list[0]);
+
+      return list[0];
+    }
   });
   // It's IMPORTANT to return the config object
   // with any changed environment variables
