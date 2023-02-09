a0a2d0c2a8dbf652bb36f33969286a4435337e52
Rodrigo Quelca <rodrigo@cd ..processmaker.com>
2023-02-09 10:12:39 -0400

    add collection to store the images as a blob file+

--- src/store/modules/blobImagesModule.js
+++ src/store/modules/blobImagesModule.js
@@ -2,7 +2,7 @@ const blobImagesModule = {
   namespaced: true,
   state() {
     return {
-      blobImages: {}
+      blobImages: []
     };
   },
   mutations: {
@@ -12,19 +12,11 @@ const blobImagesModule = {
         URL.revokeObjectURL(state.blobImages[prop]);
       }
       // eslint-disable-next-line no-param-reassign
-      state.blobImages = {};
+      state.blobImages = [];
     },
     buildBlobImages(state, payload) {
-      // eslint-disable-next-line no-param-reassign
-      if (payload.image) {
-        const binaryData = atob(payload.image.split(",")[1]);
-        const binaryArray = new Uint8Array(binaryData.length);
-        for (let i = 0; i < binaryData.length; i++) {
-          binaryArray[i] = binaryData.charCodeAt(i);
-        }
-        const blob = new Blob([binaryArray], { type: "image/jpeg" });
-        // eslint-disable-next-line no-param-reassign
-        state.blobImages[payload.name] = URL.createObjectURL(blob);
+      if (payload && payload.image) {
+        state.blobImages.push(payload);
       }
     }
   },
