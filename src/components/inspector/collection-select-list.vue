31aaedbeaf50e9b0790d31c7c054b7ba320cdfcf
Nolan Ehrstrom <github@nolanpro.com>
2023-04-12 13:50:15 -0700

    Add unique option

--- src/components/inspector/collection-select-list.vue
+++ src/components/inspector/collection-select-list.vue
@@ -45,6 +45,15 @@
       }}</small>
     </div>
 
+    <div class="mt-3" v-if="fields.length > 1">
+      <form-checkbox
+        :label="$t('Return unique values')"
+        v-model="unique"
+        helper=""
+        data-cy="inspector-collection-isDependent"
+      />
+    </div>
+
   </div>
 </template>
 
@@ -58,6 +67,7 @@ const CONFIG_FIELDS = [
   "labelField",
   "valueField",
   "pmql",
+  "unique",
 ];
 
 export default {
@@ -74,6 +84,7 @@ export default {
       labelField: null,
       valueField: null,
       pmql: "",
+      unique: false,
     };
   },
   watch: {
