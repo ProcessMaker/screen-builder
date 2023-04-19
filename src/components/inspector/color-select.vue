c3933ead9d08c8feb056de96760593e4482e497d
Eleazar Resendez <eleazar.resendez@processmaker.com>
2023-04-19 14:29:10 -0600

    Apply eslint fixes

--- src/components/inspector/color-select.vue
+++ src/components/inspector/color-select.vue
@@ -4,65 +4,69 @@
       <label>{{ label }}</label>
       <b-button-toolbar>
         <b-button-group size="sm">
-          <b-button size="sm"
-            variant="outline-light"
+          <b-button
             v-for="option in options"
             :key="option.value"
+            size="sm"
+            variant="outline-light"
             class="btn btn-sm mr-1 pr-1 pl-1 pt-0 pb-0 btn-outline-none"
             :class="['bg-' + parsedColor(option.value)]"
             :title="option.content"
           >
-            <i class="fas fa-check"
-              :class="[option.value === value ? 'text-light' : 'text-' + parsedColor(option.value)]"
+            <i
+              class="fas fa-check"
+              :class="[
+                option.value === value
+                  ? 'text-light'
+                  : 'text-' + parsedColor(option.value)
+              ]"
               @click="selectColor(option.value)"
             />
           </b-button>
         </b-button-group>
       </b-button-toolbar>
-      <small @click="checkColor">
-        <i class="fas fa-ban"/>
-        {{ $t('Clear Color Selection') }}
+      <small @click="checkColor()">
+        <i class="fas fa-ban" />
+        {{ $t("Clear Color Selection") }}
       </small>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  props: ['label', 'value', 'helper', 'options'],
-  components: {
-  },
+  components: {},
+  props: ["label", "value", "helper", "options"],
   data() {
     return {
-      newColor: '',
+      newColor: ""
     };
   },
   computed: {
     hasColor() {
       return !!this.value;
-    },
+    }
   },
   methods: {
     checkColor() {
-      this.hasColor ? this.$emit('input', '') : null;
+      this.hasColor ? this.$emit("input", "") : null;
     },
     selectColor(color) {
-      this.$emit('input', color);
+      this.$emit("input", color);
     },
     parsedColor(color) {
-      return color.split('-')[1];
-    },
-  },
+      return color.split("-")[1];
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-  .image-preview {
-    border: 1px solid #ced4da;
-    border-radius: 4px;
-    height: 4em;
-    text-align: center;
-    overflow: hidden;
-  }
+.image-preview {
+  border: 1px solid #ced4da;
+  border-radius: 4px;
+  height: 4em;
+  text-align: center;
+  overflow: hidden;
+}
 </style>
