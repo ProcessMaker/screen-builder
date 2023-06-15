d432b775a36dd069b98a1bed72b3ac39e93fbf72
danloa <danloa@gmail.com>
2023-06-15 15:34:21 -0400

    Add device visibility toggles for all components

---
+++ src/components/inspector/device-visibility.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="form-group">
+    <div>
+      <label>{{ label }}</label>
+
+      <b-form-checkbox
+        v-model="settings.showForDesktop"
+        :value="true"
+        :unchecked-value="false"
+        switch
+      >
+        {{ $t("Show for Desktop") }}
+      </b-form-checkbox>
+
+      <b-form-checkbox
+        v-model="settings.showForMobile"
+        :value="true"
+        :unchecked-value="false"
+        switch
+      >
+        {{ $t("Show for Mobile") }}
+      </b-form-checkbox>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: ["label", "value", "helper", "options"],
+  data() {
+    return {
+      settings: {
+        showForDesktop: true,
+        showForMobile: true
+      }
+    };
+  },
+  watch: {
+    settings: {
+      handler() {
+        this.emitChanges();
+      },
+      deep: true
+    },
+  },
+  methods: {
+    emitChanges() {
+      this.$emit("input", this.settings);
+      this.$emit("update-state");
+    },
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.image-preview {
+  border: 1px solid #ced4da;
+  border-radius: 4px;
+  height: 4em;
+  text-align: center;
+  overflow: hidden;
+}
+</style>
