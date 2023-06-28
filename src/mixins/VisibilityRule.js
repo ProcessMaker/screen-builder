04fd03ed96145154e5cf4c725b4aea3e9d917025
danloa <danloa@gmail.com>
2023-06-28 09:26:47 -0400

    Use screen size to show/hide elements

--- src/mixins/VisibilityRule.js
+++ src/mixins/VisibilityRule.js
@@ -2,13 +2,25 @@ import { Parser } from "expr-eval";
 
 export default {
   methods: {
-    visibilityRuleIsVisible(rule) {
+    visibilityRuleIsVisible(rule, name, deviceVisibility) {
+      // const isMobile = window.ProcessMaker && window.ProcessMaker.isMobile
+      //   ? window.Processmaker.isMobile
+      //   : false;
+
+      const visibility = deviceVisibility || {showForDesktop: true, showForMobile: true, isMobile: false};
+      const visibleInDevice =
+        (visibility.isMobile && visibility.showForMobile) ||
+        (!visibility.isMobile && visibility.showForDesktop);
+
       try {
-        const dataWithParent = this.getDataReference();
-        const isVisible = Boolean(Parser.evaluate(rule, dataWithParent));
-        return isVisible;
+        if (rule && rule.trim().length > 0) {
+          const dataWithParent = this.getDataReference();
+          const isVisible = Boolean(Parser.evaluate(rule, dataWithParent));
+          return isVisible && visibleInDevice;
+        }
+        return visibleInDevice;
       } catch (e) {
-        return false;
+        return true;
       }
     }
   }
