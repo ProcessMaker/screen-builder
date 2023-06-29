08fd3d38ce8d4d67b57288b58ead4f28d4621e3a
Miguel Angel <miguelangeldev@icloud.com>
2023-06-29 11:23:43 -0400

    test: add responsive preview e2e test cases

--- src/components/vue-form-renderer.vue
+++ src/components/vue-form-renderer.vue
@@ -1,5 +1,9 @@
 <template>
-  <div :class="[containerClass, containerDeviceClass]" :style="cssDevice">
+  <div
+    :class="[containerClass, containerDeviceClass]"
+    :style="cssDevice"
+    data-cy="screen-renderer-container"
+  >
     <custom-css-output>{{ customCssWrapped }}</custom-css-output>
     <screen-renderer
       ref="renderer"
