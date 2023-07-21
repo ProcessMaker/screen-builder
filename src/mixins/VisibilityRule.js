d377096c3a7a969dfa35734ce07bea965c1defa2
Miguel Angel <miguelangeldev@icloud.com>
2023-07-21 10:06:18 -0400

    fix: validate variable in visibility rule mixin

--- src/mixins/VisibilityRule.js
+++ src/mixins/VisibilityRule.js
@@ -1,23 +1,26 @@
-import { Parser } from "expr-eval";
+import { Parser } from 'expr-eval';
 
 export default {
   methods: {
     visibilityRuleIsVisible(rule, name, deviceVisibility) {
-      const visibility = deviceVisibility || {showForDesktop: true, showForMobile: true, isMobile: false};
+      const visibility = deviceVisibility || { showForDesktop: true, showForMobile: true, isMobile: false };
       const visibleInDevice =
-        (visibility.isMobile && visibility.showForMobile) ||
-        (!visibility.isMobile && visibility.showForDesktop);
+        (visibility.isMobile && visibility.showForMobile) || (!visibility.isMobile && visibility.showForDesktop);
 
       try {
         if (rule && rule.trim().length > 0) {
           const dataWithParent = this.getDataReference();
           const isVisible = Boolean(Parser.evaluate(rule, dataWithParent));
+
           return isVisible && visibleInDevice;
         }
+
         return visibleInDevice;
       } catch (e) {
-        return visibleInDevice;
+        // empty.
       }
+
+      return false;
     }
   }
 };
