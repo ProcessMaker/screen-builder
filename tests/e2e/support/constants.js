b6fc77016821365be47711c44c53d1164be234a2
ana <ana.mauricio.gutierrez@gmail.com>
2023-07-21 17:34:33 -0400

    Add test cases

---
+++ tests/e2e/support/constants.js
@@ -0,0 +1,19 @@
+/**
+ * Node types are controls of the screen
+ */
+export const nodeControls = {
+  checkbox: '[data-cy=controls-FormCheckbox]',
+  datePicker: '[data-cy=controls-FormDatePicker]',
+  fileDownload: '[data-cy=controls-FileDownload]',
+  fileUpload: '[data-cy=controls-FileUpload]',
+  formImage: '[data-cy=controls-FormImage]',
+  formInput: '[data-cy=controls-FormInput]',
+  formLoop: '[data-cy=controls-FormLoop]',
+  formMultiColumn: '[data-cy=controls-FormMultiColumn]',
+  formNestedScreen: '[data-cy=controls-FormNestedScreen]',
+  formRecordList: '[data-cy=controls-FormRecordList]',
+  formHtmlViewer: '[data-cy=controls-FormHtmlViewer]',
+  formSelectList: '[data-cy=controls-FormSelectList]',
+  formSubmit: '[data-cy=controls-FormButton]',
+  formTextArea: '[data-cy=controls-FormTextArea]'
+};
