63fd0633af0c1b4722d70ec9bc1b5c76c9ea91b2
Agustín Busso <90727999+agustinbusso@users.noreply.github.com>
2023-09-04 10:10:14 -0300

    Fix ai control styles in loop

--- src/components/editor/loop.vue
+++ src/components/editor/loop.vue
@@ -1,97 +1,97 @@
 <template>
   <div class="column-draggable" :selector="config.customCssSelector">
     <draggable
-      style="min-height: 80px;"
-      :list="items"
-      group="controls"
+    style="min-height: 80px;"
+    :list="items"
+    group="controls"
     >
-      <div class="control-item"
-        :class="{selected: selected === element, hasError: hasError(element)}"
-        v-for="(element,index) in items"
-        :key="index"
-        @click.stop="inspect(element)"
-      >
-        <div v-if="element.container" @click.stop="inspect(element)">
-          <div class="m-2 card border-0">
-            <div
-              v-if="selected === element"
-              class="card-header form-element-header d-flex align-items-center border rounded"
-            >
-              <i class="fas fa-arrows-alt-v mr-1 text-muted"/>
-              <i v-if="element.config.icon" :class="element.config.icon" class="mr-2 ml-1"/>
-              {{ element.config.name || $t('Variable Name') }}
-              <div class="ml-auto">
-                <button
-                  class="btn btn-sm btn-secondary mr-2"
-                  :title="$t('Copy Control')"
-                  @click="duplicateItem(index)"
-                >
-                  <i class="fas fa-copy text-light"/>
-                </button>
-                <button
-                  class="btn btn-sm btn-danger"
-                  @click="deleteItem(index)"
-                  :aria-label="$t('Delete')"
-                >
-                  <i class="far fa-trash-alt text-light"/>
-                </button>
-              </div>
-            </div>
-
-            <component :class="elementCssClass(element)"
-              :validationErrors="validationErrors"
-              class="mb-3 mr-3 ml-3"
-              :selected="selected"
-              @inspect="inspect"
-              @update-state="$emit('update-state')"
-              v-model="element.items"
-              :config="element.config"
-              :is="element['editor-component']"
-            />
-          </div>
-        </div>
-
-        <div v-else :id="element.config.name ? element.config.name : undefined">
-          <div class="m-2" :class="{ 'card' : selected === element }">
-            <div
-              v-if="selected === element"
-              class="card-header form-element-header d-flex align-items-center"
-            >
-              <i class="fas fa-arrows-alt-v mr-1 text-muted"/>
-              <i v-if="element.config.icon" :class="element.config.icon" class="mr-2 ml-1"/>
-              {{ element.config.name || $t('Variable Name') }}
-              <div class="ml-auto">
-                <button
-                  class="btn btn-sm btn-secondary mr-2"
-                  :title="$t('Copy Control')"
-                  @click="duplicateItem(index)"
-                >
-                  <i class="fas fa-copy text-light"/>
-                </button>
-                <button
-                  class="btn btn-sm btn-danger"
-                  @click="deleteItem(index)"
-                  :aria-label="$t('Delete')"
-                >
-                  <i class="far fa-trash-alt text-light"/>
-                </button>
-              </div>
-            </div>
-
-            <component
-              class="p-3"
-              :class="[elementCssClass(element), { 'prevent-interaction': !element.config.interactive }]"
-              :tabindex="element.config.interactive ? 0 : -1"
-              v-bind="element.config"
-              :config="element.config"
-              @input="element.config.interactive ? element.config.content = $event : null"
-              :is="element['editor-component']"
-            />
-          </div>
-        </div>
-      </div>
-    </draggable>
+    <div class="control-item"
+    :class="{selected: selected === element, hasError: hasError(element)}"
+    v-for="(element,index) in items"
+    :key="index"
+    @click.stop="inspect(element)"
+    >
+    <div v-if="element.container" @click.stop="inspect(element)">
+      <div class="m-2 card border-0" :class="{ 'ai-section-card': isAiSection(element) }">
+        <div
+        v-if="selected === element"
+        class="card-header form-element-header d-flex align-items-center border rounded"
+        >
+        <i class="fas fa-arrows-alt-v mr-1 text-muted"/>
+        <i v-if="element.config.icon" :class="element.config.icon" class="mr-2 ml-1"/>
+        {{ element.config.name || $t('Variable Name') }}
+        <div class="ml-auto">
+          <button
+          class="btn btn-sm btn-secondary mr-2"
+          :title="$t('Copy Control')"
+          @click="duplicateItem(index)"
+          >
+          <i class="fas fa-copy text-light"/>
+        </button>
+        <button
+        class="btn btn-sm btn-danger"
+        @click="deleteItem(index)"
+        :aria-label="$t('Delete')"
+        >
+        <i class="far fa-trash-alt text-light"/>
+      </button>
+    </div>
   </div>
+  
+  <component :class="elementCssClass(element)"
+  :validationErrors="validationErrors"
+  class="mb-3 mr-3 ml-3"
+  :selected="selected"
+  @inspect="inspect"
+  @update-state="$emit('update-state')"
+  v-model="element.items"
+  :config="element.config"
+  :is="element['editor-component']"
+  />
+</div>
+</div>
+
+<div v-else :id="element.config.name ? element.config.name : undefined">
+  <div class="m-2" :class="{ 'card' : selected === element }">
+    <div
+    v-if="selected === element"
+    class="card-header form-element-header d-flex align-items-center"
+    >
+    <i class="fas fa-arrows-alt-v mr-1 text-muted"/>
+    <i v-if="element.config.icon" :class="element.config.icon" class="mr-2 ml-1"/>
+    {{ element.config.name || $t('Variable Name') }}
+    <div class="ml-auto">
+      <button
+      class="btn btn-sm btn-secondary mr-2"
+      :title="$t('Copy Control')"
+      @click="duplicateItem(index)"
+      >
+      <i class="fas fa-copy text-light"/>
+    </button>
+    <button
+    class="btn btn-sm btn-danger"
+    @click="deleteItem(index)"
+    :aria-label="$t('Delete')"
+    >
+    <i class="far fa-trash-alt text-light"/>
+  </button>
+</div>
+</div>
+
+<component
+class="p-3"
+:class="[elementCssClass(element), { 'prevent-interaction': !element.config.interactive }]"
+:tabindex="element.config.interactive ? 0 : -1"
+v-bind="element.config"
+:config="element.config"
+@input="element.config.interactive ? element.config.content = $event : null"
+:is="element['editor-component']"
+/>
+</div>
+</div>
+</div>
+</draggable>
+</div>
 </template>
 
 <script>
@@ -158,59 +158,70 @@ export default {
       this.items.push(duplicate);
       this.$emit('update-state');
     },
-  },
+    isAiSection(element) {
+      return element.component === "AiSection";
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-    .hasError {
+.hasError {
+  border: 1px solid red;
+  border-radius: 0.25rem;
+  
+  .form-element-header {
+    border-bottom: 1px solid red;
+    color: red;
+  }
+}
+
+.column-draggable {
+  border: 1px dashed #000;
+  min-height: 80px;
+  content: "Drag Controls";
+}
+
+.selected .column-draggable {
+  border: none;
+}
+
+.control-item {
+  position: relative;
+  
+  .delete {
+    position: absolute;
+    top: 0px;
+    right: 0px;
+    display: none;
+  }
+  
+  &.selected,
+  &:hover {
+    .mask {
       border: 1px solid red;
-      border-radius: 0.25rem;
-
-      .form-element-header {
-        border-bottom: 1px solid red;
-        color: red;
-      }
     }
-
-    .column-draggable {
-        border: 1px dashed #000;
-        min-height: 80px;
-        content: "Drag Controls";
+    
+    .delete {
+      display: inline-block;
     }
+  }
+  
+  .mask {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    background-color: rgba(0, 0, 0, 0);
+    width: 100%;
+    height: 100%;
+  }
+}
 
-    .selected .column-draggable {
-      border: none;
-    }
+.ai-section-card {
+  border-color: #8ab8ff;
+}
 
-    .control-item {
-        position: relative;
-
-        .delete {
-            position: absolute;
-            top: 0px;
-            right: 0px;
-            display: none;
-        }
-
-        &.selected,
-        &:hover {
-            .mask {
-                border: 1px solid red;
-            }
-
-            .delete {
-                display: inline-block;
-            }
-        }
-
-        .mask {
-            position: absolute;
-            top: 0px;
-            left: 0px;
-            background-color: rgba(0, 0, 0, 0);
-            width: 100%;
-            height: 100%;
-        }
-    }
+.ai-section-card .card-header {
+  background: #cbdfff;
+}
 </style>
