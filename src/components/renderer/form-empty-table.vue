7d5a8a94ab466173c5b2eafb106653b7c54cbd6d
Fabio <frguachallab@gmail.com>
2023-10-12 20:02:21 +0000

    FOUR-10454:Home: No Results message

---
+++ src/components/renderer/form-empty-table.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container content">
+    <img src="../../assets/table_empty.svg" alt="empty table" />
+    <span class="title">
+      {{ $t(title) }}
+    </span>
+    <b-link :href="url">
+      {{ linkText }}
+    </b-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props:["title", "link", "url"],
+  data() {
+    const linkText = `View All ${this.link}`;
+    return {
+      linkText
+    };
+  }
+};
+</script>
+
+<style scoped>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+}
+.title {
+  font-size: 32px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 38px;
+  letter-spacing: -1.28px;
+}
+</style>
