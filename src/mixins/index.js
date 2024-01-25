7389963acb96fa015db07ba3532a403edc1eea0e
Jose Chirivella <jose.chirivella@processmaker.com>
2024-01-25 15:26:02 -0600

    Exporting the mixins

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -1,6 +1,19 @@
-export { default as HasColorProperty } from './HasColorProperty';
-export { default as shouldElementBeVisible } from './shouldElementBeVisible';
-export { default as getValidPath } from './getValidPath';
-export { default as formWatchers } from './formWatchers';
-export { default as multiselectApi } from './multiselectApi';
-export { default as defaultValues } from './defaultValues';
+import { camelCase, upperFirst } from "lodash";
+
+const mixins = [];
+const modules = import.meta.globEager("./**/*.js");
+
+Object.entries(modules).forEach(([path, m]) => {
+  const mixingName = upperFirst(
+    camelCase(
+      path
+        .split("/")
+        .pop()
+        .replace(/\.\w+$/, "")
+    )
+  );
+
+  mixins.push(m.default);
+});
+
+export default mixins;
