3913e4956a5fd664c13f63ab194d79893a27404d
Luciana Nunez <luciana.nunez@processmaker.com>
2024-02-07 15:10:26 -0400

    feature/FOUR-13370

--- src/components/renderer/form-avatar.vue
+++ src/components/renderer/form-avatar.vue
@@ -1,21 +1,69 @@
 <template>
-  <div class="form-group form-image" style="overflow-x: hidden" >
-    avatar
-  </div>
+  <span>
+    <b-button
+      variant="primary"
+      class="avatar-button rounded-circle overflow-hidden p-0 m-0 d-inline-flex border-0"
+      disabled
+    >
+      <img v-if="user.avatar" :src="user.avatar" :width="width" :height="height">
+      
+      <span
+        v-else
+        class="border-0 d-inline-flex align-items-center justify-content-center text-white text-uppercase text-nowrap font-weight-normal"
+        :style="styleAvatar()"
+      >
+        <span v-if="getInitials()">{{ getInitials() }}</span>
+        <span v-else>PM</span>
+      </span>
+    </b-button>
+  </span>
 </template>
 
 <script>
 export default {
-  props: ['id', 'image', 'width', 'height', 'name', 'renderImage', 'variableName', 'validationData', 'imageName'],
+  props: {
+    width: {
+      default: 64,
+    },
+    height: {
+      default: 64,
+    },
+  },
+
   data() {
     return {
+      user: window.Processmaker.user,
     };
   },
+  mounted() {
+    this.getInitials();
+  },
+  methods: {
+    getInitials() {
+      return this.user.firstname && this.user.lastname
+        ? this.user.firstname.match(/./u)[0] + this.user.lastname.match(/./u)[0]
+        : "";
+    },
+    styleAvatar() {
+      return "width: " +
+        this.width +
+        "px; height: " +
+        this.height +
+        "px; font-size:" +
+        this.height / 2.5 +
+        "px; padding:0;";
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-  .form-image {
-    overflow-x: hidden;
+  .avatar-button:disabled {
+    opacity: 1;
+    pointer-events: none;
+  }
+  .empty-image {
+    font-size: 2em;
   }
 </style>
+
