35591f797888d30b666fd9208e5d8c01089be727
Julio Cesar Laura Avendaño <juliocesar@processmaker.com>
2024-09-11 13:51:11 +0000

    FOUR-18070 STORY Reveal/Conceal Content in Form Preview

--- src/components/renderer/form-masked-input.vue
+++ src/components/renderer/form-masked-input.vue
@@ -3,28 +3,59 @@
     <required-asterisk /><label v-uni-for="name">{{ label }}</label>
     <component
       :is="componentType"
-      v-if="componentType !== 'input'"
+      v-if="componentType !== 'input' && !encryptedConfig?.encrypted"
       v-model="localValue"
       v-bind="componentConfigComputed"
       v-uni-id="name"
       :name="name"
       class="form-control"
       :class="classList"
-      type="text"
+      :type="inputType"
       @change="onChange"
     />
     <input
-      v-else
+      v-else-if="componentType === 'input' && !encryptedConfig?.encrypted"
       v-model="localValue"
       v-bind="componentConfig"
       v-uni-id="name"
       :name="name"
       class="form-control"
       :class="classList"
-      :type="dataType"
+      :type="inputType"
       :maxlength="maxlength"
       @change="onChange"
     />
+    <div class="div-data-container" v-else-if="encryptedConfig?.encrypted">
+      <component
+        :is="componentType"
+        v-if="componentType !== 'input'"
+        v-model="localValue"
+        v-bind="componentConfigComputed"
+        v-uni-id="name"
+        :name="name"
+        class="form-control"
+        :class="classList"
+        :type="inputType"
+        @change="onChange"
+      />
+      <input
+        v-else
+        v-model="localValue"
+        v-bind="componentConfig"
+        v-uni-id="name"
+        :name="name"
+        class="form-control"
+        :class="classList"
+        :type="inputType"
+        :maxlength="maxlength"
+        @change="onChange"
+      />
+      <b-button variant="outline-secondary conceal-reveal-btn" @click="concealOrReveal">
+        <i :class="iconBtn"></i>
+        {{ labelBtn }}
+      </b-button>
+    </div>
+
     <template v-if="validator && validator.errorCount">
       <div
         v-for="(errors, index) in validator.errors.all()"
@@ -37,6 +68,7 @@
       </div>
     </template>
     <div v-if="error" class="invalid-feedback">{{ error }}</div>
+    <div v-if="encryptedConfig?.encrypted && errorEncryptedField" class="invalid-feedback d-block">{{ errorEncryptedField }}</div>
     <small v-if="helper" class="form-text text-muted">{{ helper }}</small>
   </div>
 </template>
@@ -52,6 +84,7 @@ import {
   ValidationMixin
 } from "@processmaker/vue-form-elements";
 import Inputmasked from "./form-input-masked.vue";
+import { validate as uuidValidate } from 'uuid';
 
 const uniqIdsMixin = createUniqIdsMixin();
 const componentTypes = {
@@ -86,7 +119,8 @@ export default {
     // these should not be passed by $attrs
     "transientData",
     "formConfig",
-    "form-watchers"
+    "form-watchers",
+    "encryptedConfig",
   ],
   data() {
     const { dataFormat, customFormatter } = this.config;
@@ -190,19 +224,31 @@ export default {
       localValue: null,
       validationRules: {
         percentage: "regex:/^[+-]?\\d+(\\.\\d+)?$/"
-      }
+      },
+      // For encrypted field
+      inputType: '',
+      iconBtn: '',
+      labelBtn: '',
+      errorEncryptedField: ''
     };
   },
   computed: {
     classList() {
       return {
         "is-invalid":
-          (this.validator && this.validator.errorCount) || this.error,
+          (this.validator && this.validator.errorCount) || this.error || this.errorEncryptedField,
         [this.controlClass]: !!this.controlClass
       };
     },
     componentConfigComputed() {
       return JSON.parse(JSON.stringify(this.componentConfig));
+    },
+    requestId() {
+      const node = document.head.querySelector(`meta[name="request-id"]`);
+      if (node === null) {
+        return null;
+      }
+      return node.content;
     }
   },
   watch: {
@@ -212,6 +258,7 @@ export default {
       }
     },
     localValue(value) {
+      // To Do: Add logic for the encrypted fields
       if (value !== this.value) {
         this.$emit("input", this.convertToData(value));
       }
@@ -221,6 +268,25 @@ export default {
     if (this.value !== undefined) {
       this.localValue = this.value;
     }
+    /*
+     * Set initial atttributes for Conceal/Reveal button only if
+     * "encrypted" attribute is enabled
+     */
+    if (this.encryptedConfig?.encrypted) {
+      if (uuidValidate(this.localValue)) {
+        this.inputType = "password";
+        this.iconBtn = "fas fa-eye";
+        this.labelBtn = this.$t("Reveal");
+        this.componentConfig.readonly = true;
+      } else {
+        this.inputType = "text";
+        this.iconBtn = "fas fa-eye-slash";
+        this.labelBtn = this.$t("Conceal");
+        this.componentConfig.readonly = false;
+      }
+    } else {
+        this.inputType = this.dataType;
+      }
   },
   methods: {
     onChange() {
@@ -291,7 +357,85 @@ export default {
         date: ["####-##-##"],
         dateTime: ["####-##-## ##:##"]
       };
+    },
+    concealOrReveal() {
+      // Execute only if "encrypted" attribute is enabled
+      if (this.encryptedConfig?.encrypted) {
+        if (this.inputType === "text") {
+          if (this.localValue !== "") {
+            // Build data to send
+            const dataToEncrypt = {
+              request_id: this.requestId,
+              field_name: this.name,
+              plain_text: this.localValue,
+              screen_id: this.$root.task?.screen?.screen_id
+            };
+
+            // Call endpoint to encrypt data
+            window.ProcessMaker.apiClient
+              .post("encrypted_data/encryptText", dataToEncrypt)
+              .then((response) => {
+                // Change controls appearance
+                this.inputType = "password";
+                this.iconBtn = "fas fa-eye";
+                this.labelBtn = this.$t("Reveal");
+                this.componentConfig.readonly = true;
+                this.errorEncryptedField = "";
+
+                // Assign uuid from encrypted data
+                this.localValue = response;
+              })
+              .catch((err) => {
+                const { data } = err.response;
+                if (data.message) {
+                  this.errorEncryptedField = data.message;
+                }
+              });
+          } else {
+            this.errorEncryptedField = this.$t(
+              "The current value is empty but a value is required. Please provide a valid value."
+            );
+          }
+        } else {
+          // Build data to send
+          const dataToDecrypt = {
+              request_id: this.requestId,
+              field_name: this.name,
+              screen_id: this.$root.task?.screen?.screen_id
+            };
+
+            // Call endpoint to decrypt data
+            window.ProcessMaker.apiClient
+              .post("encrypted_data/decryptText", dataToDecrypt)
+              .then((response) => {
+                // Change controls appearance
+                this.inputType = this.dataType;
+                this.iconBtn = "fas fa-eye-slash";
+                this.labelBtn = this.$t("Conceal");
+                this.componentConfig.readonly = false;
+
+                // Assign value decrypted
+                this.localValue = response;
+              })
+              .catch((err) => {
+                const { data } = err.response;
+                if (data.message) {
+                  this.errorEncryptedField = data.message;
+                }
+              });
+        }
+      }
     }
   }
 };
 </script>
+<style scoped>
+.div-data-container {
+  display: inline-flex;
+  width: 100%;
+}
+.conceal-reveal-btn {
+  width: 20%;
+  margin-left: 5px;
+}
+</style>
