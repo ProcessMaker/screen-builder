6a325a285c35d7be541df81a91950b248b53c36f
Teisha McRae <mcraeteisha@gmail.com>
2024-09-16 22:37:31 -0400

    Add translation methods to hardcoded text

--- src/components/ScreenTemplates.vue
+++ src/components/ScreenTemplates.vue
@@ -4,7 +4,7 @@
     data-cy="screen-templates-section"
   >
     <div class="d-flex justify-content-between">
-      <h6 class="pt-2">Select a Template</h6>
+      <h6 class="pt-2">{{ $t("Select a Template") }}</h6>
       <button
         class="panel-close-btn"
         @click="$emit('close-templates-panel')"
@@ -20,7 +20,7 @@
         :class="{ 'my-templates-selected': myTemplatesSelected }"
         data-cy="my-templates-tab"
       >
-        My Templates
+      {{ $t("My Templates") }}
       </b-button>
       <b-button
         @click="showSharedTemplates"
@@ -28,7 +28,7 @@
         :class="{ 'shared-templates-selected': sharedTemplatesSelected }"
         data-cy="shared-templates-tab"
       >
-        Shared Templates
+      {{ $t("Shared Templates") }}
       </b-button>
     </div>
     <div class="d-flex justify-content-center cards-container">
@@ -42,14 +42,13 @@
             v-if="noMyTemplatesFound"
             class="p-2 h-100 overflow-auto"
           >
-            <h5>No templates found.</h5>
+            <h5>{{ $t("No templates found.") }}</h5>
           </b-card-body>
           <screen-template-card
             v-else
             v-for="template in myTemplatesData"
             :key="template.id"
             :template="template"
-            @template-selected="handleSelectedTemplate"
           />
         </b-card-group>
       </div>
@@ -63,14 +62,13 @@
             v-if="noSharedTemplatesFound"
             class="p-2 h-100 overflow-auto"
           >
-            <h5>No templates found.</h5>
+            <h5>{{ $t("No templates found.") }}</h5>
           </b-card-body>
           <screen-template-card
             v-else
             v-for="template in sharedTemplatesData"
             :key="template.id"
             :template="template"
-            @template-selected="handleSelectedTemplate"
           />
         </b-card-group>
       </div>
@@ -86,7 +84,6 @@
       ScreenTemplateCard,
     },
     mounted() {
-        console.log('screen-templates component mounted');
     },
     data() {
       return {
@@ -133,9 +130,6 @@
         .catch((error) => {
           console.error(error);
         });
-      },
-      handleSelectedTemplate() {
-        console.log('hit handleSelectedTemplate');
       },
       showSharedTemplates() {
         this.myTemplatesSelected = false;
