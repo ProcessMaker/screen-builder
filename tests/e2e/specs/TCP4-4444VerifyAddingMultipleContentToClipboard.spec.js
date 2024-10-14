a1b07b83b829c9c1057a45b4b104cf5bd5e1827a
Rodrigo Quelca <rodrigo@processmaker.com>
2024-10-14 10:55:36 -0400

    add wait to sync load pages

--- tests/e2e/specs/TCP4-4444VerifyAddingMultipleContentToClipboard.spec.js
+++ tests/e2e/specs/TCP4-4444VerifyAddingMultipleContentToClipboard.spec.js
@@ -1,4 +1,4 @@
-describe("Clipboard Button Actions", () => {
+describe("Verify Adding Multiple Content To The Clipboard", () => {
   beforeEach(() => {
     cy.visit("/");
     cy.showValidationOnLoad();
@@ -16,17 +16,20 @@ describe("Clipboard Button Actions", () => {
 
     // Step 2: Load a new screen and repeat the process for adding to clipboard
     cy.loadFromJson("screen2TCP4-4446.json", 0);
+    cy.wait(100);
     cy.get(':nth-child(1) > [data-cy="screen-element-container"]').click({ force: true });
     cy.get('[data-cy="addToClipboard"]').should("be.visible").click();
     cy.get('[data-cy="addToClipboard"]').should("not.exist");
 
     // Step 3: Reload the first screen and verify that the element is flagged as selected
     cy.loadFromJson("TCP4-4446.json", 0);
+    cy.wait(100);
     cy.get(':nth-child(1) > [data-cy="screen-element-container"]').click({ force: true });
     cy.get('[data-cy="addToClipboard"]').should("not.exist");
 
     // Step 4: Reload the second screen and verify clipboard status remains unchanged
     cy.loadFromJson("screen2TCP4-4446.json", 0);
+    cy.wait(100);
     cy.get(':nth-child(1) > [data-cy="screen-element-container"]').click({ force: true });
     cy.get('[data-cy="addToClipboard"]').should("not.exist");
   });
