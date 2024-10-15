0b4049d6067441190a1e586cd10f0db74b5a1bec
CarliPinell <carlos.pinell@processmaker.com>
2024-10-15 11:19:26 -0400

    Apllying Modern color palette

--- src/mixins/HasColorProperty.js
+++ src/mixins/HasColorProperty.js
@@ -5,9 +5,13 @@ export default {
       const css = [];
       element.config.bgcolor ? css.push(element.config.bgcolor) : null;
       element.config.color ? css.push(element.config.color) : null;
-      element.config.bgcolormodern ? css.push(element.config.bgcolormodern) : null;
       return css.join(' ');
     },
+    elementCssClassModern(element) {
+      const css = [];
+      element.config.bgcolormodern ? css.push(element.config.bgcolormodern) : null;
+      return css.join(' ');
+    }
   },
 };
 
