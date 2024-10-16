43cd02820b7c0789466f0ca82dce4ac1a04708c9
Eleazar Resendez <eleazar.resendez@processmaker.com>
2024-10-16 13:22:17 -0600

    Apply ESlint in ScreenTemplates.vue component

--- src/components/ScreenTemplates.vue
+++ src/components/ScreenTemplates.vue
@@ -1,32 +1,29 @@
 <template>
-  <div
-    class="screen-templates-container"
-    data-cy="screen-templates-section"
-  >
+  <div class="screen-templates-container" data-cy="screen-templates-section">
     <div class="d-flex justify-content-between">
       <h6 class="pt-2">{{ $t("Select a Template") }}</h6>
       <button
         class="panel-close-btn"
-        @click="$emit('close-templates-panel')"
         data-cy="close-templates-section"
+        @click="$emit('close-templates-panel')"
       >
         <i class="fas fa-times"></i>
       </button>
     </div>
     <div class="d-flex template-tabs justify-content-center">
       <b-button
-        @click="showMyTemplates"
         class="d-inline default-template-btn px-1"
         :class="{ 'my-templates-selected': myTemplatesSelected }"
         data-cy="my-templates-tab"
+        @click="showMyTemplates"
       >
         {{ $t("My Templates") }}
       </b-button>
       <b-button
-        @click="showSharedTemplates"
         class="d-inline default-template-btn"
         :class="{ 'shared-templates-selected': sharedTemplatesSelected }"
         data-cy="shared-templates-tab"
+        @click="showSharedTemplates"
       >
         {{ $t("Shared Templates") }}
       </b-button>
@@ -45,12 +42,12 @@
             <h5>{{ $t("No templates found.") }}</h5>
           </b-card-body>
           <screen-template-card
-            v-else
             v-for="template in myTemplatesData"
+            v-else
             :key="template.id"
             :template="template"
             :screen-id="screenId"
-            :currentScreenPage="currentScreenPage"
+            :current-screen-page="currentScreenPage"
           />
         </b-card-group>
       </div>
@@ -67,96 +64,98 @@
             <h5>{{ $t("No templates found.") }}</h5>
           </b-card-body>
           <screen-template-card
-            v-else
             v-for="template in sharedTemplatesData"
+            v-else
             :key="template.id"
             :template="template"
             :screen-id="screenId"
-            :currentScreenPage="currentScreenPage"
+            :current-screen-page="currentScreenPage"
           />
         </b-card-group>
       </div>
     </div>
   </div>
 </template>
-  
-<script>
-  import ScreenTemplateCard from './ScreenTemplateCard.vue';
 
-  export default {
-    components: {
-      ScreenTemplateCard,
+<script>
+import ScreenTemplateCard from "./ScreenTemplateCard.vue";
+
+export default {
+  components: {
+    ScreenTemplateCard
+  },
+  props: {
+    screenId: {
+      type: Number,
+      required: true
     },
-    props: {
-      screenId: {
-        type: Number,
-        required: true,
-      },
-      currentScreenPage: {
-        type: Number,   
-        default: 0,
-      },
-      screenType: {
-        type: String,   
-        default: 'FORM',
-      }
+    currentScreenPage: {
+      type: Number,
+      default: 0
     },
-    data() {
-      return {
-        myTemplatesData: null,
-        sharedTemplatesData: null,
-        myTemplatesSelected: true,
-        sharedTemplatesSelected: false,
-        noMyTemplatesFound: false,
-        noSharedTemplatesFound: false,
-      };
+    screenType: {
+      type: String,
+      default: "FORM"
+    }
+  },
+  data() {
+    return {
+      myTemplatesData: null,
+      sharedTemplatesData: null,
+      myTemplatesSelected: true,
+      sharedTemplatesSelected: false,
+      noMyTemplatesFound: false,
+      noSharedTemplatesFound: false
+    };
+  },
+  mounted() {
+    this.showMyTemplates();
+  },
+  methods: {
+    showMyTemplates() {
+      this.myTemplatesSelected = true;
+      this.sharedTemplatesSelected = false;
+      this.fetchMyTemplates();
     },
-    methods: {
-      showMyTemplates() {
-        this.myTemplatesSelected = true;
-        this.sharedTemplatesSelected = false;
-        this.fetchMyTemplates();
-      },
-      fetchMyTemplates() {
-        ProcessMaker.apiClient
-        .get(
-          `templates/screen?is_public=0&screen_type=${this.screenType}`,
-        )
+    fetchMyTemplates() {
+      ProcessMaker.apiClient
+        .get(`templates/screen?is_public=0&screen_type=${this.screenType}`)
         .then((response) => {
           this.myTemplatesData = response.data.data;
-          if (this.myTemplatesData.length === 0 || this.myTemplatesData === undefined) {
+          if (
+            this.myTemplatesData.length === 0 ||
+            this.myTemplatesData === undefined
+          ) {
             this.noMyTemplatesFound = true;
           }
         })
         .catch((error) => {
           console.error(error);
         });
-      },
-      fetchSharedTemplates() {
+    },
+    fetchSharedTemplates() {
       ProcessMaker.apiClient
-        .get(
-          `templates/screen?is_public=1&screen_type=${this.screenType}`,
-        )
+        .get(`templates/screen?is_public=1&screen_type=${this.screenType}`)
         .then((response) => {
           this.sharedTemplatesData = response.data.data;
-          if (this.sharedTemplatesData.length === 0 || this.sharedTemplatesData === undefined) {
+          if (
+            this.sharedTemplatesData.length === 0 ||
+            this.sharedTemplatesData === undefined
+          ) {
             this.noSharedTemplatesFound = true;
           }
         })
         .catch((error) => {
           console.error(error);
         });
-      },
-      showSharedTemplates() {
-        this.myTemplatesSelected = false;
-        this.sharedTemplatesSelected = true;
-        this.fetchSharedTemplates();
-      },
     },
-    mounted() {
-      this.showMyTemplates();
+    showSharedTemplates() {
+      this.myTemplatesSelected = false;
+      this.sharedTemplatesSelected = true;
+      this.fetchSharedTemplates();
     }
-  };
+  }
+};
 </script>
 
 <style lang="scss" scoped>
@@ -164,53 +163,56 @@
   height: 100%;
 }
 
-  .panel-close-btn {
-    background-color: transparent;
-    border: none;
-    color: #596372;
-  }
+.panel-close-btn {
+  background-color: transparent;
+  border: none;
+  color: #596372;
+}
 
-  .template-tabs {
-    padding: 4px;
-    background-color: #E9ECF1;
-    border-radius: 8px;
-    margin-top: 0.5rem;
-    margin-bottom: 0.5rem;
-  }
+.template-tabs {
+  padding: 4px;
+  background-color: #e9ecf1;
+  border-radius: 8px;
+  margin-top: 0.5rem;
+  margin-bottom: 0.5rem;
+}
 
-  .default-template-btn {
-    width: 50%;
-    background-color: transparent;
-    border: none;
-    color: #596372;
-    font-size: 12px;
-    padding-left: 0px;
-    padding-right: 0px;
-    text-transform: none;
-  }
+.default-template-btn {
+  width: 50%;
+  background-color: transparent;
+  border: none;
+  color: #596372;
+  font-size: 12px;
+  padding-left: 0px;
+  padding-right: 0px;
+  text-transform: none;
+}
 
-  .my-templates-selected {
-    background-color: #ffffff;
-    color: #20242A;
-    border-radius: 8px;
-    border: none;
-    font-weight: 600;
-    font-size: 12px;
-    padding-left: 0px;
-    padding-right: 0px;
-    box-shadow: 0px 3px 6px -3px rgb(0, 0, 0, 0.05), 0px 2px 4px -2px rgba(0, 0, 0, 0.05), 0px 1px 2px -1px rgb(0, 0, 0, 0.05), 0px 1px 0px -1px rgb(0, 0, 0, 0.05);
-  }
+.my-templates-selected {
+  background-color: #ffffff;
+  color: #20242a;
+  border-radius: 8px;
+  border: none;
+  font-weight: 600;
+  font-size: 12px;
+  padding-left: 0px;
+  padding-right: 0px;
+  box-shadow: 0px 3px 6px -3px rgb(0, 0, 0, 0.05),
+    0px 2px 4px -2px rgba(0, 0, 0, 0.05), 0px 1px 2px -1px rgb(0, 0, 0, 0.05),
+    0px 1px 0px -1px rgb(0, 0, 0, 0.05);
+}
 
-  .shared-templates-selected {
-    background-color: #ffffff;
-    color: #20242A;
-    border-radius: 8px;
-    border: none;
-    font-weight: 600;
-    font-size: 12px;
-    padding-left: 0px;
-    padding-right: 0px;
-    box-shadow: 0px 3px 6px -3px rgb(0, 0, 0, 0.05), 0px 2px 4px -2px rgba(0, 0, 0, 0.05), 0px 1px 2px -1px rgb(0, 0, 0, 0.05), 0px 1px 0px -1px rgb(0, 0, 0, 0.05);
-  }
-
+.shared-templates-selected {
+  background-color: #ffffff;
+  color: #20242a;
+  border-radius: 8px;
+  border: none;
+  font-weight: 600;
+  font-size: 12px;
+  padding-left: 0px;
+  padding-right: 0px;
+  box-shadow: 0px 3px 6px -3px rgb(0, 0, 0, 0.05),
+    0px 2px 4px -2px rgba(0, 0, 0, 0.05), 0px 1px 2px -1px rgb(0, 0, 0, 0.05),
+    0px 1px 0px -1px rgb(0, 0, 0, 0.05);
+}
 </style>
