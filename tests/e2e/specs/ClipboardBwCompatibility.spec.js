186e62801ed2a9c6985240baabccd914c4cf70ba
David Callizaya <davidcallizaya@gmail.com>
2024-10-21 11:30:15 -0400

    Test add to clipboard from page 2

--- tests/e2e/specs/ClipboardBwCompatibility.spec.js
+++ tests/e2e/specs/ClipboardBwCompatibility.spec.js
@@ -19,5 +19,21 @@ describe("Clipboard backward compatibility", () => {
         expect(content.length).to.equal(index + 1);
       });
     });
+
+    // Go to second page
+    cy.get('[data-test="page-dropdown"]').click({force: true});
+    cy.get('[data-test="page-popup"]').click({force: true});
+    const elementsAddedFromMainPage = 17;
+
+    // check if each control in the second page can be added to the clipboard
+    cy.get('.control-item').each(($el, index) => {
+      cy.wrap($el).click({ force: true });
+      cy.get('[data-cy="addToClipboard"]').click({ force: true });
+      cy.get('[data-cy="addToClipboard"]').should("not.exist").then(() => {
+        // Verify it was added to the clipboard storage
+        const content = ClipboardManager.loadFromLocalStorage();
+        expect(content.length).to.equal(elementsAddedFromMainPage + index + 1);
+      });
+    });
   });
 });
