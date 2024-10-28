af7b7450d56bc5aa7ad84e087a644bb258c876cd
CarliPinell <carlos.pinell@processmaker.com>
2024-10-28 11:50:55 -0400

    fixing also Design Modern options issue

--- src/components/vue-form-builder.vue
+++ src/components/vue-form-builder.vue
@@ -245,7 +245,9 @@
                   v-model="element.items"
                   :validation-errors="validationErrors"
                   class="card-body"
-                  :class="styleMode === 'Modern' ? elementCssClassModern(element) : elementCssClass(element)"
+                  :class="styleMode === 'Modern' && element.component === 'FormRecordList' 
+                    ? elementCssClassModern(element) 
+                    : elementCssClass(element)"
                   :selected="selected"
                   :config="element.config"
                   :ai-element="element"
@@ -299,7 +301,9 @@
                   :tabindex="element.config.interactive ? 0 : -1"
                   class="card-body m-0 pb-4 pt-4"
                   :class="[
-                    styleMode === 'Modern' ? elementCssClassModern(element) : elementCssClass(element),
+                    styleMode === 'Modern' && element.component === 'FormRecordList' 
+                    ? elementCssClassModern(element) 
+                    : elementCssClass(element),
                     { 'prevent-interaction': !element.config.interactive }
                   ]"
                   @input="
@@ -883,7 +887,7 @@ export default {
     },
     shouldShow(item, accordion) {
       const sourceOptions = this.inspection.config[item.field]?.sourceOptions;
-
+      //console.log("INICIO this.styleMode: ", this.styleMode);
       if (sourceOptions === 'Variable') {
         this.enableOption = true;
         return true;
