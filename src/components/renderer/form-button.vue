3073dd1da56ebd48cab854aa06ae5baf23c6fa86
David Callizaya <davidcallizaya@gmail.com>
2025-07-22 12:47:24 -0400

    Add handler support to FormButton component and integrate it into LoadFieldComponents mixin

--- src/components/renderer/form-button.vue
+++ src/components/renderer/form-button.vue
@@ -22,7 +22,7 @@ import { getValidPath } from '@/mixins';
 
 export default {
   mixins: [getValidPath],
-  props: ['variant', 'label', 'event', 'eventData', 'name', 'fieldValue', 'value', 'tooltip', 'transientData', 'loading', 'loadingLabel'],
+  props: ['variant', 'label', 'event', 'eventData', 'name', 'fieldValue', 'value', 'tooltip', 'transientData', 'loading', 'loadingLabel', 'handler'],
   data() {
     return {
       showSpinner: false
@@ -80,6 +80,8 @@ export default {
         const trueValue = this.fieldValue || '1';
         const value = (this.value == trueValue) ? null : trueValue;
         this.$emit('input', value);
+        // Run handler after setting the value
+        await this.runHandler();
       }
       if (this.event !== 'pageNavigate' && this.name) {
         this.setValue(this.$parent, this.name, this.fieldValue);
@@ -89,6 +91,8 @@ export default {
           this.showSpinner = true;
         }
         this.$emit('input', this.fieldValue);
+        // Run handler after setting the value
+        await this.runHandler();
         this.$nextTick(() => {
           this.$emit('submit', this.eventData, this.loading, this.buttonInfo);
         });
@@ -99,6 +103,15 @@ export default {
         this.$emit('page-navigate', this.eventData);
       }
     },
+    async runHandler() {
+      if (this.handler) {
+        try {
+          await new Function(this.handler).apply(this.transientData);
+        } catch (error) {
+          console.error('❌ There is an error in the button handler', error);
+        }
+      }
+    }
   },
 };
 </script>
