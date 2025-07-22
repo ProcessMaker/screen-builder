03fc071b3c1b7364e214e7c5ee8c9e1394adf6dd
David Callizaya <davidcallizaya@gmail.com>
2025-07-22 14:02:16 -0400

    Remove non required code

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,5 @@
 /** @type { import('@storybook/vue').Preview } */
 
-import Vue from "vue";
-import MonacoEditor from "vue-monaco";
-
-Vue.component("MonacoEditor", MonacoEditor);
-
 const preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
