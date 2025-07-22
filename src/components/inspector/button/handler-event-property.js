52f792fd7c1384daaee5e745e46329afebf6cefe
David Callizaya <davidcallizaya@gmail.com>
2025-07-22 12:46:40 -0400

    Implement code editor for button click handler

---
+++ src/components/inspector/button/handler-event-property.js
@@ -0,0 +1,11 @@
+import CodeEditor from '../code-editor.vue';
+
+export const handlerEventProperty = {
+  type: CodeEditor,
+  field: 'handler',
+  config: {
+    label: 'Click Handler',
+    helper: 'The handler is a JavaScript function that will be executed when the button is clicked.',
+    dataFeature: 'i1177',
+  },
+};
