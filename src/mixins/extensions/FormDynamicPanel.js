91e23159df2afc15027f249ff0901acc90d18976
Rodrigo <rodrigoquelca@gmail.com>
2025-07-31 14:35:02 -0400

    add cypress test

--- src/mixins/extensions/FormDynamicPanel.js
+++ src/mixins/extensions/FormDynamicPanel.js
@@ -13,24 +13,18 @@ export default {
       const index = element.config.settings.indexName;
 
       // Add itemData to the properties of FormDynamicPanel
-      properties[':itemData'] = `${variableName}?.[${index}]`;
+      properties[':itemData'] = `${variableName} && ${variableName}[${index}]`;
       this.registerVariable(element.config.settings.varname, element);
-      /*this.loops.push({
-        variable: element.config.settings.varname,
-        element,
-        properties
-      });*/
     },
     loadFormDynamicPanelItems({ element, node, definition }) {
-      //const safeDotName = this.safeDotName(element.config.settings.varname);
       const nested = {
         config: [
           {
             items: element.items,
           }
         ],
-        watchers: [], //definition.watchers,
-        isMobile: false //definition.isMobile
+        watchers: [], 
+        isMobile: false
       };
 
       let loopContext = "";
@@ -42,29 +36,21 @@ export default {
       const variableName = element.config.settings.varname;
       const index = element.config.settings.indexName;
 
-      // Add nested component inside loop
+      // Add nested component inside dynamic panel
       const child = this.createComponent("ScreenRenderer", {
         ":definition": this.byRef(nested),
-        ":value": `${variableName}?.[${index}]`, //"currentItem",
-        ":loop-context": `'${variableName}?.[${index}]'`,
+        ":value": `${variableName} && ${variableName}[${index}]`,
+        ":loop-context": `'${variableName} && ${variableName}[${index}]'`,
         ":_parent": "getValidationData()",
         ":components": this.byRef(this.components),
         ":config-ref": this.byRef(this.configRef || definition.config),
         "@submit": "submitForm"
       });
       node.appendChild(child);
-      // this.registerVariable(element.config.settings.varname, element);
-      // Register nested component as Array
-      /*this.registerNestedVariable(
-        element.config.settings.varname,
-        `${element.config.settings.varname}.index.`,
-        nested
-      );*/
     }
   },
   mounted() {
     // Convert the FormDynamicPanel to a div
-    //this.alias.FormDynamicPanel = "div";
     this.extensions.push({
       onloadproperties(params) {
         if (params.element.container && params.componentName === "FormDynamicPanel") {
