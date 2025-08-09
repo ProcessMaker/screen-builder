61495900cbd9ef26db90be4ce35de845281f362e
Roly Gutierrez <roly.gutierrez@processmaker.com>
2025-08-09 00:24:44 -0400

    FOUR-25305 Fix the observations.

--- src/components/vue-form-builder.vue
+++ src/components/vue-form-builder.vue
@@ -1532,7 +1532,6 @@ export default {
           copy.config.settings.varname = copy.config.name;
         }
       }
-
       if (copy.component === "FormHtmlViewer") {
         copy.config.name = "form_html_viewer";
       }
