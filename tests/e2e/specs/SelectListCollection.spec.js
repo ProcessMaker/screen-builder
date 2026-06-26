fd96616aeeb635e64d3403748a4f0c674e3f00fb
Miguel Angel <miguelangeldev@icloud.com>
2026-06-26 16:24:33 -0400

    test: add mock data source responses for select list tests

--- tests/e2e/specs/SelectListCollection.spec.js
+++ tests/e2e/specs/SelectListCollection.spec.js
@@ -1,80 +1,87 @@
 describe("select list mustache", () => {
   beforeEach(() => {
     cy.visit("/");
+    const dataSourceResponse = {
+      status: 200,
+      response: {
+        data: [
+          {
+            id: 1,
+            created_by_id: 2,
+            updated_by_id: 2,
+            created_at: "2021-11-08 10:29:56",
+            updated_at: "2021-11-08 10:29:56",
+            data: {
+              dni: "1234",
+              name: {
+                last: "Smith",
+                first: "Oliver"
+              },
+              id: 1
+            },
+            collection_id: 1,
+            title: "1",
+            created_by: {
+              id: 2,
+              email: "admin@processmaker.com"
+            },
+            updated_by: {
+              id: 2,
+              email: "admin@processmaker.com"
+            }
+          },
+          {
+            id: 2,
+            created_by_id: 2,
+            updated_by_id: 2,
+            created_at: "2021-11-08 10:29:56",
+            updated_at: "2021-11-08 10:29:56",
+            data: {
+              dni: "5678",
+              name: {
+                last: "Doe",
+                first: "John"
+              },
+              id: 2
+            },
+            collection_id: 1,
+            title: "2",
+            created_by: {
+              id: 2,
+              email: "admin@processmaker.com"
+            },
+            updated_by: {
+              id: 2,
+              email: "admin@processmaker.com"
+            }
+          }
+        ],
+        meta: {
+          filter: "",
+          sort_by: "",
+          sort_order: "",
+          count: 2,
+          total_pages: 1,
+          current_page: 1,
+          from: 1,
+          last_page: 1,
+          path: "/api/1.0/collections/1/records",
+          per_page: 9223372036854775807,
+          to: 2,
+          total: 2
+        }
+      }
+    };
+
     cy.intercept(
       "POST",
       "/api/1.0/requests/data_sources/2",
-      JSON.stringify({
-        status: 200,
-        response: {
-          data: [
-            {
-              id: 1,
-              created_by_id: 2,
-              updated_by_id: 2,
-              created_at: "2021-11-08 10:29:56",
-              updated_at: "2021-11-08 10:29:56",
-              data: {
-                dni: "1234",
-                name: {
-                  last: "Smith",
-                  first: "Oliver"
-                },
-                id: 1
-              },
-              collection_id: 1,
-              title: "1",
-              created_by: {
-                id: 2,
-                email: "admin@processmaker.com"
-              },
-              updated_by: {
-                id: 2,
-                email: "admin@processmaker.com"
-              }
-            },
-            {
-              id: 2,
-              created_by_id: 2,
-              updated_by_id: 2,
-              created_at: "2021-11-08 10:29:56",
-              updated_at: "2021-11-08 10:29:56",
-              data: {
-                dni: "5678",
-                name: {
-                  last: "Doe",
-                  first: "John"
-                },
-                id: 2
-              },
-              collection_id: 1,
-              title: "2",
-              created_by: {
-                id: 2,
-                email: "admin@processmaker.com"
-              },
-              updated_by: {
-                id: 2,
-                email: "admin@processmaker.com"
-              }
-            }
-          ],
-          meta: {
-            filter: "",
-            sort_by: "",
-            sort_order: "",
-            count: 2,
-            total_pages: 1,
-            current_page: 1,
-            from: 1,
-            last_page: 1,
-            path: "/api/1.0/collections/1/records",
-            per_page: 9223372036854775807,
-            to: 2,
-            total: 2
-          }
-        }
-      })
+      JSON.stringify(dataSourceResponse)
+    ).as("executeScript");
+    cy.intercept(
+      "GET",
+      "/api/1.0/requests/data_sources/2/resources/ListAll/data*",
+      JSON.stringify(dataSourceResponse)
     ).as("executeScript");
   });
 
